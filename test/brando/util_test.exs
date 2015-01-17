e94da2ffd5979b0719ebef6c4bed42da4c923bf9
Trond Mjøen <trond@twined.net>
2015-01-17 18:25:40 +0100

    Add tests for slugify_filename/1, random_filename/1 & to_string_map/1

--- test/brando/util_test.exs
+++ test/brando/util_test.exs
@@ -1,6 +1,11 @@
 defmodule Brando.UtilTest do
   use ExUnit.Case
   import Brando.Util
+  alias Brando.UtilTest.TestStruct
+
+  defmodule TestStruct do
+    defstruct a: "a", b: "b"
+  end
 
   test "slugify basic stripping/no ending dash" do
     assert(slugify("This is basic functionality!!!    ") == "this-is-basic-functionality")
@@ -32,5 +37,24 @@ defmodule Brando.UtilTest do
 
   test "slugify strip extraneous dashes" do
     assert(slugify("so - just one then?") == "so-just-one-then")
+  end
+
+  test "slugify_filename/1" do
+    assert(slugify_filename("testing with spaces.jpeg") == "testing-with-spaces.jpeg")
+    assert(slugify_filename("-start æøå-.jpeg") == "start-aeoa.jpeg")
+  end
+
+  test "random_filename/1" do
+    f = random_filename("original-filename.jpg")
+    refute(f == "original-filename.jpg")
+    assert(String.contains?(f, ".jpg"))
+  end
+
+  test "to_string_map/1" do
+    test_struct = %TestStruct{}
+    test_map = %{"a" => "a", "b" => "b"}
+    assert to_string_map(nil) == nil
+    assert to_string_map(test_struct) == test_map
+    assert to_string_map(test_map) == test_map
   end
 end
