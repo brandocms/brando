92eb77c2b550e7d4cfb517a4658a21a3c91938e0
Trond Mjøen <trond@twined.net>
2015-01-17 17:02:06 +0100

    Add docs

--- lib/brando.ex
+++ lib/brando.ex
@@ -1,13 +1,32 @@
 defmodule Brando do
+  @moduledoc """
+  Brando is a set of helping hands for building Twined applications.
+  """
 
+  @doc """
+  Gets the configuration for `module` under :brando,
+  as set in config.exs
+  """
   def config(module), do: Application.get_env(:brando, module, [])
 
+  @doc """
+  Gets the parent app's router, as set in config.exs
+  """
   def get_router, do: config(:router)
 
+  @doc """
+  Gets the parent app's endpoint, as set in config.exs
+  """
   def get_endpoint, do: config(:endpoint)
 
+  @doc """
+  Gets the parent app's repo, as set in config.exs
+  """
   def get_repo, do: config(:repo)
 
+  @doc """
+  Gets the parent app's helpers, as set in config.exs
+  """
   def get_helpers, do: Module.concat(get_router, "Helpers")
 
   defprotocol Render do
