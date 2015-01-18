61ff7e4b12c2b9e41a49bb60e899f796727dc00f
Trond Mjøen <trond@twined.net>
2015-01-19 00:39:33 +0100

    Dump the `Brando.Render` protocol

--- lib/brando.ex
+++ lib/brando.ex
@@ -28,14 +28,4 @@ defmodule Brando do
   Gets the parent app's helpers, as set in config.exs
   """
   def get_helpers, do: Module.concat(get_router, "Helpers")
-
-  defprotocol Render do
-    @moduledoc """
-    Render entities to something that can be showed publicly.
-    Used, for example, when converting entities to JSON responses.
-    """
-
-    @spec render(term) :: Dict.t
-    def render(entity)
-  end
 end
