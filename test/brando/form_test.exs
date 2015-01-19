25ce7311f2a845a4eedb7be52428dc0150a4e6f6
Trond Mjøen <trond@twined.net>
2015-01-19 21:07:45 +0100

    Some more tests

--- test/brando/form_test.exs
+++ test/brando/form_test.exs
@@ -1,5 +1,6 @@
 defmodule Brando.FormTest do
   use ExUnit.Case
+  import Brando.Form
 
   defmodule UserForm do
     use Brando.Form
@@ -12,10 +13,10 @@ defmodule Brando.FormTest do
     form "user", [action: "", class: "col-md-offset-2"] do
       field :full_name, :text,
         [required: true,
-         label: "Fullt navn",
+         label: "Full name",
          label_class: "control-label",
-         placeholder: "Fullt navn",
-         help_text: "Skriv inn ditt fødselsnavn - fornavn og etternavn",
+         placeholder: "Full name",
+         help_text: "Enter full name",
          class: "form-control",
          wrapper_class: ""]
       field :username, :text,
@@ -27,7 +28,7 @@ defmodule Brando.FormTest do
          wrapper_class: ""]
       field :email, :email,
         [required: true,
-         label: "E-post",
+         label: "E-mail",
          label_class: "control-label",
          placeholder: "E-post",
          class: "form-control",
@@ -44,21 +45,21 @@ defmodule Brando.FormTest do
          wrapper_class: "",
          default: false]
       field :editor, :checkbox,
-        [label: "Redaktør",
+        [label: "Editor",
          wrapper_class: "",
          default: true]
       field :status, :select,
-        [choices: {__MODULE__, :get_status_choices},
+        [choices: &__MODULE__.get_status_choices/0,
          default: "1",
          label: "Status",
          label_class: "control-label",
          class: "form-control",
          wrapper_class: ""]
       field :avatar, :file,
-        [label: "Bilde",
+        [label: "Avatar",
          label_class: "control-label",
          wrapper_class: ""]
-      submit "Lagre",
+      submit "Save",
         [class: "btn btn-default",
          wrapper_class: ""]
     end
@@ -68,8 +69,51 @@ defmodule Brando.FormTest do
 
   #@opts [context: Brando.Form.Fields]
 
-  test "render form" do
-    #assert UserForm.get_form(action: :create, params: [], values: nil, errors: []) == ""
-    assert true
+  #test "render form" do
+  #  assert UserForm.get_form(action: :create, params: [], values: nil, errors: []) == ""
+  #end
+
+  test "render_fields/6" do
+    form_fields =
+      [submit: [type: :submit, text: "Save", class: "btn btn-default"],
+       avatar: [type: :file, label: "Avatar"],
+       fs123477010: [type: :fieldset_close],
+       editor: [type: :checkbox, in_fieldset: 2, label: "Editor", default: true],
+       administrator: [type: :checkbox, in_fieldset: 2, label: "Administrator", default: false],
+       fs34070328: [type: :fieldset, legend: "Permissions", row_span: 2],
+       email: [type: :email, required: true, label: "E-mail", placeholder: "E-mail"]]
+    f = UserForm.render_fields("user", form_fields, :create, [], nil, nil)
+    assert f ==
+      ["<div data-row-span=\"1\"><div data-field-span=\"1\" class=\"form-group required\">\n  <label for=\"user[email]\" class=\"\">E-mail</label><input name=\"user[email]\" type=\"email\" placeholder=\"E-mail\" />\n  \n</div>\n</div>",
+       "<fieldset><legend><br>Permissions</legend><div data-row-span=\"2\">",
+       "<div data-field-span=\"1\" class=\"form-group\">\n  <div class=\"checkbox\"><label for=\"user[administrator]\" class=\"\"></label><label for=\"user[administrator]\" class=\"\"><input name=\"user[administrator]\" type=\"checkbox\" />Administrator</label></div>\n  \n</div>\n",
+       "<div data-field-span=\"1\" class=\"form-group\">\n  <div class=\"checkbox\"><label for=\"user[editor]\" class=\"\"></label><label for=\"user[editor]\" class=\"\"><input name=\"user[editor]\" type=\"checkbox\" checked=\"checked\" />Editor</label></div>\n  \n</div>\n",
+       "</div></fieldset>",
+       "<div data-row-span=\"1\"><div data-field-span=\"1\" class=\"form-group\">\n  <label for=\"user[avatar]\" class=\"\">Avatar</label><input name=\"user[avatar]\" type=\"file\" />\n  \n</div>\n</div>",
+       "<div data-row-span=\"1\"><div data-field-span=\"1\" class=\"form-group\">\n  <input name=\"user[submit]\" type=\"submit\" class=\"btn btn-default\" />\n  \n</div>\n</div>"]
+  end
+
+  test "get_choices/1" do
+    assert get_choices(&UserForm.get_status_choices/0) == [[value: "0", text: "Valg 1"], [value: "1", text: "Valg 2"]]
+  end
+
+  test "get_value/2" do
+    values = %{"a" => "a val", "b" => "b val"}
+    assert get_value(values, :a) == "a val"
+    assert get_value(values, :c) == []
+    assert get_value([], :c) == []
+  end
+
+  test "get_errors/2" do
+    errors = [a: "error a", b: "error b"]
+    assert get_errors(errors, :a) == ["error a"]
+    errors = [a: "error a", b: "error b", a: "another error a"]
+    assert get_errors(errors, :a) == ["error a", "another error a"]
+  end
+
+  test "render_choices/4" do
+    assert render_choices(:create, [choices: &UserForm.get_status_choices/0],
+                          "val", nil) == ["<option value=\"0\">Valg 1</option>",
+                                          "<option value=\"1\">Valg 2</option>"]
   end
 end
