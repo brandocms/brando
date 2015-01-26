dba314cd9fd3fb056524220c4eebee2cdaf592b5
Trond Mjøen <trond@twined.net>
2015-01-26 14:33:47 +0100

    Test :radio update

--- test/brando/form_test.exs
+++ test/brando/form_test.exs
@@ -137,6 +137,7 @@ defmodule Brando.FormTest do
        administrator: [type: :checkbox, in_fieldset: 2, label: "Administrator", default: false],
        fs34070328: [type: :fieldset, row_span: 2],
        status: [type: :select, choices: &UserForm.get_status_choices/0, default: "1", label: "Status"],
+       status3: [type: :radio, choices: &UserForm.get_status_choices/0, default: "1", label: "Status"],
        email: [type: :email, required: true, label: "E-mail", placeholder: "E-mail"]]
     values = %Brando.Users.Model.User{avatar: "images/default/0.jpeg",
                                       email: "test@email.com",
@@ -150,6 +151,7 @@ defmodule Brando.FormTest do
     f = UserForm.render_fields("user", form_fields, :update, [], values, nil)
     assert f ==
       ["<div data-row-span=\"1\"><div data-field-span=\"1\" class=\"form-group required\">\n  <label for=\"user[email]\" class=\"\">E-mail</label><input name=\"user[email]\" type=\"email\" value=\"test@email.com\" placeholder=\"E-mail\" />\n  \n</div>\n</div>",
+       "<div data-row-span=\"1\"><div data-field-span=\"1\" class=\"form-group\">\n  <label for=\"user[status3]\" class=\"\">Status</label><div class=\"radio\"><label for=\"user[status3]\"></label><label for=\"user[status3]\"><input name=\"user[status3]\" type=\"radio\" value=\"1\" />Valg 1</label></div><div class=\"radio\"><label for=\"user[status3]\"></label><label for=\"user[status3]\"><input name=\"user[status3]\" type=\"radio\" value=\"2\" />Valg 2</label></div>\n  \n</div>\n</div>",
        "<div data-row-span=\"1\"><div data-field-span=\"1\" class=\"form-group\">\n  <label for=\"user[status]\" class=\"\">Status</label><select name=\"user[status]\" class=\"\"><option value=\"1\">Valg 1</option><option value=\"2\">Valg 2</option></select>\n  \n</div>\n</div>",
        "<fieldset><div data-row-span=\"2\">",
        "<div data-field-span=\"1\" class=\"form-group\">\n  <div class=\"checkbox\"><label for=\"user[administrator]\" class=\"\"></label><label for=\"user[administrator]\" class=\"\"><input name=\"user[administrator]\" type=\"checkbox\" />Administrator</label></div>\n  \n</div>\n",
