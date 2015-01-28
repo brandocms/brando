9f451d9177faed0248ff0216c998568bea07eeb1
Trond Mjøen <trond@twined.net>
2015-01-28 21:38:24 +0100

    Rewrite auth and user controllers to plugs
    Only until Phoenix adds support for passing options to controllers through the router

--- lib/brando/auth/controllers/auth_controller.ex
+++ lib/brando/auth/controllers/auth_controller.ex
@@ -1,35 +1,44 @@
 defmodule Brando.Auth.AuthController do
-  defmacro __using__(_) do
-    quote do
-      use Phoenix.Controller
+  use Phoenix.Controller
 
-      plug :put_layout, @layout
-      plug :action
+  plug :action
 
-      def login(conn, %{"user" => %{"email" => email, "password" => password}}) do
-        user = @model.get(email: email)
-        case @model.auth?(user, password) do
-          true ->   fetch_session(conn)
-                    |> put_session(:current_user, user)
-                    |> put_flash(:notice, "Innloggingen var vellykket")
-                    |> redirect(to: "/admin")
-          false ->  conn
-                    |> put_flash(:error, "Innloggingen feilet")
-                    |> redirect(to: "/login")
-        end
+  def init(options) do
+    options
+  end
 
-      end
+  def call(conn, opts) do
+    conn = conn
+    |> put_layout(opts[:layout])
+    |> assign(:model, opts[:model])
+    super(conn, action_name(conn))
+  end
 
-      def login(conn, _params) do
+  def login(conn, %{"user" => %{"email" => email, "password" => password}}) do
+    model = conn.assigns[:model]
+    user = model.get(email: email)
+    case model.auth?(user, password) do
+      true ->
+        user = Map.delete(user, :password)
+        fetch_session(conn)
+        |> put_session(:current_user, user)
+        |> put_flash(:notice, "Innloggingen var vellykket")
+        |> redirect(to: "/admin")
+      false ->
         conn
-        |> render(:login)
-      end
-
-      def logout(conn, _params) do
-        conn
-        |> delete_session(:current_user)
-        |> render(:logout)
-      end
+        |> put_flash(:error, "Innloggingen feilet")
+        |> redirect(to: "/login")
     end
+  end
+
+  def login(conn, _params) do
+    conn
+    |> render(:login)
+  end
+
+  def logout(conn, _params) do
+    conn
+    |> delete_session(:current_user)
+    |> render(:logout)
   end
 end
--- lib/brando/users/controllers/admin/user_controller.ex
+++ lib/brando/users/controllers/admin/user_controller.ex
@@ -1,102 +1,100 @@
 defmodule Brando.Users.Admin.UserController do
   @moduledoc """
   This is a boilerplate UserController.
-
-  ## Example:
-
-      @model MyApp.Model.User
-      @layout {MyApp.Admin.LayoutView, "admin.html"}
-      use Brando.Users.Admin.UserController
-
-  where `@layout` is the layout you want to use, and `@model` is your
-  user model.
   """
 
-  defmacro __using__(_) do
-    quote do
-      use Phoenix.Controller
+  use Phoenix.Controller
 
-      plug :put_layout, @layout
-      plug :action
+  import Brando.Plugs.Role
+  plug :check_role, :superuser when action in [:new, :create, :destroy]
+  plug :action
 
-      def index(conn, _params) do
-        conn
-        |> assign(:users, @model.all)
-        |> render("index.html")
-      end
+  def init(options) do
+    options
+  end
 
-      def show(conn, _params) do
-        conn
-        |> assign(:user, get_session(conn, :current_user))
-        |> render("show.html")
-      end
+  def call(conn, opts) do
+    conn = conn
+    |> put_layout(opts[:layout])
+    |> assign(:model, opts[:model])
+    super(conn, action_name(conn))
+  end
 
-      def new(conn, _params) do
-        case @model.has_role?(get_session(conn, :current_user), :superuser) do
-          true -> conn |> render("new.html")
-          false -> conn
+  def index(conn, _params) do
+    model = conn.assigns[:model]
+    conn
+    |> assign(:users, model.all)
+    |> render("index.html")
+  end
+
+  def show(conn, _params) do
+    conn
+    |> assign(:user, get_session(conn, :current_user))
+    |> render("show.html")
+  end
+
+  def new(conn, _params) do
+    conn |> render("new.html")
+  end
+
+  def edit(conn, %{"id" => user_id}) do
+    model = conn.assigns[:model]
+    form_data = model.get(id: String.to_integer(user_id))
+    conn
+    |> assign(:user, form_data)
+    |> assign(:id, user_id)
+    |> render(:edit)
+  end
+
+  def create(conn, %{"user" => form_data}) do
+    model = conn.assigns[:model]
+    created_user = model.create(form_data)
+    case created_user do
+      {:ok, created_user} ->
+        case model.check_for_uploads(created_user, form_data) do
+          {:ok, _val} -> conn |> put_flash(:notice, "Bilde lastet opp.")
+          {:errors, _errors} -> nil
+          :nouploads -> nil
         end
-      end
-
-      def edit(conn, %{"id" => user_id}) do
-        form_data = @model.get(id: String.to_integer(user_id))
+        conn
+        |> put_flash(:notice, "Bruker opprettet.")
+        |> redirect(to: Brando.get_helpers().admin_user_path(conn, :index))
+      {:error, errors} ->
         conn
         |> assign(:user, form_data)
+        |> assign(:errors, errors)
+        |> put_flash(:error, "Feil i skjema")
+        |> render(:new)
+    end
+  end
+
+  def create(conn, _params) do
+    conn |> render(:new)
+  end
+
+  def update(conn, %{"user" => form_data, "id" => user_id}) do
+    model = conn.assigns[:model]
+    user = model.get(id: String.to_integer(user_id))
+    case model.update(user, form_data) do
+      {:ok, _updated_user} ->
+        conn
+        |> put_flash(:notice, "Bruker oppdatert.")
+        |> redirect(to: Brando.get_helpers.admin_user_path(conn, :index))
+      {:error, errors} ->
+        conn
+        |> assign(:user, form_data)
+        |> assign(:errors, errors)
         |> assign(:id, user_id)
         |> render(:edit)
-      end
-
-      def create(conn, %{"user" => form_data}) do
-        created_user = @model.create(form_data)
-        case created_user do
-          {:ok, created_user} ->
-            case @model.check_for_uploads(created_user, form_data) do
-              {:ok, val} -> conn |> put_flash(:notice, "Bilde lastet opp.")
-              {:errors, _errors} -> nil
-              :nouploads -> nil
-            end
-            conn
-            |> put_flash(:notice, "Bruker opprettet.")
-            |> redirect(to: Brando.get_helpers().admin_user_path(conn, :index))
-          {:error, errors} ->
-            conn
-            |> assign(:user, form_data)
-            |> assign(:errors, errors)
-            |> put_flash(:error, "Feil i skjema")
-            |> render(:new)
-        end
-      end
-
-      def create(conn, _params) do
-        conn |> render(:new)
-      end
-
-      def update(conn, %{"user" => form_data, "id" => user_id}) do
-        user = @model.get(id: String.to_integer(user_id))
-        case @model.update(user, form_data) do
-          {:ok, updated_user} ->
-            conn
-            |> put_flash(:notice, "Bruker oppdatert.")
-            |> redirect(to: Brando.get_helpers.admin_user_path(conn, :index))
-          {:error, errors} ->
-            conn
-            |> assign(:user, form_data)
-            |> assign(:errors, errors)
-            |> assign(:id, user_id)
-            |> render(:edit)
-        end
-      end
-
-      def destroy(conn, %{"id" => user_id}) do
-        user = @model.get(id: String.to_integer(user_id))
-        @model.delete(user)
-        conn
-        |> put_flash(:notice, "Bruker #{user.username} slettet.")
-        |> redirect(to: Brando.get_helpers.admin_user_path(conn, :index))
-      end
-
-      defoverridable [index: 2, show: 2, new: 2, edit: 2,
-                      create: 2, update: 2, destroy: 2]
     end
+  end
+
+  def destroy(conn, %{"id" => user_id}) do
+    model = conn.assigns[:model]
+    user = model.get(id: String.to_integer(user_id))
+    model.delete(user)
+    conn
+    |> put_flash(:notice, "Bruker #{user.username} slettet.")
+    |> redirect(to: Brando.get_helpers.admin_user_path(conn, :index))
   end
 end
