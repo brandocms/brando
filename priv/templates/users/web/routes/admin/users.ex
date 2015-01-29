3ff09e2149ffa3b66a085e0b339265891e2ca21e
Trond Mjøen <trond@twined.net>
2015-01-29 12:36:12 +0100

    Update users routes macro

--- priv/templates/users/web/routes/admin/users.ex
+++ priv/templates/users/web/routes/admin/users.ex
@@ -1,24 +1,38 @@
 defmodule <%= application_name %>.Users.Admin.Routes do
   alias Phoenix.Router.Resource
+  #alias Phoenix.Router.Scope
 
-  defmacro users_resources(path, controller, opts, do: nested_context) do
-    add_users_resources path, controller, opts, do: nested_context
-  end
+  @doc """
+    Defines "RESTful" endpoints for a resource.
+    The given definition:
+        users_resources "/users"
+    will include routes to the following actions:
+      * `GET /users` => `:index`
+      * `GET /users/new` => `:new`
+      * `POST /users` => `:create`
+      * `GET /users/:id` => `:show`
+      * `GET /users/:id/edit` => `:edit`
+      * `PATCH /users/:id` => `:update`
+      * `PUT /users/:id` => `:update`
+      * `DELETE /users/:id` => `:destroy`
+    ## Options
+    This macro accepts a set of options:
+      * `:only` - a list of actions to generate routes for, for example: `[:show, :edit]`
+      * `:except` - a list of actions to exclude generated routes from, for example: `[:destroy]`
+      * `:param` - the name of the paramter for this resource, defaults to `"id"`
+      * `:name` - the prefix for this resource. This is used for the named helper
+        and as the prefix for the parameter in nested resources. The default value
+        is automatically derived from the controller name, i.e. `UserController` will
+        have name `"user"`
+      * `:as` - configures the named helper exclusively
+  """
 
-  defmacro users_resources(path, controller, do: nested_context) do
-    add_users_resources path, controller, [], do: nested_context
-  end
-
-  defmacro users_resources(path, controller, opts) do
-    add_users_resources path, controller, opts, do: nil
-  end
-
-  defmacro users_resources(path, controller) do
-    add_users_resources path, controller, [], do: nil
+  defmacro users_resources(path, opts) do
+    add_users_resources path, Brando.Users.Admin.UserController, opts, do: nil
   end
 
   defmacro users_resources(path) do
-    add_users_resources path, <%= application_name %>.Users.Admin.UserController, [], do: nil
+    add_users_resources path, Brando.Users.Admin.UserController, [], do: nil
   end
 
   defp add_users_resources(path, controller, options, do: context) do
@@ -27,7 +41,7 @@ defmodule <%= application_name %>.Users.Admin.Routes do
       parm = resource.param
       path = resource.path
       ctrl = resource.controller
-      opts = [as: resource.as]
+      opts = resource.route
 
       Enum.each resource.actions, fn action ->
         case action do
@@ -36,10 +50,10 @@ defmodule <%= application_name %>.Users.Admin.Routes do
           :new     -> get    "#{path}/ny",           ctrl, :new, opts
           :edit    -> get    "#{path}/:#{parm}/endre", ctrl, :edit, opts
           :create  -> post   "#{path}",               ctrl, :create, opts
-          :destroy -> delete "#{path}/:#{parm}",      ctrl, :destroy, opts
+          :delete  -> delete "#{path}/:#{parm}",      ctrl, :delete, opts
           :update  ->
             patch "#{path}/:#{parm}", ctrl, :update, opts
-            put   "#{path}/:#{parm}", ctrl, :update, as: nil
+            put   "#{path}/:#{parm}", ctrl, :update, Keyword.put(opts, :as, nil)
         end
       end
       get    "#{path}/profil",      ctrl, :profile, opts
