016e5df3545ac62a6a86aa12fbf6173d414bf954
Trond Mjøen <trond@twined.net>
2015-01-29 18:52:23 +0100

    Add Brando.Admin.LayoutView

---
+++ lib/brando/admin/views/layout_view.ex
@@ -0,0 +1,11 @@
+defmodule Brando.Admin.LayoutView do
+  use Brando.AdminView, root: Brando.config(:templates_path)
+
+  def get_menus do
+    modules = Brando.config(:use_modules)
+    colors = Brando.config(:menu_colors)
+    for {mod, color} <- Enum.zip(modules, colors) do
+      {color, Module.concat(mod, "Menu").get_menu}
+    end
+  end
+end
