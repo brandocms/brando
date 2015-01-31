8a7db0c5513b0ae2e9eff0b0ee76cf175ff6d696
Trond Mjøen <trond@twined.net>
2015-01-31 20:20:04 +0100

    Test auth controller

---
+++ test/brando/auth/controller_test.exs
@@ -0,0 +1,43 @@
+Code.require_file("router_helper.exs", Path.join([__DIR__, "..", ".."]))
+
+defmodule Brando.Auth.ControllerTest do
+  use ExUnit.Case
+  use Brando.Integration.TestCase
+  use Plug.Test
+  use RouterHelper
+  alias Brando.Users.Model.User
+
+  @params %{"avatar" => "", "role" => ["2", "4"],
+            "email" => "admin@gmail.com", "full_name" => "Admin Admin",
+            "password" => "finimeze", "status" => "1",
+            "submit" => "Submit", "username" => "zabuzasixu"}
+
+  @login %{"email" => "admin@gmail.com", "password" => "finimeze"}
+  @bad_login %{"email" => "bad@gmail.com", "password" => "finimeze"}
+
+  test "login get" do
+    conn = call_with_session(RouterHelper.TestRouter, :get, "/login")
+    assert conn.status == 200
+    assert conn.path_info == ["login"]
+    assert conn.resp_body =~ "<form"
+  end
+
+  test "login post ok" do
+    assert {:ok, _user} = User.create(@params)
+    conn = call_with_session(RouterHelper.TestRouter, :post, "/login", %{"user" => @login})
+    assert conn.status == 302
+    assert get_resp_header(conn, "Location") == ["/admin"]
+    %{phoenix_flash: flash} = conn.private
+    assert flash == %{"notice" => "Innloggingen var vellykket"}
+  end
+
+  test "login post failed" do
+    assert {:ok, _user} = User.create(@params)
+    conn = call_with_session(RouterHelper.TestRouter, :post, "/login", %{"user" => @bad_login})
+    assert conn.status == 302
+    assert get_resp_header(conn, "Location") == ["/login"]
+    %{phoenix_flash: flash} = conn.private
+    assert flash == %{"error" => "Innloggingen feilet"}
+  end
+
+end
