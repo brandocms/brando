ed297633891c51b98b38f442b4e8dd272a10d37d
Trond Mjøen <trond@twined.net>
2015-01-31 09:55:52 +0100

    Add tests

---
+++ test/brando/plugs/role_test.exs
@@ -0,0 +1,97 @@
+defmodule Brando.Plugs.RoleTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+  import Brando.Plugs.Role
+
+  defmodule RolePlugFail do
+    import Plug.Conn
+    import Phoenix.Controller
+    use Plug.Builder
+
+    plug Plug.Session,
+      store: :cookie,
+      key: "_test",
+      signing_salt: "signingsalt",
+      encryption_salt: "encsalt"
+    plug :fetch_session
+    plug :fetch_flash
+    plug :put_secret_key_base
+    plug :check_role, :superuser
+
+    def put_secret_key_base(conn, _) do
+      put_in conn.secret_key_base, "C590A24F0CCB864E01DD077CFF144EFEAAAB7835775438E414E9847A4EE8035D"
+    end
+  end
+
+  defmodule RolePlugFailsPerms do
+    import Plug.Conn
+    import Phoenix.Controller
+    use Plug.Builder
+
+    plug Plug.Session,
+      store: :cookie,
+      key: "_test",
+      signing_salt: "signingsalt",
+      encryption_salt: "encsalt"
+    plug :fetch_session
+    plug :fetch_flash
+    plug :put_secret_key_base
+    plug :put_current_user
+    plug :check_role, :superuser
+
+    def put_secret_key_base(conn, _) do
+      put_in conn.secret_key_base, "C590A24F0CCB864E01DD077CFF144EFEAAAB7835775438E414E9847A4EE8035D"
+    end
+
+    def put_current_user(conn, _) do
+      conn |> put_session(:current_user, %{role: []})
+    end
+  end
+
+  defmodule RolePlugSucceeds do
+    import Plug.Conn
+    import Phoenix.Controller
+    use Plug.Builder
+
+    plug Plug.Session,
+      store: :cookie,
+      key: "_test",
+      signing_salt: "signingsalt",
+      encryption_salt: "encsalt"
+    plug :fetch_session
+    plug :fetch_flash
+    plug :put_secret_key_base
+    plug :put_current_user
+    plug :check_role, :superuser
+
+    def put_secret_key_base(conn, _) do
+      put_in conn.secret_key_base, "C590A24F0CCB864E01DD077CFF144EFEAAAB7835775438E414E9847A4EE8035D"
+    end
+
+    def put_current_user(conn, _) do
+      conn |> put_session(:current_user, %{role: [:admin, :superuser]})
+    end
+  end
+
+  test "role fails" do
+    conn = conn(:get, "/", [])
+    |> assign(:secret_key_base, "asdf")
+    |> RolePlugFail.call([])
+    assert conn.status == 403
+  end
+
+  test "role succeeds" do
+    conn = conn(:get, "/", [])
+    |> assign(:secret_key_base, "asdf")
+    |> RolePlugSucceeds.call([])
+    assert conn.status == nil
+  end
+
+  test "role fails perms" do
+    conn = conn(:get, "/", [])
+    |> assign(:secret_key_base, "asdf")
+    |> RolePlugFailsPerms.call([])
+    assert conn.status == 403
+  end
+
+end
