1909a6cc7e38e21b6e93c9425116210d50b7ef64
Trond Mjøen <trond@twined.net>
2015-02-11 00:15:40 +0100

    Add some more fields to the post form

--- lib/brando/news/forms/post_form.ex
+++ lib/brando/news/forms/post_form.ex
@@ -5,11 +5,30 @@ defmodule Brando.News.Form.PostForm do
   """
   use Brando.Form
 
+  def get_language_choices do
+    [[value: "no", text: "Norsk"],
+     [value: "en", text: "English"]]
+  end
+
   form "post", [helper: :admin_post_path, class: "grid-form"] do
+    fieldset [row_span: 4] do
+      field :language, :select,
+        [required: true,
+        label: "Språk",
+        default: "no",
+        choices: &__MODULE__.get_language_choices/0]
+    end
+    fieldset [row_span: 4] do
+      field :featured, :checkbox,
+        [label: "Vektet post",
+        default: false]
+    end
     field :header, :text,
       [required: true,
        label: "Overskrift",
        placeholder: "Overskrift"]
+    field :lead, :textarea,
+      [label: "Ingress"]
     field :body, :textarea,
       [label: "Innhold"]
     submit "Lagre",
