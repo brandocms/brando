e844c43cba1b0bfbeabb27cb8dfb796a735a851a
Trond Mjøen <trond@twined.net>
2015-02-12 10:17:35 +0100

    Return string

--- lib/brando/villain.ex
+++ lib/brando/villain.ex
@@ -5,12 +5,13 @@ defmodule Villain do
   otp_app's config.exs.
   Returns HTML.
   """
+  @spec parse(String.t) :: String.t
   def parse(json) do
     {:ok, data} = Poison.decode(json)
     parser_module = Brando.config(Villain)[:parser]
     html = Enum.reduce(data, [], fn(d, acc) ->
       [apply(parser_module, String.to_atom(d["type"]), [d["data"]])|acc]
     end)
-    Enum.reverse(html)
+    html |> Enum.reverse |> Enum.join
   end
 end
