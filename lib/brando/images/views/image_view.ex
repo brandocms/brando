66e476b90966e27d4f76e3a1cb74ddf36d03b6bc
Trond Mjøen <trond@twined.net>
2015-02-16 14:02:54 +0100

    Add view

---
+++ lib/brando/images/views/image_view.ex
@@ -0,0 +1,7 @@
+defmodule Brando.Images.Admin.ImageView do
+  @moduledoc """
+  View for the Brando Images module.
+  """
+  #alias Brando.Images.Form.PostForm
+  use Brando.AdminView, root: "templates"
+end
