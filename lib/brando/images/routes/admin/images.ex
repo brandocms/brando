7a7a0bec040fd0284892380ca6e7db6889b8bebe
Trond Mjøen <trond@twined.net>
2015-02-16 14:03:03 +0100

    Add prelim routes

---
+++ lib/brando/images/routes/admin/images.ex
@@ -0,0 +1,35 @@
+defmodule Brando.Images.Admin.Routes do
+  alias Phoenix.Router.Resource
+  alias Brando.Images.Admin.ImageController
+  alias Brando.Images.Admin.ImageSeriesController
+  alias Brando.Images.Admin.ImageCategoriesController
+  alias Brando.Images.Model.Image
+  alias Brando.Images.Model.ImageSeries
+  alias Brando.Images.Model.ImageCategory
+
+  defmacro image_resources(path, opts \\ []) do
+    add_resources(path, opts)
+  end
+
+  defp add_resources(path, opts) do
+    options = []
+    options = Keyword.put(options, :private, quote(do: %{image_model: unquote(Keyword.get(opts, :image_model) || Image)}))
+    options = Keyword.put(options, :private, quote(do: %{series_model: unquote(Keyword.get(opts, :series_model) || ImageSeries)}))
+    options = Keyword.put(options, :private, quote(do: %{category_model: unquote(Keyword.get(opts, :category_model) || ImageCategory)}))
+
+    quote do
+      image_ctrl = ImageController
+      series_ctrl = ImageSeriesController
+      categories_ctrl = ImageCategoriesController
+      resource = Resource.build(unquote(path), image_ctrl, unquote(options))
+      parm = resource.param
+      path = resource.path
+      ctrl = resource.controller
+      opts = resource.route
+
+      get    "#{path}",                   ctrl, :index, opts
+      get    "#{path}/serier",            series_ctrl, :index, opts
+      get    "#{path}/kategorier",        categories_ctrl, :index, opts
+    end
+  end
+end
