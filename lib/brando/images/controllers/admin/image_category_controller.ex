744907a5e4ffea13696cb77f2e240a7fdc1bc3b0
Trond Mjøen <trond@twined.net>
2015-02-27 11:03:56 +0100

    Add create and delete_confirm to image_category_controller

--- lib/brando/images/controllers/admin/image_category_controller.ex
+++ lib/brando/images/controllers/admin/image_category_controller.ex
@@ -2,7 +2,6 @@ defmodule Brando.Images.Admin.ImageCategoryController do
   @moduledoc """
   Controller for the Brando ImageCategory module.
   """
-  alias Brando.Images.Model.ImageCategory
   use Phoenix.Controller
 
   plug :action
@@ -19,5 +18,33 @@ defmodule Brando.Images.Admin.ImageCategoryController do
   def new(conn, _params) do
     conn
     |> render(:new)
+  end
+
+  @doc false
+  def create(conn, %{"imagecategory" => imagecategory}) do
+    require Logger
+    Logger.debug(inspect(Brando.HTML.current_user(conn)))
+    model = conn.private[:category_model]
+    case model.create(imagecategory, Brando.HTML.current_user(conn)) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:notice, "Kategori opprettet.")
+        |> redirect(to: router_module(conn).__helpers__.admin_image_category_path(conn, :index))
+      {:error, errors} ->
+        conn
+        |> assign(:imagecategory, imagecategory)
+        |> assign(:errors, errors)
+        |> put_flash(:error, "Feil i skjema")
+        |> render(:new)
+    end
+  end
+
+  @doc false
+  def delete_confirm(conn, %{"id" => id}) do
+    model = conn.private[:category_model]
+    record = model.get(id: id)
+    conn
+    |> assign(:record, record)
+    |> render(:delete_confirm)
   end
 end
