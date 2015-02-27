8a66750e45d92ff2388c0a50c101506c8c8c86ab
Trond Mjøen <trond@twined.net>
2015-02-27 11:04:59 +0100

    Add image category logic

--- lib/brando/images/models/image_category.ex
+++ lib/brando/images/models/image_category.ex
@@ -11,6 +11,27 @@ defmodule Brando.Images.Model.ImageCategory do
   alias Brando.Users.Model.User
   alias Brando.Images.Model.ImageSeries
 
+  def __name__(:singular), do: "bildekategori"
+  def __name__(:plural), do: "bildekategorier"
+
+  def __str__(model) do
+    "#{model.name}"
+  end
+
+  use Linguist.Vocabulary
+  locale "no", [
+    model: [
+      id: "ID",
+      name: "Navn",
+      slug: "URL-tamp",
+      cfg: "Konfigurasjon",
+      creator: "Opprettet av",
+      image_series: "Bildeserie",
+      inserted_at: "Opprettet",
+      updated_at: "Oppdatert"
+    ]
+  ]
+
   schema "imagecategories" do
     field :name, :string
     field :slug, :string
@@ -18,6 +39,80 @@ defmodule Brando.Images.Model.ImageCategory do
     belongs_to :creator, User
     has_many :image_series, ImageSeries
     timestamps
+  end
+
+  @doc """
+  Casts and validates `params` against `model` to create a valid
+  changeset when action is :create.
+
+  ## Example
+
+      model_changeset = changeset(%__MODULE__{}, :create, params)
+
+  """
+  @spec changeset(t, atom, Keyword.t | Options.t) :: t
+  def changeset(model, :create, params) do
+    params
+    |> cast(model, ~w(name slug creator_id), ~w(cfg))
+  end
+
+  @doc """
+  Casts and validates `params` against `model` to create a valid
+  changeset when action is :update.
+
+  ## Example
+
+      model_changeset = changeset(%__MODULE__{}, :update, params)
+
+  """
+  @spec changeset(t, atom, Keyword.t | Options.t) :: t
+  def changeset(model, :update, params) do
+    params
+    |> cast(model, [], ~w(name slug cfg creator_id))
+  end
+
+  @doc """
+  Create a changeset for the model by passing `params`.
+  If valid, generate a hashed password and insert model to Repo.
+  If not valid, return errors from changeset
+  """
+  def create(params, current_user) do
+    params = Utils.Model.put_creator(params, current_user)
+    model_changeset = changeset(%__MODULE__{}, :create, params)
+    case model_changeset.valid? do
+      true ->
+        inserted_model = Brando.get_repo().insert(model_changeset)
+        {:ok, inserted_model}
+      false ->
+        {:error, model_changeset.errors}
+    end
+  end
+
+  @doc """
+  Create an `update` changeset for the model by passing `params`.
+  If password is in changeset, hash and insert in changeset.
+  If valid, update model in Repo.
+  If not valid, return errors from changeset
+  """
+  def update(model, params) do
+    model_changeset = changeset(model, :update, params)
+    case model_changeset.valid? do
+      true ->
+        {:ok, Brando.get_repo().update(model_changeset)}
+      false ->
+        {:error, model_changeset.errors}
+    end
+  end
+
+  @doc """
+  Get model from DB by `id`
+  """
+  def get(id: id) do
+    from(m in __MODULE__,
+         where: m.id == ^id,
+         preload: [:creator, :image_series],
+         limit: 1)
+    |> Brando.get_repo.one!
   end
 
   @doc """
