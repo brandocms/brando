e764359c718a34eb0b17762426757105ab88b867
Trond Mjøen <trond@twined.net>
2015-02-27 19:51:31 +0100

    delete and delete_confirm image_series

--- lib/brando/images/controllers/admin/image_series_controller.ex
+++ lib/brando/images/controllers/admin/image_series_controller.ex
@@ -19,4 +19,25 @@ defmodule Brando.Images.Admin.ImageSeriesController do
     conn
     |> render(:new)
   end
+
+  @doc false
+  def delete_confirm(conn, %{"id" => id}) do
+    require Logger
+    Logger.debug(inspect(conn.private))
+    model = conn.private[:series_model]
+    record = model.get!(id: id)
+    conn
+    |> assign(:record, record)
+    |> render(:delete_confirm)
+  end
+
+  @doc false
+  def delete(conn, %{"id" => id}) do
+    model = conn.private[:series_model]
+    record = model.get!(id: id)
+    model.delete(record)
+    conn
+    |> put_flash(:notice, "#{Brando.HTML.model_name(record, :singular)} #{model.__str__(record)} slettet.")
+    |> redirect(to: router_module(conn).__helpers__.admin_image_path(conn, :index))
+  end
 end
