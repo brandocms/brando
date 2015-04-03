8e8bcc4cb8c8ad389a1cb4fc6d9e003c6d640765
Trond Mjøen <trond@twined.net>
2015-04-03 16:58:48 +0200

    Rewrite Brando.Type.Image. Now is a struct with sizes.

--- lib/brando/types/image.ex
+++ lib/brando/types/image.ex
@@ -3,22 +3,34 @@ defmodule Brando.Type.Image do
   Defines a type for an image field.
   """
 
+  defstruct title: nil,
+            credits: nil,
+            path: nil,
+            sizes: %{},
+            optimized: false
+
   @behaviour Ecto.Type
 
   @doc """
   Returns the internal type representation of our `Role` type for pg
   """
-  def type, do: :string
+  def type, do: :json
 
   @doc """
   Cast should return OUR type no matter what the input.
   """
-  def cast(val), do: {:ok, val}
+  def cast(val) when is_binary(val) do
+    val = Poison.decode!(val, as: Brando.Type.Image, keys: :atoms!)
+    {:ok, val}
+  end
+  def cast(val) when is_map(val) do
+    {:ok, val}
+  end
 
   @doc """
   Integers are never considered blank
   """
-  def blank?(_), do: false
+  def blank?(_), do: %Brando.Type.Image{}
 
   @doc """
   When loading `roles` from the database, we are guaranteed to
@@ -26,6 +38,8 @@ defmodule Brando.Type.Image do
   just return it to be stored in the model struct.
   """
   def load(val) do
+    val = Poison.decode!(val, as: Brando.Type.Image, keys: :atoms!)
+    if val == nil, do: val = %Brando.Type.Image{}
     {:ok, val}
   end
 
@@ -33,5 +47,8 @@ defmodule Brando.Type.Image do
   When dumping data to the database we expect a `list`, but check for
   other options as well.
   """
-  def dump(val), do: {:ok, val}
+  def dump(val) do
+    val = Poison.encode!(val)
+    {:ok, val}
+  end
 end
