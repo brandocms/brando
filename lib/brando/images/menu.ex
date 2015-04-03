0f5a28b01f2907285db5e7f4e959b6d132ca5a15
Trond Mjøen <trond@twined.net>
2015-04-03 22:13:27 +0200

    Fix incorrect menu entries

--- lib/brando/images/menu.ex
+++ lib/brando/images/menu.ex
@@ -8,6 +8,5 @@ defmodule Brando.Images.Menu do
   menu "Bilder", %{name: "Bilder", anchor: "bilder", icon: "fa fa-picture-o icon",
                     submenu: [%{name: "Oversikt", url: {:admin_image_path, :index}},
                               %{name: "Legg til kategori", url: {:admin_image_category_path, :new}},
-                              %{name: "Legg til serie", url: {:admin_post_path, :new}},
-                              %{name: "Last opp bilder", url: {:admin_post_path, :new}}]}
+                              %{name: "Legg til serie", url: {:admin_image_series_path, :new}}]}
 end
