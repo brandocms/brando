1544b03d065899bdaaefb01aed90b309b428e312
Trond Mjøen <trond@twined.net>
2015-04-08 11:20:04 +0200

    Strip out menu items

--- lib/brando/images/menu.ex
+++ lib/brando/images/menu.ex
@@ -6,7 +6,5 @@ defmodule Brando.Images.Menu do
   use Brando.Menu
 
   menu "Bilder", %{name: "Bilder", anchor: "bilder", icon: "fa fa-picture-o icon",
-                    submenu: [%{name: "Oversikt", url: {:admin_image_path, :index}},
-                              %{name: "Legg til kategori", url: {:admin_image_category_path, :new}},
-                              %{name: "Legg til serie", url: {:admin_image_series_path, :new}}]}
+                    submenu: [%{name: "Oversikt", url: {:admin_image_path, :index}}]}
 end
