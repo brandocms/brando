f58169ed9b0a56248523b0151405c16b86b55dae
Trond Mjøen <trond@twined.net>
2015-04-08 11:17:29 +0200

    Split out activate_tab

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -4,16 +4,18 @@ $(document).ready(function () {
   });
 
   $('.accordion-tabs-minimal').on('click', 'li > a', function(event) {
-    if (!$(this).hasClass('is-active')) {
-      event.preventDefault();
-      var accordionTabs = $(this).closest('.accordion-tabs-minimal');
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
-
-      $(this).next().toggleClass('is-open').toggle();
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
-    } else {
-      event.preventDefault();
-    }
+    event.preventDefault();
+    activate_tab(this);
   });
 });
+
+function activate_tab(obj) {
+  if (!$(obj).hasClass('is-active')) {
+    var accordionTabs = $(obj).closest('.accordion-tabs-minimal');
+    accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+    $(obj).next().toggleClass('is-open').toggle();
+    accordionTabs.find('.is-active').removeClass('is-active');
+    $(obj).addClass('is-active');
+  }
+}
