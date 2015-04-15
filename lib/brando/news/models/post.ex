0db95b0c8b38a1b86b02f8ec815241b5021f969e
Trond Mjøen <trond@twined.net>
2015-04-15 10:25:17 +0200

    Use pipeline

--- lib/brando/news/models/post.ex
+++ lib/brando/news/models/post.ex
@@ -178,10 +178,10 @@ defmodule Brando.News.Model.Post do
   Get all posts. Ordered by `id`. Preload :creator.
   """
   def all do
-    q = from m in __MODULE__,
+    (from m in __MODULE__,
         order_by: [asc: m.status, desc: m.inserted_at],
-        preload: [:creator]
-    Brando.get_repo.all(q)
+        preload: [:creator])
+    |> Brando.get_repo.all
   end
 
   #
