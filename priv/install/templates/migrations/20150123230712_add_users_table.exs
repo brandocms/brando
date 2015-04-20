febc24ba8ba59660b386fccb8620d759b4d413d8
Trond Mjøen <trond@twined.net>
2015-04-20 15:25:14 +0200

    Add users migration

---
+++ priv/install/templates/migrations/20150123230712_add_users_table.exs
@@ -0,0 +1,24 @@
+defmodule <%= application_module %>.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def up do
+    create table(:users) do
+      add :username,      :text
+      add :full_name,     :text
+      add :email,         :text
+      add :password,      :text
+      add :avatar,        :text
+      add :role,          :integer
+      add :last_login,    :datetime
+      timestamps
+    end
+    create index(:users, [:username], unique: true)
+    create index(:users, [:email], unique: true)
+  end
+
+  def down do
+    drop table(:users)
+    drop index(:users, [:username], unique: true)
+    drop index(:users, [:email], unique: true)
+  end
+end
