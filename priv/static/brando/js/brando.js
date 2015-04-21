963c7b4350720c39e3510cfb2757b0d916e0d0ef
Trond Mjøen <trond@twined.net>
2015-04-21 22:58:19 +0200

    Compiled js

--- priv/static/brando/js/brando.js
+++ priv/static/brando/js/brando.js
@@ -145,10 +145,22 @@ var Images = (function () {
     _createClass(Images, null, [{
         key: "setup",
         value: function setup() {
-            if ($("body#images").length) {
-                this.getHash();
-                this.deleteListener();
-                this.imageSelectionListener();
+            switch ($("body").attr("data-script")) {
+                case "images-index":
+                    this.getHash();
+                    this.deleteListener();
+                    this.imageSelectionListener();
+                    return;
+                case "images-sort":
+                    var el = document.getElementById("sortable");
+                    this.sortable = new Sortable(el, {
+                        animation: 150,
+                        ghostClass: "sortable-ghost",
+                        onUpdate: function onUpdate(e) {
+                            $("#sort-post").removeClass("btn-default", "btn-success").addClass("btn-warning").html("Lagre ny rekkefølge");
+                        } });
+                    this.sortListener();
+                    return;
             }
         }
     }, {
@@ -228,6 +240,30 @@ var Images = (function () {
                 }
                 imagePool = [];
             }
+        }
+    }, {
+        key: "sortSuccess",
+        value: function sortSuccess(data) {
+            if (data.status == 200) {
+                $("#sort-post").removeClass("btn-warning").addClass("btn-success").html("Lagret rekkefølge!");
+            }
+        }
+    }, {
+        key: "sortListener",
+        value: function sortListener() {
+            var _this = this;
+            $("#sort-post").on("click", function (e) {
+                e.preventDefault();
+                $(this).removeClass("btn-default").addClass("btn-warning").html("Lagrer ...");
+                $.ajax({
+                    headers: { Accept: "application/json; charset=utf-8" },
+                    type: "POST",
+                    url: "",
+                    data: {
+                        order: _this.sortable.toArray()
+                    },
+                    success: _this.sortSuccess });
+            });
         }
     }]);
 
