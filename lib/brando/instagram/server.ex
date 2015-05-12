65fd54f4a28d2de6a57b8128f0e229c9d2a30e10
Trond Mjøen <trond@twined.net>
2015-05-12 18:31:24 +0200

    Catch more errors in terminate. Send initial poll

--- lib/brando/instagram/server.ex
+++ lib/brando/instagram/server.ex
@@ -1,28 +1,60 @@
 defmodule Brando.Instagram.Server do
   @moduledoc """
-  Polls Instagram's API according to config's `interval` option.
+  Polls Instagram API.
+
+  See Brando.Instagram for more info
   """
+
   use GenServer
   alias Brando.Instagram
   alias Brando.Instagram.API
 
+  @doc false
   def start_link(server_name) do
     :gen_server.start_link({:local, server_name}, __MODULE__, [], [])
   end
 
+  @doc false
   def init(_) do
     Process.flag(:trap_exit, true)
+    :timer.send_after(5000, :poll)
+    send(self(), :poll)
     {:ok, timer} = :timer.send_interval(Instagram.cfg(:interval), :poll)
     {:ok, {timer, :blank}}
   end
 
+  @doc false
   def handle_info(:poll, {timer, filter}) do
     {:ok, filter} = API.fetch(filter)
     {:noreply, {timer, filter}}
   end
 
+  @doc false
   def terminate(:shutdown, {timer, _}) do
     :timer.cancel(timer)
+    :ok
+  end
+
+  @doc false
+  def terminate({%HTTPoison.Error{reason: :connect_timeout}, [_|_]}, {_, _}) do
+    Brando.SystemChannel.log(:error, "InstagramServer: Tilkoblingen brukte for lang tid.")
+    :ok
+  end
+
+  @doc false
+  def terminate({%HTTPoison.Error{reason: :econnrefused}, [_|_]}, {_, _}) do
+    Brando.SystemChannel.log(:error, "InstagramServer: Tilkoblingen ble nektet fra server.")
+    :ok
+  end
+
+  @doc false
+  def terminate({%HTTPoison.Error{reason: :nxdomain}, [_|_]}, {_, _}) do
+    Brando.SystemChannel.log(:error, "InstagramServer: Kunne ikke koble til pga DNS problemer.")
+    :ok
+  end
+
+  @doc false
+  def terminate(reason, state) do
     :ok
   end
 end
