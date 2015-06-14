e6523c70695cca940d915b1102bba69f187865e1
Trond Mjøen <trond@twined.net>
2015-06-14 19:53:22 +0200

    Use new image format. Check for slug instead of ID

--- lib/brando/villain/controller.ex
+++ lib/brando/villain/controller.ex
@@ -16,23 +16,31 @@ defmodule Brando.Villain.Controller do
 
 
   """
+
+
   defmacro __using__(options) do
     image_model = Keyword.fetch!(options, :image_model)
     series_model = Keyword.fetch!(options, :series_model)
     quote do
+      import Ecto.Query
       @doc false
-      def browse_images(conn, %{"id" => series_slug} = params) do
-        image_series = unquote(series_model).get(slug: series_slug)
+      def browse_images(conn, %{"slug" => series_slug} = params) do
+        image_series = unquote(series_model)
+        |> preload([:image_category, :images])
+        |> Brando.repo.get_by(slug: series_slug)
+
         image_list = Enum.map(image_series.images, fn image ->
-          %{src: Brando.HTML.media_url(image.image),
-            thumb: Brando.HTML.media_url(Brando.Images.Utils.size_dir(image.image, :thumb))}
+          %{src: Brando.HTML.media_url(image.image.path),
+            thumb: Brando.HTML.media_url(Brando.HTML.img(image.image, :thumb))}
         end)
         json(conn, %{status: "200", images: image_list})
       end
 
       @doc false
-      def upload_image(conn, %{"uid" => uid, "id" => series_slug} = params) do
-        series = unquote(series_model).get(slug: series_slug)
+      def upload_image(conn, %{"uid" => uid, "slug" => series_slug} = params) do
+        series = unquote(series_model)
+        |> preload(:image_category)
+        |> Brando.repo.get_by(slug: series_slug)
         cfg = series.image_category.cfg || Brando.config(Brando.Images)[:default_config]
         opts = Map.put(%{}, "image_series_id", series.id)
         {:ok, image} = unquote(image_model).check_for_uploads(params, Brando.HTML.current_user(conn), cfg, opts)
@@ -62,7 +70,9 @@ defmodule Brando.Villain.Controller do
       @doc false
       def image_info(conn, %{"form" => form, "id" => id, "uid" => uid}) do
         form = URI.decode_query(form)
-        {:ok, image} = unquote(image_model).update_image_meta(unquote(image_model).get(id: id), form["title"], form["credits"])
+        image = unquote(image_model)
+        |> Brando.repo.get_by(id: id)
+        {:ok, image} = unquote(image_model).update_image_meta(image, form["title"], form["credits"])
         json conn, %{status: 200, id: id, uid: uid,
                      title: image.image.title, credits: image.image.credits}
       end
