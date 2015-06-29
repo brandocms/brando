9569d656a5278c90f11db70225a2a1152292e515
Trond Mjøen <trond@twined.net>
2015-06-29 19:20:15 +0200

    Add instagram status

--- web/channels/stats_channel.ex
+++ web/channels/stats_channel.ex
@@ -20,12 +20,21 @@ defmodule Brando.StatsChannel do
   end
 
   def handle_info(:update, socket) do
+    instagram_status =
+      try do
+        Keyword.get(Brando.config(Brando.Instagram), :server_name)
+        |> Process.whereis
+        |> Process.alive?
+      rescue
+        _ -> false
+      end
     mem_list =
       :erlang.memory(@info_memory)
       |> Keyword.values
     :erlang.send_after(@interval, self, :update)
     push socket, "update", %{total_memory: Enum.at(mem_list, 0),
-                             atom_memory: Enum.at(mem_list, 2)}
+                             atom_memory: Enum.at(mem_list, 2),
+                             instagram_status: instagram_status}
     {:noreply, socket}
   end
 end
