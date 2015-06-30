06664b4c7bc9658e57f99cc30f329fbd7371d9cd
Trond Mjøen <trond@twined.net>
2015-06-30 20:39:27 +0200

    Remove action

--- priv/install/templates/web/controllers/lockdown_controller.ex
+++ priv/install/templates/web/controllers/lockdown_controller.ex
@@ -2,8 +2,6 @@ defmodule <%= application_module %>.LockdownController do
   use <%= application_module %>.Web, :controller
   import Ecto.Query
 
-  plug :action
-
   def index(conn, params) do
     conn
     |> put_layout({<%= application_module %>.LayoutView, "lockdown.html"})
