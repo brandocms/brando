1827416b0ce6b634646f5af3cdfd89e9075c8419
Trond Mjøen <trond@twined.net>
2015-06-30 20:38:30 +0200

    Call media_url once

--- web/views/instagram_view.ex
+++ web/views/instagram_view.ex
@@ -5,20 +5,20 @@ defmodule Brando.Admin.InstagramView do
   use Brando.Web, :view
 
   def show_grouped_images(images) do
-    require Logger
     approved_header = "<h3 class=\"negative\">Godkjente bilder</h3><div class=\"image-selection-pool approved\">"
     rejected_header = "<h3 class=\"negative\">Avviste bilder</h3><div class=\"image-selection-pool rejected\">"
     deleted_header  = "<h3 class=\"negative\">Slettede bilder</h3><div class=\"image-selection-pool deleted\">"
     div_close       = "</div>"
+    prefix          = media_url()
 
     {_, {approved, rejected, deleted}} = Enum.map_reduce images, {"", "", ""}, fn(img, {approved, rejected, deleted}) ->
       case img.status do
         :approved ->
-          approved = approved <> "<img data-id=\"#{img.id}\" data-status=\"#{img.status}\" src=\"#{media_url(img(img.image, :thumb))}\" />"
+          approved = approved <> "<img data-id=\"#{img.id}\" data-status=\"#{img.status}\" src=\"#{img(img.image, :thumb, prefix: prefix)}\" />"
         :rejected ->
-          rejected = rejected <> "<img data-id=\"#{img.id}\" data-status=\"#{img.status}\" src=\"#{media_url(img(img.image, :thumb))}\" />"
+          rejected = rejected <> "<img data-id=\"#{img.id}\" data-status=\"#{img.status}\" src=\"#{img(img.image, :thumb, prefix: prefix)}\" />"
         :deleted ->
-          deleted = deleted <> "<img data-id=\"#{img.id}\" data-status=\"#{img.status}\" src=\"#{media_url(img(img.image, :thumb))}\" />"
+          deleted = deleted <> "<img data-id=\"#{img.id}\" data-status=\"#{img.status}\" src=\"#{img(img.image, :thumb, prefix: prefix)}\" />"
       end
       {img, {approved, rejected, deleted}}
     end
