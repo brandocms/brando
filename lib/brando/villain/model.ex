ee8900745c6594e243a5d2b6642c313f44d309a2
Trond Mjøen <trond@twined.net>
2015-07-18 16:10:14 +0200

    Check for missing villain images

--- lib/brando/villain/model.ex
+++ lib/brando/villain/model.ex
@@ -33,6 +33,43 @@ defmodule Brando.Villain.Model do
           changeset
         end
       end
+
+      @doc """
+      Check all posts for missing images
+      """
+      def check_posts_for_missing_images do
+        posts = __MODULE__ |> Brando.repo.all
+        result = Enum.reduce posts, [], fn(post, acc) ->
+          check_post_for_missing_images(post)
+        end
+        case result do
+          [] -> false
+          result -> result
+        end
+      end
+
+      @doc """
+      Check post's villain data field for missing images
+      """
+      def check_post_for_missing_images(post) do
+        image_blocks =
+          post.data
+          |> Enum.filter(fn(block) -> block["type"] == "image" end)
+
+        Enum.reduce image_blocks, [], fn(image_block, acc) ->
+          reduced_block =
+            Enum.reduce image_block["data"]["sizes"], [], fn({_size, path}, acc) ->
+              case File.exists?(Path.join(["priv", path])) do
+                true  -> acc
+                false -> {:missing, post, path}
+              end
+            end
+          case reduced_block do
+            []  -> acc
+            res -> [res|acc]
+          end
+        end
+      end
     end
   end
 
