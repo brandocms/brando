af95a6d007c709d7936ee62d729a23f7a1f77544
Trond Mjøen <trond@twined.net>
2015-10-14 14:46:37 +0200

    Add missing slideshow parser

--- priv/install/templates/web/villain/parser.ex
+++ priv/install/templates/web/villain/parser.ex
@@ -4,6 +4,9 @@ defmodule <%= application_module %>.Villain.Parser do
   """
   @behaviour Brando.Villain.Parser
 
+  import Brando.Utils, only: [img_url: 3, media_url: 0]
+  import Ecto.Query
+
   @doc """
   Convert header to HTML
   """
@@ -77,6 +80,31 @@ defmodule <%= application_module %>.Villain.Parser do
           #{credits}
         </div>
       </div>
+    </div>
+    """
+  end
+
+  @doc """
+  Slideshow
+  """
+  def slideshow(%{"imageseries" => series_slug, "size" => size}) do
+    q = from is in Brando.ImageSeries,
+             join: c in assoc(is, :image_category),
+             join: i in assoc(is, :images),
+             where: c.slug == "slideshows" and is.slug == ^series_slug,
+             order_by: i.sequence,
+             preload: [image_category: c, images: i]
+    series = Brando.repo.one(q)
+
+    images = Enum.map_join series.images, "\n", fn(img) ->
+      src = img_url(img.image, String.to_atom(size), [prefix: media_url()])
+      ~s(<li><img src="#{src}" /></li>)
+    end
+    """
+    <div class="flexslider flex-viewport">
+      <ul class="slides">
+        #{images}
+      </ul>
     </div>
     """
   end
