e627917877dbaa6259c8deb848cc1b4d6faa1e03
Trond Mjøen <trond@twined.net>
2015-10-14 14:54:23 +0200

    Remove LoggerFileBackend

--- priv/install/templates/config/brando.exs
+++ priv/install/templates/config/brando.exs
@@ -2,13 +2,6 @@
 
 use Mix.Config
 
-config :logger,
-  backends: [{LoggerFileBackend, :error_log}, :console]
-
-config :logger, :error_log,
-  path: "logs/app_error.log",
-  level: :error
-
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
