f0dcd6e69c7c4a56ff7f1c1285c1efcdd0f540b3
Trond Mjøen <trond@twined.net>
2015-10-18 21:08:46 +0200

    Fix gettext install file

--- priv/templates/brando.install/lib/gettext.ex
+++ priv/templates/brando.install/lib/gettext.ex
@@ -1,7 +1,7 @@
-defmodule <%= base %>.Gettext do
+defmodule <%= application_module %>.Gettext do
   use Gettext, otp_app: :<%= application_name %>, priv: "priv/gettext/frontend"
 end
 
-defmodule <%= base %>.Backend.Gettext do
+defmodule <%= application_module %>.Backend.Gettext do
   use Gettext, otp_app: :<%= application_name %>, priv: "priv/gettext/backend"
 end
