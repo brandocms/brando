dd67570007b901efd2cf4d4b770af13a72c7454e
Trond Mjøen <trond@twined.net>
2015-10-22 00:55:46 +0200

    Prepare 0.6.0 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Brando.Mixfile do
   use Mix.Project
 
-  @version "0.6.0-dev"
+  @version "0.6.0"
   @description "Boilerplate for Twined applications. Experimental, do not use."
 
   def project do
