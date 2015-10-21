982b644cfafc0d6181d157b43b7b46c673984410
Trond Mjøen <trond@twined.net>
2015-10-21 11:03:45 +0200

    Toggle class when opening subpages

--- priv/static/vendor/js/brando.js
+++ priv/static/vendor/js/brando.js
@@ -585,6 +585,7 @@ var Pages = (function () {
         value: function onClickExpandButton(e, elem) {
             e.preventDefault();
             $(elem).toggleClass('active');
+            $(elem).find('i').toggleClass('fa-plus').toggleClass('fa-times');
             $('tr.child[data-parent-id=' + $(elem).attr('data-id') + ']').toggleClass('hidden');
         }
     }]);
