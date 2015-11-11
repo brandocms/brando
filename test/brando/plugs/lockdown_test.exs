e37bd169a1d121db7b26bec42babae589250c1bc
Trond Mjøen <trond@twined.net>
2015-11-11 14:51:30 +0100

    Test that lockdown auth fails

--- test/brando/plugs/lockdown_test.exs
+++ test/brando/plugs/lockdown_test.exs
@@ -55,6 +55,34 @@ defmodule Brando.Plug.LockdownTest do
     end
   end
 
+  defmodule LockdownPlugAuthFail do
+    import Plug.Conn
+    import Phoenix.Controller
+    use Plug.Builder
+
+    plug Plug.Session,
+      store: :cookie,
+      key: "_test",
+      signing_salt: "signingsalt",
+      encryption_salt: "encsalt"
+    plug :fetch_session
+    plug :fetch_flash
+    plug :put_secret_key_base
+    plug :put_current_user
+    plug Brando.Plug.Lockdown, [
+           layout: {MyApp.LockdownLayoutView, "lockdown.html"},
+           view: {MyApp.LockdownView, "lockdown.html"}]
+
+    def put_secret_key_base(conn, _) do
+      put_in conn.secret_key_base,
+        "C590A24F0CCB864E01DD077CFF144EFEAAAB7835775438E414E9847A4EE8035D"
+    end
+
+    def put_current_user(conn, _) do
+      conn |> put_session(:current_user, %{role: []})
+    end
+  end
+
   test "lockdown" do
     Application.put_env(:brando, :lockdown, true)
 
@@ -67,6 +95,13 @@ defmodule Brando.Plug.LockdownTest do
     assert conn.resp_headers["location"] == "/coming-soon"
 
     Application.put_env(:brando, :lockdown, false)
+
+    conn =
+      :get
+      |> call("/")
+      |> LockdownPlug.call([])
+
+    assert conn.status == nil
   end
 
   test "lockdown with auth" do
@@ -76,7 +111,16 @@ defmodule Brando.Plug.LockdownTest do
       :get
       |> call("/")
       |> LockdownPlugAuth.call([])
+
     assert conn.status == nil
+
+    conn =
+      :get
+      |> call("/")
+      |> LockdownPlugAuthFail.call([])
+
+    assert conn.status == 302
+    assert conn.resp_headers["location"] == "/coming-soon"
 
     Application.put_env(:brando, :lockdown, false)
   end
