0315ddc5644f91028767a6521b060b8dd326d447
Trond Mjøen <trond@twined.net>
2015-11-16 21:10:59 +0100

    Fix test header

--- test/brando/plugs/i18n_test.exs
+++ test/brando/plugs/i18n_test.exs
@@ -8,7 +8,7 @@ defmodule Brando.Plug.I18nTest do
     assert conn.assigns.language == "nb"
     assert conn.private.plug_session["language"] == "nb"
 
-    mock_conn = %Plug.Conn{path_info: ["en", "news"], private: %{plug_session: %{language: "en"}}}
+    mock_conn = %Plug.Conn{path_info: ["en", "news"], private: %{plug_session: %{"language" => "en"}}}
     conn = I18n.put_locale(mock_conn, Brando.Gettext)
     assert conn.assigns.language == "en"
     assert conn.private.plug_session["language"] == "en"
