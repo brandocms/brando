afefed684a9391fd23c3a3d5124bcd7f701a0314
Trond Mjøen <trond@twined.net>
2015-11-16 20:51:22 +0100

    Test i18n plug

---
+++ test/brando/plugs/i18n_test.exs
@@ -0,0 +1,16 @@
+defmodule Brando.Plug.I18nTest do
+  use ExUnit.Case, async: true
+  alias Brando.Plug.I18n
+
+  test "put_locale" do
+    mock_conn = %Plug.Conn{path_info: ["news"], private: %{plug_session: %{}}}
+    conn = I18n.put_locale(mock_conn, Brando.Gettext)
+    assert conn.assigns.language == "nb"
+    assert conn.private.plug_session["language"] == "nb"
+
+    mock_conn = %Plug.Conn{path_info: ["en", "news"], private: %{plug_session: %{language: "en"}}}
+    conn = I18n.put_locale(mock_conn, Brando.Gettext)
+    assert conn.assigns.language == "en"
+    assert conn.private.plug_session["language"] == "en"
+  end
+end
