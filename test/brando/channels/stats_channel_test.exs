cc9efc28ad684f49a0b5130801271fbfada3f9b1
Trond Mjøen <trond@twined.net>
2015-11-18 20:11:30 +0100

    Adding dummy channel test

---
+++ test/brando/channels/stats_channel_test.exs
@@ -0,0 +1,11 @@
+defmodule Brando.StatsChannelTest do
+  use ExUnit.Case
+  use Phoenix.ChannelTest
+  # alias Brando.Integration.UserSocket
+
+  @endpoint Brando.Integration.Endpoint
+
+  test "stats channel join" do
+    assert 1 == 1
+  end
+end
