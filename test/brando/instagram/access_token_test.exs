0ef3c46c9b3d1c30c4ffecf5c57cd93ec9ba0c60
Trond Mjøen <trond@twined.net>
2015-12-07 21:32:36 +0100

    Return only token, not map. Fix test

--- test/brando/instagram/access_token_test.exs
+++ test/brando/instagram/access_token_test.exs
@@ -3,8 +3,13 @@ defmodule Brando.Instagram.AccessTokenTest do
   use Brando.ConnCase
   alias Brando.Instagram.AccessToken
 
+  setup do
+    File.rm_rf!(Path.join([Mix.Project.app_path, "tmp"]))
+    :ok
+  end
+
   test "retrieve_token" do
-    assert AccessToken.load_token == %{"access_token" => "abcd123"}
-    assert AccessToken.load_token == %{"access_token" => "abcd123"}
+    assert AccessToken.load_token == "abcd123"
+    assert AccessToken.load_token == "abcd123"
   end
 end
