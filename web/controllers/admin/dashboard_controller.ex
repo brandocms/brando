81a01b67b46ed429b5756a6506d638d271b9e83f
Trond Mjøen <trond@twined.net>
2015-12-10 22:19:27 +0100

    Refactor system_info

--- web/controllers/admin/dashboard_controller.ex
+++ web/controllers/admin/dashboard_controller.ex
@@ -24,28 +24,32 @@ defmodule Brando.Admin.DashboardController do
   def system_info(conn, _params) do
     log_file = Path.join([Brando.config(:log_dir), @log_filename])
     {log_last_updated, log_last_lines} =
-      case File.stat(log_file) do
-        {:ok, stat} ->
-          last_updated =
-            stat.mtime
-            |> Ecto.DateTime.from_erl
-            |> Ecto.DateTime.to_string
-
-          last_lines =
-            log_file
-            |> File.stream!
-            |> Enum.reverse
-            |> Enum.take(30)
-            |> Enum.reverse
-
-          {last_updated, last_lines}
-        {:error, _} ->
-          {"", gettext("File not found")}
-      end
+      get_log_info(log_file)
 
     conn
     |> assign(:log_last_lines, log_last_lines)
     |> assign(:log_last_updated, log_last_updated)
     |> render
+  end
+
+  defp get_log_info(log_file) do
+    case File.stat(log_file) do
+      {:ok, stat} ->
+        last_updated =
+          stat.mtime
+          |> Ecto.DateTime.from_erl
+          |> Ecto.DateTime.to_string
+
+        last_lines =
+          log_file
+          |> File.stream!
+          |> Enum.reverse
+          |> Enum.take(30)
+          |> Enum.reverse
+
+        {last_updated, last_lines}
+      {:error, _} ->
+        {"", gettext("File not found")}
+    end
   end
 end
