42878a5c3ae026b0de280405f4cf4d87ea40b641
Trond Mjøen <trond@twined.net>
2015-12-10 22:41:12 +0100

    Fix test

--- test/mix/brando_test.exs
+++ test/mix/brando_test.exs
@@ -13,7 +13,7 @@ defmodule Mix.BrandoTest do
 
   test "modules" do
     modules = Mix.Brando.modules()
-    if modules do
+    if modules != [] do
       assert Brando.Image in Mix.Brando.modules()
     end
   end
