763b14b2c4004a0af7e628bcfc2a32f273fb2c5d
Trond Mjøen <trond@twined.net>
2016-01-30 22:11:02 +0100

    Fix js template

--- priv/templates/brando.install/web/static/js/app/app.js
+++ priv/templates/brando.install/web/static/js/app/app.js
@@ -1,18 +1,20 @@
-import {Socket} from "deps/phoenix/web/static/js/phoenix"
+import {Socket} from "phoenix"
 import {Flexslider} from "./flexslider"
-import "deps/phoenix_html/web/static/js/phoenix_html"
+import "phoenix_html"
 
 
 $(() => {
     /* page specific switch */
     /*
+
     switch ($('body').attr('data-script')) {
         case "section-name-here":
             break;
     }
     */
+
     // Look for flexsliders on page and initialize.
-    Flexslider.setup();
+    // Flexslider.setup();
 });
 
 let App = {
