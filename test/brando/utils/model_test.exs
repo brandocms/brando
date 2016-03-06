687d056295130b989f2b95c13cca9756bc57ebe4
Trond Mjøen <trond@twined.net>
2016-03-06 20:36:40 +0100

    Clean up

--- test/brando/utils/model_test.exs
+++ test/brando/utils/model_test.exs
@@ -5,17 +5,21 @@ defmodule Brando.Utils.ModelTest do
   use Plug.Test
   alias Brando.Utils
 
-  @params %{"avatar" => nil, "role" => ["2", "4"], "language" => "nb",
-            "email" => "fanogigyni@gmail.com", "full_name" => "Nita Bond",
-            "password" => "finimeze", "status" => "1",
-            "submit" => "Submit", "username" => "zabuzasixu"}
+  @params %{
+    "avatar" => nil,
+    "role" => ["2", "4"],
+    "language" => "nb",
+    "email" => "fanogigyni@gmail.com",
+    "full_name" => "Nita Bond",
+    "password" => "finimeze",
+    "status" => "1",
+    "submit" => "Submit",
+    "username" => "zabuzasixu"
+  }
 
   test "update_field/2" do
-    assert {:ok, user}
-           = create_user(@params)
-    assert {:ok, model}
-           = Utils.Model.update_field(user, [full_name: "James Bond"])
-    assert model.full_name
-           == "James Bond"
+    assert {:ok, user} = create_user(@params)
+    assert {:ok, model} = Utils.Model.update_field(user, [full_name: "James Bond"])
+    assert model.full_name == "James Bond"
   end
 end
