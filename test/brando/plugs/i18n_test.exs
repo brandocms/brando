091facb4c81c046f4e135025bf685f0a304e21cd
Trond Mjøen <trond@twined.net>
2016-05-06 12:54:04 +0200

    Fix tests

--- test/brando/plugs/i18n_test.exs
+++ test/brando/plugs/i18n_test.exs
@@ -4,12 +4,12 @@ defmodule Brando.Plug.I18nTest do
 
   test "put_locale" do
     mock_conn = %Plug.Conn{path_info: ["news"], private: %{plug_session: %{}}}
-    conn = I18n.put_locale(mock_conn, Brando.Gettext)
+    conn = I18n.put_locale(mock_conn, [])
     assert conn.assigns.language == "nb"
     assert conn.private.plug_session["language"] == "nb"
 
     mock_conn = %Plug.Conn{path_info: ["en", "news"], private: %{plug_session: %{"language" => "en"}}}
-    conn = I18n.put_locale(mock_conn, Brando.Gettext)
+    conn = I18n.put_locale(mock_conn, [])
     assert conn.assigns.language == "en"
     assert conn.private.plug_session["language"] == "en"
   end
