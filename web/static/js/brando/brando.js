fbd94faa70bbc0b2fd8d0ab8d67c9388d78cb177
Trond Mjøen <trond@twined.net>
2016-10-16 19:51:59 +0200

    Toss out old fonts, use google fonts instead

--- web/static/js/brando/brando.js
+++ web/static/js/brando/brando.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import $        from 'jquery';
 import Dropzone from 'dropzone';
 import Chart    from 'chart.js';
@@ -11,28 +9,28 @@ import './brando/extensions/slugit';
 import './brando/extensions/tablesaw';
 import './brando/extensions/tags_input';
 
-import Accordion        from './brando/components/accordion';
-import Autoslug         from './brando/components/autoslug';
-import brando           from './brando/components/brando';
-import DatePicker       from './brando/components/datepicker';
-import Flash            from './brando/components/flash';
-import FilterTable      from './brando/components/filter_table';
-import ImagePreview     from './brando/components/image_preview';
-import Mobile           from './brando/components/mobile';
-import {VexBrando, vex} from './brando/components/vex_brando';
-import bI18n            from './brando/components/i18n';
-import Images           from './brando/components/images';
-import ImageConfig      from './brando/components/image_config';
-import Menu             from './brando/components/menu';
-import Pages            from './brando/components/pages';
-import PopupForm        from './brando/components/popup_form';
-import Sequence         from './brando/components/sequence';
-import Sortable         from './brando/components/sortable';
-import Stats            from './brando/components/stats';
-import Tags             from './brando/components/tags';
-import Toolbar          from './brando/components/toolbar';
-import Utils            from './brando/components/utils';
-import WS               from './brando/components/ws';
+import Accordion from './brando/components/accordion';
+import Autoslug from './brando/components/autoslug';
+import brando from './brando/components/brando';
+import DatePicker from './brando/components/datepicker';
+import Flash from './brando/components/flash';
+import FilterTable from './brando/components/filter_table';
+import ImagePreview from './brando/components/image_preview';
+import Mobile from './brando/components/mobile';
+import { VexBrando, vex } from './brando/components/vex_brando';
+import bI18n from './brando/components/i18n';
+import Images from './brando/components/images';
+import ImageConfig from './brando/components/image_config';
+import Menu from './brando/components/menu';
+import Pages from './brando/components/pages';
+import PopupForm from './brando/components/popup_form';
+import Sequence from './brando/components/sequence';
+import Sortable from './brando/components/sortable';
+import Stats from './brando/components/stats';
+import Tags from './brando/components/tags';
+import Toolbar from './brando/components/toolbar';
+import Utils from './brando/components/utils';
+import WS from './brando/components/ws';
 
 $(() => {
   /**
@@ -55,21 +53,21 @@ $(() => {
    */
 
   switch ($('body').attr('data-script')) {
-  case 'images-index':
-    Images.setup();
-    break;
-  case 'images-configure':
-    ImageConfig.setup();
-    break;
-  case 'portfolio-configure':
-    ImageConfig.setup();
-    break;
-  case 'dashboard-system_info':
-    Stats.setup();
-    break;
-  case 'pages-index':
-    Pages.setup();
-    break;
+    case 'images-index':
+      Images.setup();
+      break;
+    case 'images-configure':
+      ImageConfig.setup();
+      break;
+    case 'portfolio-configure':
+      ImageConfig.setup();
+      break;
+    case 'dashboard-system_info':
+      Stats.setup();
+      break;
+    case 'pages-index':
+      Pages.setup();
+      break;
   }
 
   /**
@@ -83,7 +81,7 @@ $(() => {
   $(document).trigger('enhance.tablesaw');
 
   $('.expander-trigger')
-    .click(function() {
+    .click(function onClick() {
       $(this).toggleClass('expander-hidden');
     });
 });
@@ -99,5 +97,5 @@ export {
 
   brando,
   bI18n,
-  vex
+  vex,
 };
