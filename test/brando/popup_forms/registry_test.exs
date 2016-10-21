b6a89903191ef064675bdea34367cc2032cafe4a
Trond Mjøen <trond@twined.net>
2016-10-21 12:31:08 +0200

    Test popup forms

---
+++ test/brando/popup_forms/registry_test.exs
@@ -0,0 +1,31 @@
+defmodule Brando.PopupForm.RegistryTest do
+  use ExUnit.Case, async: true
+  alias Brando.PopupForm.Registry
+
+  setup do
+    Registry.wipe()
+    :ok
+  end
+
+  test "empty state" do
+    assert Registry.state() == %Registry.State{forms: %{}}
+  end
+
+  test "add entry to registry" do
+    assert Registry.register("user", Brando.UserForm, "Create user", [:id, :username])
+           == %Registry.State{forms: %{"user" => {Brando.UserForm,
+                                       "Create user", [:id, :username]}}}
+  end
+
+  test "get entry from registry" do
+    Registry.register("user", Brando.UserForm, "Create user", [:id, :username])
+    assert Registry.get("user") == {:ok, {Brando.UserForm, "Create user", [:id, :username]}}
+  end
+
+  test "wipe registry" do
+    Registry.register("user", Brando.UserForm, "Create user", [:id, :username])
+    assert Registry.get("user") == {:ok, {Brando.UserForm, "Create user", [:id, :username]}}
+    Registry.wipe()
+    assert Registry.state() == %Registry.State{forms: %{}}
+  end
+end
