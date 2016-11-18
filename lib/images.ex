7a0e260005b5e9059b73826d829c8c22da52127e
Trond Mjøen <trond@twined.net>
2016-11-18 13:14:50 +0100

    Add empty context

---
+++ lib/images.ex
@@ -0,0 +1,9 @@
+defmodule Brando.Images do
+  @moduledoc """
+  Context for Images.
+
+  Interfaces with database
+  """
+  alias Brando.Utils
+  alias Brando.{ImageCategory, ImageSeries, Image}
+end
