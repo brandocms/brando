16e56a01ff0cc6d38584443716c4535316a0972e
Trond Mjøen <trond@twined.net>
2016-11-21 17:01:39 +0100

    Add guardian conf to make docs rendering work

--- config/docs.exs
+++ config/docs.exs
@@ -1,1 +1,11 @@
 use Mix.Config
+
+# Configure Guardian for auth.
+config :guardian, Guardian,
+  allowed_algos: ["HS512"], # optional
+  verify_module: Guardian.JWT,  # optional
+  issuer: "BrandoTesting",
+  ttl: {30, :days},
+  verify_issuer: true, # optional
+  secret_key: "XX9ND0BmT51mrKWp46WdYZoPWOv6awnEScbNg3HPWTnnl605tmDKEogZCb9109gb",
+  serializer: Brando.GuardianSerializer
