f4b99ee953f27d2a3f49deeff92842f6aa705ca6
Trond Mjøen <trond@twined.net>
2017-03-16 22:26:48 +0100

    Use context

--- priv/templates/brando.gen.html/controller.ex
+++ priv/templates/brando.gen.html/controller.ex
@@ -4,7 +4,7 @@ defmodule <%= module %>Controller do
   alias <%= base %>.<%= domain %>.<%= scoped %>
 
   def index(conn, _params) do
-    <%= plural %> = Brando.repo.all(<%= alias %>)
+    <%= plural %> = <%= domain %>.list_<%= plural %>
 
     render conn, :index, [
       <%= plural %>: <%= plural %>,
