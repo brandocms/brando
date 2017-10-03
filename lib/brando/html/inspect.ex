3a5bea86b983cf9d9a73e340723f84af3848b105
T. Mjoen <trond@twined.net>
2017-10-03 21:50:26 +0200

    Add nil clause

--- lib/brando/html/inspect.ex
+++ lib/brando/html/inspect.ex
@@ -258,6 +258,9 @@ defmodule Brando.HTML.Inspect do
   defp do_inspect_assoc(name, %Ecto.Association.Has{}, []) do
     ~s(<tr><td>#{name}</td><td><em>#{gettext("Empty association")}</em></td></tr>)
   end
+  defp do_inspect_assoc(name, %Ecto.Association.Has{}, nil) do
+    ~s(<tr><td>#{name}</td><td><em>#{gettext("Empty association")}</em></td></tr>)
+  end
   defp do_inspect_assoc(name, %Ecto.Association.ManyToMany{}, %Ecto.Association.NotLoaded{}) do
     ~s(<tr><td>#{name}</td><td>#{gettext("Association not fetched")}</td></tr>)
   end
