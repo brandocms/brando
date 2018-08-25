7f2b4cf6552d2eafc0e78be28ec16308f5314502
T. Mjoen <trond@twined.net>
2018-08-25 14:51:25 +0200

    Bump deps

--- mix.exs
+++ mix.exs
@@ -105,7 +105,7 @@ defmodule Brando.Mixfile do
 
       # Documentation dependencies
       {:ex_doc, "~> 0.11", only: :docs},
-      {:inch_ex, "~> 0.5", only: :docs}
+      {:inch_ex, "~> 1.0", only: :docs}
     ]
   end
 
