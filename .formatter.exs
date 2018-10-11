54345a9ec5f8a831930a71c80fac73c0e0588acd
T. Mjoen <trond@twined.net>
2018-10-11 13:24:23 +0200

    Clean up formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,40 +1,4 @@
 [
-  inputs: [
-    "lib/**/*.{ex,exs}",
-    "test/**/*.{ex,exs}",
-    "mix.exs"
-  ],
-  import_deps: [:ecto, :absinthe, :plug, :phoenix],
-  locals_without_parens: [
-    assert_reply: 2,
-    assert_reply: 3,
-    push: 3,
-    defenum: :*,
-    raw: :*,
-    redirect: :*,
-    # plug, plug_rest
-    plug: 1,
-    plug: 2,
-    resource: 2,
-    resource: 3,
-    match: 2,
-    pipe_through: :*,
-    get: :*,
-    put: :*,
-    post: :*,
-    delete: :*,
-    forward: :*,
-    # ecto
-    belongs_to: :*,
-    has_many: :*,
-    has_one: :*,
-    from: 2,
-    field: :*,
-    arg: :*,
-    resolve: :*,
-    parse: :*,
-    serialize: :*,
-    scalar: :*
-  ],
-  # rename_deprecated_at: "1.5.4"
+  inputs: ["*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"],
+  import_deps: [:ecto, :absinthe, :plug, :phoenix]
 ]
