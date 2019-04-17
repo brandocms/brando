04eeb9136a1cdc1ff1386f8e68e623669519fa17
T. Mjoen <trond@twined.net>
2019-04-17 19:45:29 +0200

    A spec beginning

--- priv/templates/brando.gen.html/assets/backend/cypress/integration/spec.js
+++ priv/templates/brando.gen.html/assets/backend/cypress/integration/spec.js
@@ -4,112 +4,41 @@ describe('<%= Recase.SentenceCase.convert(snake_domain) %>', () => {
     cy.login('lou@reed.com', 'admin')
   })
 
-  it('can list pages', () => {
-    cy.factorydb('page', { title: 'Page title', slug: 'page-title' })
-    cy.visit('/admin/sider')
-    cy.contains('Administrér sideinnhold og fragmenter')
-
-    cy.contains('Page title')
-    cy.contains('a-key')
+  it('can list <%= Recase.SentenceCase.convert(plural) |> String.downcase() %>', () => {
+    cy.factorydb('<%= singular %>', {})
+    cy.visit('/admin/<%= plural %>')
+    cy.location('pathname').should('eq', '/admin/<%= plural %>')
+    cy.contains('Ny <%= vue_singular %>')
   })
 
-  it('can edit page', () => {
-    cy.factorydb('page', { title: 'Page title', slug: 'page-title' })
-    cy.visit('/admin/sider')
-    cy.contains('Administrér sideinnhold og fragmenter')
+  it('can create <%= Recase.SentenceCase.convert(plural) |> String.downcase() %>', () => {
+    cy.visit('/admin/<%= plural %>')
+    cy.contains('Ny <%= vue_singular %>').click()
+    cy.location('pathname').should('eq', '/admin/<%= singular %>/ny')
+    cy.contains('Lagre').click()
+    cy.location('pathname').should('eq', '/admin/<%= plural %>')
+  })
 
-    cy.contains('Page title')
-    cy.contains('a-key')
+  it('can edit <%= Recase.SentenceCase.convert(plural) |> String.downcase() %>', () => {
+    cy.factorydb('<%= singular %>', {})
+    cy.visit('/admin/<%= plural %>')
 
     cy.get('tr > :nth-child(6) .dropdown').click()
-    cy.contains('Endre side').click()
+    cy.contains('Endre').click()
 
-    cy.location('pathname').should('match', /admin\/side\/endre\/(\d+)/)
-    cy.contains('Endre side')
+    cy.location('pathname').should('match', /admin\/<%= singular %>\/endre\/(\d+)/)
+    cy.contains('Endre')
 
-    cy.get('#page_key_').type('-edit')
-    cy.get('#page_title_').type(' edit')
+    cy.contains('Lagre').click()
 
-    cy.contains('Lagre oppdatert side').click()
-
-    cy.contains('data -> can\'t be blank')
-
-    cy.get('.vex-dialog-button-primary').click()
-
-    cy.get('.villain-editor-plus-inactive > a').click()
-    cy.get('.villain-editor-plus-available-blocks > :nth-child(1)').click()
-    cy.get('p').click().type('This is a paragraph')
-
-    cy.contains('Lagre oppdatert side').click()
-
-    cy.location('pathname').should('eq', '/admin/sider')
-    cy.contains('Side oppdatert')
-    cy.contains('Page title edit')
-    cy.contains('a-key-edit')
+    cy.location('pathname').should('eq', '/admin/<%= plural %>')
   })
 
   it('can delete page', () => {
-    cy.factorydb('page', { title: 'Page title', slug: 'page-title' })
-    cy.visit('/admin/sider')
-    cy.contains('Administrér sideinnhold og fragmenter')
-
-    cy.contains('Page title')
-    cy.contains('a-key')
+    cy.factorydb('<%= singular %>', {})
+    cy.visit('/admin/<%= plural %>')
 
     cy.get('tr > :nth-child(6) .dropdown').click()
-    cy.contains('Slett side').click()
-    cy.contains('Er du sikker på at du vil slette denne siden?')
-
-    cy.contains('OK').click()
-  })
-
-  it('can recreate page', () => {
-    cy.factorydb('page', { title: 'Page title', slug: 'page-title' })
-    cy.visit('/admin/sider')
-    cy.contains('Administrér sideinnhold og fragmenter')
-
-    cy.contains('Page title')
-    cy.contains('a-key')
-
-    cy.get('tr > :nth-child(6) .dropdown').click()
-    cy.contains('Reprosessér side').click()
-    cy.contains('Siden ble gjengitt på nytt')
-  })
-
-  it('can duplicate page', () => {
-    cy.factorydb('page', { title: 'Page title', slug: 'page-title', data: '[]' })
-    cy.visit('/admin/sider')
-    cy.contains('Administrér sideinnhold og fragmenter')
-
-    cy.contains('Page title')
-    cy.contains('a-key')
-
-    cy.get('tr > :nth-child(6) .dropdown').click()
-    cy.contains('Duplisér side').click()
-    cy.contains('Page title (kopi)')
-    cy.contains('a-key_kopi')
-  })
-
-  it('can create page', () => {
-    cy.visit('/admin/sider')
-
-    cy.contains('Ny side').click()
-
-    cy.get('#page_key_').type('my-key')
-    cy.get('#page_title_').type('My title')
-    cy.get('#page_meta_description_').type('Meta description')
-    cy.get('#page_meta_keywords_').type('Meta keywords')
-    cy.get('#page_css_classes_').type('css class')
-
-    cy.get('.villain-editor-plus-inactive > a').click()
-    cy.get('.villain-editor-plus-available-blocks > :nth-child(1)').click()
-    cy.get('p').click().type('This is a paragraph')
-
-    cy.contains('Lagre side').click()
-
-    cy.contains('Side opprettet')
-    cy.location('pathname').should('eq', '/admin/sider')
-    cy.contains('my-key')
-    cy.contains('My title')
+    cy.contains('Slett').click()
   })
 })
