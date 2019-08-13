3a5b7b064c4f64b1cd184dfb11983d947f897e61
T. Mjoen <trond@twined.net>
2019-08-13 18:33:37 +0200

    Start META work

--- lib/brando/meta/schema.ex
+++ lib/brando/meta/schema.ex
@@ -1,12 +1,27 @@
 defmodule Brando.Meta.Schema do
   @moduledoc """
-  Meta functions for Brando schemas
-
-  Deprecated, just remove from schema.
+  Macro for mapping META fields to schema
   """
 
   @doc false
   defmacro __using__(_) do
-    raise "META/schemas are deprecated, and are not in use anymore."
+    import Brando.Meta.Schema
+  end
+  
+  defmacro meta_schema([do: block]) do
+    quote do
+      Module.register_attribute(:meta_fields, accumulate: true)
+    end
+  end
+  
+  defmacro field(name, path, mutator_function) do
+    Module.put_attribute(:meta_fields, name)
+    
+    quote do
+      def __meta_field__(name, data) do
+        value = get_in(data, Enum.map(unquote(path), &Access.get/1))
+        unquote(mutator_function).(value)
+      end
+    end
   end
 end
