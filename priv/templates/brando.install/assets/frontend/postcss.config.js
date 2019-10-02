503c2a21299a8338557297b4d4870aaf56edf871
T. Mjoen <trond@twined.net>
2019-10-02 11:12:26 +0200

    Fix postcss config

--- priv/templates/brando.install/assets/frontend/postcss.config.js
+++ priv/templates/brando.install/assets/frontend/postcss.config.js
@@ -2,12 +2,12 @@ module.exports = {
   plugins: [
     require('postcss-easy-import')({
       prefix: '_',
-      extensions: ['pcss', 'scss', 'css']
+      extensions: ['pcss', 'scss', 'css'],
       plugins: [
         require('stylelint')
       ]
     }),
-    require('@univers-agency/europacss')
+    require('@univers-agency/europacss'),
     require('autoprefixer')({ grid: 'on' }),
     require('css-mqgroup')({ sort: true }),
     require('postcss-reporter')({ clearReportedMessages: true, throwError: true })
