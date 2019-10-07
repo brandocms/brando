d086db1cc0aa2861931c6cd3c2e033e51a95dbde
T. Mjoen <trond@twined.net>
2019-10-07 11:35:32 +0200

    Upgrade before ecto setup

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ then
   mix do deps.get, deps.compile, brando.install --module $MODULE, deps.get, deps.compile
   gsed -i '/Import environment specific config/i\# import BRANDO config\nimport_config "brando.exs"\n' config/config.exs
   cd assets/frontend && yarn && yarn upgrade @univers-agency/jupiter @univers-agency/europacss && cd ../backend && yarn && yarn upgrade @univers-agency/kurtz && cd ../../
-  mix do deps.get, deps.compile --force && mix ecto.setup
+  mix do deps.get, deps.compile --force && mix brando.upgrade && mix ecto.setup
   gsed -i '/pool_size:/i\  socket_options: [recbuf: 8192, sndbuf: 8192],' config/prod.secret.exs
   mix distillery.init
 fi
