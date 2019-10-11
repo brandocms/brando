b9e184a47c6520c16ba41eb9db3ed3693ac2d6e8
T. Mjoen <trond@twined.net>
2019-10-11 15:55:43 +0200

    Export locals

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
+locals_without_parens = [
+  sequence: 2
+]
+
 [
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
   import_deps: [:absinthe, :ecto, :ecto_sql, :phoenix, :plug, :phoenix_html],
   inputs: ["/lib/**/*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
