e30fd1ebdda0180b231e4612e1630be8f91b03d9
T. Mjoen <trond@twined.net>
2019-12-09 16:35:53 +0100

    Add distillery to formatter

--- .formatter.exs
+++ .formatter.exs
@@ -5,7 +5,7 @@ locals_without_parens = [
 [
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens],
-  import_deps: [:absinthe, :ecto, :ecto_sql, :phoenix, :plug, :phoenix_html],
+  import_deps: [:absinthe, :ecto, :ecto_sql, :phoenix, :plug, :phoenix_html, :distillery],
   inputs: ["/lib/**/*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
