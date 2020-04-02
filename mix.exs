74d6bdac946d6d6911be10d666add2672d4239fa
T. Mjoen <trond@twined.net>
2020-04-02 19:22:13 +0200

    Stick to 1.6 for now

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Brando.Mixfile do
     [
       app: :brando,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.6",
       deps: deps(),
       compilers: [:gettext, :phoenix] ++ Mix.compilers(),
       elixirc_paths: elixirc_paths(Mix.env()),
