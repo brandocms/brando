61e8b018d0af691cf7cf3ecb46fdff4acece0f51
T. Mjoen <trond@twined.net>
2020-04-11 10:16:34 +0200

    Count soft deleted entries per schema

--- lib/brando/soft_delete/query.ex
+++ lib/brando/soft_delete/query.ex
@@ -15,12 +15,12 @@ defmodule Brando.SoftDelete.Query do
   @doc """
   Excludes all deleted entries from query
   """
-  def exclude_deleted(query), do: where(query, [t], is_nil(t.deleted_at))
+  def exclude_deleted(query), do: from(t in query, where: is_nil(t.deleted_at))
 
   @doc """
-  List all soft delete marked schemas
+  List all soft delete enabled schemas
   """
-  def list_all_soft_delete_schemas do
+  def list_soft_delete_schemas do
     {:ok, app_modules} = :application.get_key(Brando.otp_app(), :modules)
 
     modules =
@@ -37,7 +37,45 @@ defmodule Brando.SoftDelete.Query do
     Enum.filter(modules, &({:__soft_delete__, 0} in &1.__info__(:functions)))
   end
 
-  def clean_up_soft_deletions, do: Enum.map(list_all_soft_delete_schemas(), &clean_up_schema/1)
+  @doc """
+  Count all soft deleted entries per schema
+  """
+  def count_soft_deletions do
+    schemas = list_soft_delete_schemas()
+
+    union_query =
+      Enum.reduce(schemas, nil, fn
+        schema, nil ->
+          from t in schema, select: count(t.id), where: not is_nil(t.deleted_at)
+
+        schema, q ->
+          from t in schema, select: count(t.id), where: not is_nil(t.deleted_at), union_all: ^q
+      end)
+
+    counts = Brando.repo().all(union_query)
+    Enum.zip(schemas, counts)
+  end
+
+  @doc """
+  List all soft deleted entries across schemas
+  """
+  def list_soft_deleted_entries do
+    schemas = list_soft_delete_schemas()
+    Enum.flat_map(schemas, &list_soft_deleted_entries(&1))
+  end
+
+  @doc """
+  List soft deleted entries for `schema`
+  """
+  def list_soft_deleted_entries(schema) do
+    query = from t in schema, where: not is_nil(t.deleted_at), order_by: [desc: t.deleted_at]
+    Brando.repo().all(query)
+  end
+
+  @doc """
+  Clean up and delete all expired soft deleted entries
+  """
+  def clean_up_soft_deletions, do: Enum.map(list_soft_delete_schemas(), &clean_up_schema/1)
 
   defp clean_up_schema(Brando.Image) do
     query =
