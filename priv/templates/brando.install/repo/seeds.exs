b7fed490d4e9a756be98d052dc224afe817df9a9
T. Mjoen <trond@twined.net>
2020-04-17 14:08:49 +0200

    Add page to seeds

--- priv/templates/brando.install/repo/seeds.exs
+++ priv/templates/brando.install/repo/seeds.exs
@@ -11,7 +11,7 @@
 # and so on) as they will fail if something goes wrong.
 
 post_cfg = %Brando.Type.ImageConfig{allowed_mimetypes: ["image/jpeg", "image/png", "image/gif"],
-    default_size: "medium", random_filename: true, size_limit: 10_240_000,
+    default_size: "xlarge", random_filename: true, size_limit: 10_240_000,
     sizes: %{
       "large" => %{"quality" => 85, "size" => "1400"},
       "medium" => %{"quality" => 85, "size" => "1000"},
@@ -22,7 +22,7 @@ post_cfg = %Brando.Type.ImageConfig{allowed_mimetypes: ["image/jpeg", "image/png
     upload_path: "images/site/posts"}
 
 ss_cfg = %Brando.Type.ImageConfig{allowed_mimetypes: ["image/jpeg", "image/png", "image/gif"],
-    default_size: "medium", random_filename: true, size_limit: 10_240_000,
+    default_size: "xlarge", random_filename: true, size_limit: 10_240_000,
     sizes: %{
         "cropxlarge" => %{"crop" => true, "quality" => 80, "size" => "1140x600"},
         "large" => %{"quality" => 80, "size" => "1400"},
@@ -61,3 +61,31 @@ post_series = %Brando.ImageSeries{
   name: "post", sequence: 0, slug: "post"}
 
 <%= application_module %>.Repo.insert!(post_series)
+
+page = %Brando.Pages.Page{
+  creator_id: 1,
+  css_classes: nil,
+  data: [
+    %{
+      "data" => %{"text" => "<p>Index text.</p>", "type" => "paragraph"},
+      "type" => "text"
+    }
+  ],
+  deleted_at: nil,
+  fragments: [],
+  html: "<p>Index text.</p>",
+  id: 1,
+  inserted_at: ~N[2020-04-17 12:05:44],
+  key: "index",
+  language: "en",
+  meta_description: nil,
+  meta_image: nil,
+  parent_id: nil,
+  sequence: 0,
+  slug: "index",
+  status: :published,
+  title: "Index",
+  updated_at: ~N[2020-04-17 12:05:44]
+}
+
+<%= application_module %>.Repo.insert!(page)
