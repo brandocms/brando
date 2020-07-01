60d3187d1094ebad9d7a52f37ca124ffbd9cdef6
T. Mjoen <trond@twined.net>
2020-07-01 18:14:49 +0200

    Remove unused helper

--- test/support/lexer_helper.ex
+++
@@ -1,9 +0,0 @@
-defmodule Brando.LexerHelper do
-  @moduledoc false
-
-  import ExUnit.Assertions
-
-  def assert_parse(doc, match) do
-    assert {:ok, ^match, "", _, _, _} = Brando.Lexer.parse(doc)
-  end
-end
