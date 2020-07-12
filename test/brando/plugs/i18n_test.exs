e933e14431acb667c7467abb94ef1dd841311514
T. Mjoen <trond@twined.net>
2020-07-12 21:20:31 +0200

    Test admin locale

--- test/brando/plugs/i18n_test.exs
+++ test/brando/plugs/i18n_test.exs
@@ -1,6 +1,8 @@
 defmodule Brando.Plug.I18nTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
+  use Brando.ConnCase
   alias Brando.Plug.I18n
+  alias Brando.Factory
 
   test "put_locale" do
     mock_conn = %Plug.Conn{path_info: ["news"], private: %{plug_session: %{}}}
@@ -16,5 +18,21 @@ defmodule Brando.Plug.I18nTest do
     conn = I18n.put_locale(mock_conn, [])
     assert conn.assigns.language == "en"
     assert conn.private.plug_session["language"] == "en"
+  end
+
+  test "put_admin_locale" do
+    u1 = Factory.insert(:random_user, language: "sv")
+    mock_conn = %Plug.Conn{path_info: ["news"], private: %{plug_session: %{}}}
+    _conn = I18n.put_admin_locale(mock_conn, [])
+    assert Gettext.get_locale(Brando.Gettext) == "no"
+
+    mock_conn = %Plug.Conn{
+      path_info: ["en", "news"]
+    }
+
+    mock_conn = put_private(mock_conn, :guardian_default_resource, u1)
+
+    _conn = I18n.put_admin_locale(mock_conn, [])
+    assert Gettext.get_locale(Brando.Gettext) == "sv"
   end
 end
