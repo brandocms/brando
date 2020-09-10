d105072f20e490ebb831df448e47b2152eacae7d
T. Mjoen <trond@twined.net>
2020-09-10 15:54:08 +0200

    Fix page params in template

--- priv/templates/brando.install/lib/application_name_web/controllers/page_controller.ex
+++ priv/templates/brando.install/lib/application_name_web/controllers/page_controller.ex
@@ -11,7 +11,7 @@ defmodule <%= application_module %>Web.PageController do
   @doc false
   @spec index(conn, map) :: page_not_found | conn
   def index(conn, _params) do
-    with {:ok, page} <- Pages.get_page(%{key: "index", status: :published}) do
+    with {:ok, page} <- Pages.get_page(%{matches: %{key: "index", status: :published}}) do
       conn
       |> put_section("index")
       |> put_meta(Pages.Page, page)
@@ -23,7 +23,7 @@ defmodule <%= application_module %>Web.PageController do
   @doc false
   @spec show(conn, map) :: page_not_found | conn
   def show(conn, %{"path" => path}) when is_list(path) do
-    with {:ok, page} <- Pages.get_page(%{path: path, status: :published}) do
+    with {:ok, page} <- Pages.get_page(%{matches: %{path: path, status: :published}}) do
       #  {:ok, partials} <- Pages.get_fragments("partials") do
       conn
       |> put_section(page.key)
