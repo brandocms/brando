7a239c244adc19b623cfd4672cf1c604bc391040
T. Mjoen <trond@twined.net>
2020-10-28 19:11:53 +0100

    Fix unused imports

--- priv/templates/brando.upgrade/migrations/brando_42_rename_identity_tables.exs
+++ priv/templates/brando.upgrade/migrations/brando_42_rename_identity_tables.exs
@@ -1,7 +1,6 @@
 defmodule Brando.Migrations.RenameTables do
   use Ecto.Migration
   use Brando.Sequence.Migration
-  import Ecto.Query
 
   def change do
     rename table(:sites_identities), to: table(:sites_identity)
--- priv/templates/brando.upgrade/migrations/brando_44_add_meta_title_to_pages.exs
+++ priv/templates/brando.upgrade/migrations/brando_44_add_meta_title_to_pages.exs
@@ -1,7 +1,6 @@
 defmodule Brando.Migrations.AddMetaTitleToPages do
   use Ecto.Migration
   use Brando.Sequence.Migration
-  import Ecto.Query
 
   def change do
     alter table(:pages_pages) do
