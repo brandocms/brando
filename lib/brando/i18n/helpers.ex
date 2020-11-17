a85f6d1182ab8d5853a0303ed809d23f79b10eaa
T. Mjoen <trond@twined.net>
2020-11-17 15:00:12 +0100

    Simplify I18n helper use

--- lib/brando/i18n/helpers.ex
+++ lib/brando/i18n/helpers.ex
@@ -4,21 +4,22 @@ defmodule Brando.I18n.Helpers do
 
   ## Usage:
 
-      use Brando.I18n.Helpers, helpers: MyApp.Router.Helpers
+      use Brando.I18n.Helpers
 
 
   You can then call
 
-      <a href="<%= localized(@language, :page_path, [@conn, :about]) %>"><%= gettext("About") %>
+      <a href="<%= localized_path(@language, :page_path, [@conn, :about]) %>"><%= gettext("About") %>
 
   """
-  defmacro __using__(opts) do
-    helpers = Keyword.fetch!(opts, :helpers)
-
+  defmacro __using__(_) do
     quote do
-      def localized(locale, fun, args) do
-        apply(unquote(helpers), :"#{locale}_#{fun}", args)
-      end
+      @deprecated "Use `localized_path/3` instead"
+      def localized(locale, fun, args),
+        do: localized_path(locale, fun, args)
+
+      def localized_path(locale, fun, args),
+        do: apply(Brando.helpers(), :"#{locale}_#{fun}", args)
     end
   end
 end
