6823790d0d31b987f8ea20be36f25bb9e55be581
T. Mjoen <trond@twined.net>
2020-11-20 16:47:02 +0100

    Back to true :|

--- lib/brando/images/conversion_parameters.ex
+++ lib/brando/images/conversion_parameters.ex
@@ -10,7 +10,7 @@ defmodule Brando.Images.ConversionParameters do
             image_src_path: nil,
             image_dest_path: nil,
             image_dest_rel_path: nil,
-            optimize: false,
+            optimize: true,
             original_width: nil,
             original_height: nil,
             resize_width: nil,
