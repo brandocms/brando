a7a63c8c569c2768a19f1b809b8face7549ff6d6
T. Mjoen <trond@twined.net>
2021-02-28 21:26:56 +0100

    Add publisher

---
+++ lib/brando/publisher.ex
@@ -0,0 +1,57 @@
+defmodule Brando.Publisher do
+  @moduledoc """
+  Helpers fpr scheduling and publishing content
+  """
+  import Ecto.Query
+  alias Brando.Users.User
+  alias Brando.Worker
+  alias Ecto.Changeset
+
+  @type entry :: map()
+  @type changeset :: Changeset.t()
+  @type user :: User.t()
+
+  @doc """
+  Create a job for the publisher worker if we have a publish_at
+  field in the entry struct, and it has been changed in changeset
+  """
+  @spec schedule_publishing(entry, changeset, user) :: {:ok, entry}
+  def schedule_publishing(
+        %{id: id, publish_at: publish_at, __struct__: schema} = entry,
+        %{changes: %{publish_at: _}},
+        user
+      )
+      when not is_nil(publish_at) do
+    args = %{schema: schema, id: id, user_id: user.id, status: :published}
+
+    Brando.repo().delete_all(
+      from j in Oban.Job,
+        where: fragment("? @> ?", j.args, ^args)
+    )
+
+    args
+    |> Worker.Publisher.new(replace_args: true, scheduled_at: publish_at)
+    |> Oban.insert()
+
+    {:ok, entry}
+  end
+
+  def schedule_publishing(entry, _, _) do
+    {:ok, entry}
+  end
+
+  def maybe_override_status(%{changes: %{publish_at: publish_at}} = changeset)
+      when not is_nil(publish_at) do
+    status = Changeset.get_field(changeset, :status)
+
+    if DateTime.compare(publish_at, DateTime.utc_now()) == :gt and status == :published do
+      Changeset.put_change(changeset, :status, :pending)
+    else
+      changeset
+    end
+  end
+
+  def maybe_override_status(changeset) do
+    changeset
+  end
+end
