20c6eaf024c6dec26796d7bfab331b51dd669a09
T. Mjoen <trond@twined.net>
2021-03-09 09:15:44 +0100

    Use the Ecto ver of ExMachina

--- priv/templates/brando.install/lib/application_name/factory.ex
+++ priv/templates/brando.install/lib/application_name/factory.ex
@@ -1,5 +1,5 @@
 defmodule <%= application_module %>.Factory do
-  use ExMachina, repo: <%= application_module %>.Repo
+  use ExMachina.Ecto, repo: <%= application_module %>.Repo
   use Brando.FactoryMixin
   # ++aliases
   # __aliases
