9ac124d5dc223b0fbbc111cbc605a7d4bc849cf1
T. Mjoen <trond@twined.net>
2021-03-10 21:22:13 +0100

    Clean up user resolver

--- lib/brando/graphql/resolvers/users/user_resolver.ex
+++ lib/brando/graphql/resolvers/users/user_resolver.ex
@@ -3,69 +3,17 @@ defmodule Brando.Users.UserResolver do
   Resolver for Users
   """
   use Brando.Web, :resolver
-  alias Brando.Users
-  alias Brando.Users.User
 
-  import Ecto.Query
+  use Brando.GraphQL.Resolver,
+    context: Brando.Users,
+    schema: Brando.Users.User
+
+  alias Brando.Users
 
   @doc """
   Get current user
   """
   def me(_, %{context: %{current_user: current_user}}) do
-    me =
-      Brando.repo().one(
-        from user in User,
-          where: user.id == ^current_user.id
-      )
-
-    case me do
-      nil -> {:error, "User id #{current_user.id} not found"}
-      user -> {:ok, user}
-    end
-  end
-
-  @doc """
-  Get current user
-  """
-  def find(%{user_id: user_id}, %{context: %{current_user: _current_user}}) do
-    user =
-      Brando.repo().one(
-        from user in User,
-          where: user.id == ^user_id
-      )
-
-    case user do
-      nil -> {:error, "User id #{user_id} not found"}
-      user -> {:ok, user}
-    end
-  end
-
-  @doc """
-  Get all users
-  """
-  def all(_, %{context: %{current_user: _current_user}}) do
-    users =
-      Brando.repo().all(
-        from user in User,
-          order_by: [asc: user.name]
-      )
-
-    {:ok, users}
-  end
-
-  @doc """
-  create user
-  """
-  def create(%{user_params: user_params}, %{context: %{current_user: current_user}}) do
-    Users.create_user(user_params, current_user)
-  end
-
-  @doc """
-  update user
-  """
-  def update(%{user_id: user_id, user_params: user_params}, %{
-        context: %{current_user: current_user}
-      }) do
-    Users.update_user(user_id, user_params, current_user)
+    Users.get_user(%{matches: %{id: current_user.id}})
   end
 end
