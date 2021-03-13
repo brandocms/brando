8b65af24d592cacf749286cef7e60e6a1eb4d040
T. Mjoen <trond@twined.net>
2021-03-13 16:05:27 +0100

    Return vsn on user channel join
    Closes #892

--- lib/web/channels/user_channel.ex
+++ lib/web/channels/user_channel.ex
@@ -17,7 +17,8 @@ defmodule Brando.UserChannel do
   def join("user:" <> _user_id, _params, socket) do
     user = Guardian.Phoenix.Socket.current_resource(socket)
     socket = assign(socket, :user_id, user.id)
-    {:ok, user.id, socket}
+    vsn = Application.spec(Brando.config(:otp_app), :vsn) |> to_string()
+    {:ok, %{user_id: user.id, vsn: vsn}, socket}
   end
 
   def handle_out("alert", payload, socket) do
