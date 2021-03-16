025ff84c8a8a9052f0ca472f27f7fa8e3643fb8a
T. Mjoen <trond@twined.net>
2021-03-16 09:30:17 +0100

    Refactor navigation plug

--- lib/brando/plugs/navigation.ex
+++ lib/brando/plugs/navigation.ex
@@ -4,8 +4,8 @@ defmodule Brando.Plug.Navigation do
 
   Plug this in your controller or pipeline
 
-      plug Brando.Plug.Navigation "main"
-      plug Brando.Plug.Navigation "footer", as: :footer_navigation
+      plug Brando.Plug.Navigation, key: "main"
+      plug Brando.Plug.Navigation, key: "footer", as: :footer_navigation
 
   """
 
@@ -15,10 +15,20 @@ defmodule Brando.Plug.Navigation do
   def init(opts), do: opts
 
   @doc false
-  def call(conn, menu_key, opts \\ []) do
+  def call(conn, opts) when is_list(opts) do
+    menu_key = Keyword.fetch!(opts, :key)
     name = Keyword.get(opts, :as, :navigation)
     locale = Gettext.get_locale(Brando.web_module(Gettext))
     menu = Cache.Navigation.get("#{menu_key}.#{locale}")
     Plug.Conn.assign(conn, name, menu)
+  end
+
+  def call(_, _) do
+    raise """
+    Brando.Plug.Navigation must be called with a keyword list as arg:
+
+        plug Brando.Plug.Navigation, key: "main", as: :navigation
+
+    """
   end
 end
