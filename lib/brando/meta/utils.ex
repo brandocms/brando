f5be4d1646ee1619e22e0457ae3861d79a402265
T. Mjoen <trond@twined.net>
2021-03-18 14:19:54 +0100

    Proper casing

--- lib/brando/meta/utils.ex
+++ lib/brando/meta/utils.ex
@@ -7,9 +7,9 @@ defmodule Brando.Meta.Utils do
   Convert locale to a format OG understands
   """
   @spec encode_locale(binary) :: binary
-  def encode_locale("en"), do: "en_us"
-  def encode_locale("no"), do: "nb_no"
-  def encode_locale("nb"), do: "nb_no"
-  def encode_locale("nn"), do: "nn_no"
+  def encode_locale("en"), do: "en_US"
+  def encode_locale("no"), do: "nb_NO"
+  def encode_locale("nb"), do: "nb_NO"
+  def encode_locale("nn"), do: "nn_NO"
   def encode_locale(locale), do: locale
 end
