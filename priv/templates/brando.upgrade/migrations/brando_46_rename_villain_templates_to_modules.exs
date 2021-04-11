4cca1bb3560b47918ecf846d98680c3371c6311f
T. Mjoen <trond@twined.net>
2021-04-11 13:52:36 +0200

    Remove duplicate processing

--- priv/templates/brando.upgrade/migrations/brando_46_rename_villain_templates_to_modules.exs
+++ priv/templates/brando.upgrade/migrations/brando_46_rename_villain_templates_to_modules.exs
@@ -3,23 +3,6 @@ defmodule Brando.Migrations.RenameTemplatesToModules do
   import Ecto.Query
 
   def change do
-    villain_schemas = Brando.Villain.list_villains()
-
-    actions =
-      for {schema, fields} <- villain_schemas do
-        Enum.map(fields, fn {_, f, _} ->
-          from t in schema.__schema__(:source),
-            update: [set: [{^f, fragment("REPLACE(?::text, '\"type\": \"template\"', '\"type\": \"module\"')::jsonb", field(t, ^f))}]]
-        end)
-      end
-      |> List.flatten()
-
-    for action <- actions do
-      Brando.repo().update_all(action, [])
-    end
-
-    flush()
-
     rename table(:pages_templates), to: table(:pages_modules)
   end
 end
