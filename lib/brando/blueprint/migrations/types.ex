228a99a1b9c7f29bd39b4ca6ac849fdc79c6b88b
T. Mjoen <trond@twined.net>
2021-04-29 13:42:45 +0200

    Set enums as text in db

--- lib/brando/blueprint/migrations/types.ex
+++ lib/brando/blueprint/migrations/types.ex
@@ -6,5 +6,6 @@ defmodule Brando.Blueprint.Migrations.Types do
   def migration_type(:language), do: :text
   def migration_type(:status), do: :integer
   def migration_type(:datetime), do: :utc_datetime
+  def migration_type(:enum), do: :text
   def migration_type(type), do: type
 end
