a120795b341bc8ae6714a1381da4e90a3e715985
T. Mjoen <trond@twined.net>
2021-05-07 15:41:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo   # new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "==> Starting installation"
-  gsed -i '/{:phoenix,/i\      {:brando, github: "brandocms/brando", branch: "tm/blueprints"},' mix.exs
+  gsed -i '/{:phoenix,/i\      {:brando, github: "brandocms/brando"},' mix.exs
   mix do deps.get, deps.compile, brando.install --module $MODULE, deps.get, deps.compile
   direnv allow
   cd e2e && yarn && cd ../assets/frontend && yarn && yarn upgrade @brandocms/jupiter @brandocms/europacss && cd ../backend && yalc add brandojs && yarn && yarn lint --fix && cd ../../
