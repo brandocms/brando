5a31675746ebe1b98732a11155af23fab5f7ac79
T. Mjoen <trond@twined.net>
2021-05-12 13:25:45 +0200

    Add :text type for :slug migrations

--- lib/brando/blueprint/migrations/types.ex
+++ lib/brando/blueprint/migrations/types.ex
@@ -4,6 +4,7 @@ defmodule Brando.Blueprint.Migrations.Types do
   def migration_type(:image), do: :jsonb
   def migration_type(:video), do: :jsonb
   def migration_type(:language), do: :text
+  def migration_type(:slug), do: :text
   def migration_type(:status), do: :integer
   def migration_type(:datetime), do: :utc_datetime
   def migration_type(:enum), do: :text
