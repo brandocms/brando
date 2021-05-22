4bc4aa77272600c53262bb4b14d415a5996e3274
T. Mjoen <trond@twined.net>
2021-05-22 18:58:54 +0200

    Add docs for obfuscated_fields

--- lib/brando/traits/soft_delete.ex
+++ lib/brando/traits/soft_delete.ex
@@ -1,6 +1,14 @@
 defmodule Brando.Trait.SoftDelete do
   @moduledoc """
   Adds `deleted_at`
+
+  ### Opts
+
+  - `obfuscated_fields` > Fields that should be changed on deletion to free up
+  its name for uniqueness -- for instance a slug field. It will try to reset it
+  when restoring.
+
+      trait Brando.Trait.SoftDelete, obfuscated_fields: [:slug]
   """
   use Brando.Trait
 
