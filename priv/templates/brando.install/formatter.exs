04b0848927ffe110beea2a380e70935b1bdcf5be
T. Mjoen <trond@twined.net>
2021-06-04 16:40:42 +0200

    Update formatter template

--- priv/templates/brando.install/formatter.exs
+++ priv/templates/brando.install/formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:absinthe, :ecto, :phoenix, :plug, :brando],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}"],
   subdirectories: ["priv/*/migrations"]
 ]
