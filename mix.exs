8580465f00adf850bc73abb11b8c5eae519e14eb
T. Mjoen <trond@twined.net>
2021-09-11 13:36:04 +0200

    Add surface formatter, bump deps

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule Brando.Mixfile do
       {:phoenix_live_view, "~> 0.16"},
       {:phoenix_html, "~> 3.0", override: true},
       {:surface, "~> 0.5"},
+      {:surface_formatter, "~> 0.5.4"},
       {:floki, ">= 0.27.0", only: :test},
 
       # hashing/passwords
