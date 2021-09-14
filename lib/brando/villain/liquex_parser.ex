5222ee3ad1222568fb1ab478cf650c2a9d43bd92
T. Mjoen <trond@twined.net>
2021-09-14 22:27:30 +0200

    Add marks for parsec rendering

--- lib/brando/villain/liquex_parser.ex
+++ lib/brando/villain/liquex_parser.ex
@@ -1,7 +1,9 @@
 defmodule Brando.Villain.LiquexParser do
   @moduledoc false
+  # parsec:Brando.Villain.LiquexParser
   import NimbleParsec
 
   defcombinatorp(:document, repeat(Brando.Villain.Tags.Base.base_element()))
   defparsec(:parse, parsec(:document) |> eos())
+  # parsec:Brando.Villain.LiquexParser
 end
