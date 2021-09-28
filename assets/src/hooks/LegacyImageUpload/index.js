db058681a60f4f8a62aa2f39392839838358e498
T. Mjoen <trond@twined.net>
2021-09-28 11:42:45 +0200

    Add support for multiple files

--- assets/src/hooks/LegacyImageUpload/index.js
+++ assets/src/hooks/LegacyImageUpload/index.js
@@ -6,6 +6,9 @@ const UPLOAD_URL = '/admin/api/content/upload/image'
 
 export default (app) => ({
   mounted() {
+    this.multi = this.el.hasAttribute('data-upload-multi')
+    this.files = []
+
     this.strings = {}
     this.strings.idle = Dom.find(this.el, '.instructions span')
     this.strings.uploading = this.el.dataset.textUploading
@@ -17,22 +20,33 @@ export default (app) => ({
     this.plusTimeline = gsap.timeline({ repeat: -1 })
     this.plusTimeline.to(this.pluses, { duration: 3, rotate: 360, ease: 'none', transformOrigin: '50% 50%' })
     this.plusTimeline.timeScale(0)
-
+    
     this.$fileInput = Dom.find(this.el, '.file-input')
-    this.$fileInput.addEventListener('change', e => {
+
+    this.$uploadButton = Dom.find(this.el, 'button.file-upload')
+    if (this.$uploadButton) {
+      this.$uploadButton.addEventListener('click', e => {
+        this.$fileInput.click()
+      })
+    }
+    
+    this.$fileInput.addEventListener('change', async e => {
       e.preventDefault()
       e.stopPropagation()
 
-      if (e.target.files.length && e.target.files.length === 1) {
-        this.upload(e.target.files[0])
+      this.files = e.target.files
+
+      for (let i = 0; i < this.files.length; i++) {
+        await this.upload(this.files[i])
       }
+
+      this.files = []
     })
 
     this.$uploadCanvases = Dom.all(this.el, '.upload-canvas')
 
     this.$uploadCanvases.forEach(uploadCanvas => {
       uploadCanvas.addEventListener('click', e => {
-        console.log(e.target.tagName)
         if (e.target.tagName === 'BUTTON') {
           return
         }
@@ -52,13 +66,15 @@ export default (app) => ({
         event.preventDefault()
         const files = event.dataTransfer.files
 
-        if (files.length > 1) {
-          console.log('too many files!')
-          return false
+        if (!this.multi) {
+          if (files.length > 1) {
+            console.log('too many files!')
+            return false
+          }
+  
+          const f = files.item(0)
+          this.upload(f)
         }
-
-        const f = files.item(0)
-        this.upload(f)
       })
     })
     
@@ -86,42 +102,47 @@ export default (app) => ({
 
 
   async upload (f) {
-    const formData = new FormData()
-    const headers = new Headers()
+    return new Promise(async (resolve, reject) => {
+      const formData = new FormData()
+      const headers = new Headers()
 
-    headers.append('accept', 'application/json, text/javascript, */*; q=0.01')
-    headers.append('x-csrf-token', this.csrfToken)
+      headers.append('accept', 'application/json, text/javascript, */*; q=0.01')
+      headers.append('x-csrf-token', this.csrfToken)
 
-    formData.append('image', f)
-    formData.append('name', f.name)
-    formData.append('slug', 'post')
-    formData.append('uid', this.uid)
+      formData.append('image', f)
+      formData.append('name', f.name)
+      formData.append('slug', 'post')
+      formData.append('uid', this.uid)
 
-    try {
-      this.status = UPLOADING
-      this.setStatusText()
-      this.spinPlus()
-      const response = await fetch(UPLOAD_URL, { headers, method: 'post', body: formData })
-      const data = await response.json()
+      try {
+        this.status = UPLOADING
+        this.setStatusText()
+        this.spinPlus()
+        const response = await fetch(UPLOAD_URL, { headers, method: 'post', body: formData })
+        const data = await response.json()
 
-      if (data.status === 200) {
-        this.showImages = false
-        this.uploading = false
-        this.stopPlus()
+        if (data.status === 200) {
+          this.showImages = false
+          this.uploading = false
+          this.stopPlus()
 
-        const image = data.image
-        this.pushEventTo(this.el, 'image_uploaded', { id: image.id })
-      } else {
+          const image = data.image
+          this.pushEventTo(this.el, 'image_uploaded', { id: image.id })
+          resolve()
+        } else {
+          this.status = IDLE
+          this.setStatusText()
+          this.stopPlus()
+          console.error('error uploading', { error: data.error })
+          reject()
+        }
+      } catch (e) {
         this.status = IDLE
         this.setStatusText()
         this.stopPlus()
-        console.error('error uploading', { error: data.error })
+        console.error('error uploading', { error: e })
+        reject()
       }
-    } catch (e) {
-      this.status = IDLE
-      this.setStatusText()
-      this.stopPlus()
-      console.error('error uploading', { error: e })
-    }
+    })
   }
 })
