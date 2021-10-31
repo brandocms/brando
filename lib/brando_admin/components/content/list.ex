012bdfffb2ec585e4ae8c003a2099527dbd3634d
T. Mjoen <trond@twined.net>
2021-10-31 11:23:31 +0100

    Restructure Content components

--- lib/brando_admin/components/content/list.ex
+++ lib/brando_admin/components/content/list.ex
@@ -1,5 +1,5 @@
 defmodule BrandoAdmin.Components.Content.List do
-  use Surface.LiveComponent
+  use BrandoAdmin, :live_component
 
   import Brando.Gettext
 
@@ -8,29 +8,7 @@ defmodule BrandoAdmin.Components.Content.List do
   alias Brando.Trait.SoftDelete
   alias Brando.Trait.Status
 
-  alias BrandoAdmin.Components.Content.List.Entries
-  alias BrandoAdmin.Components.Content.List.Pagination
   alias BrandoAdmin.Components.Content.List.Row
-  alias BrandoAdmin.Components.Content.List.SelectedRows
-  alias BrandoAdmin.Components.Content.List.Tools
-
-  prop listing, :any
-  prop blueprint, :any
-  prop current_user, :map, required: true
-  prop uri, :any
-  prop params, :any
-
-  data schema, :module
-  data context, :module
-  data singular, :string
-  data plural, :string
-  data active_filter, :any
-  data list_opts, :any
-  data selected_rows, :list
-  data entries, :list
-  data status?, :boolean
-  data sortable?, :boolean
-  data creator?, :boolean
 
   def mount(socket) do
     {:ok, assign(socket, :selected_rows, [])}
@@ -47,59 +25,6 @@ defmodule BrandoAdmin.Components.Content.List do
      |> assign_listing(assigns)
      |> assign_filter(assigns)
      |> assign_entries(assigns)}
-  end
-
-  def render(assigns) do
-    ~F"""
-    <div class="content-list-wrapper">
-      <article class="content-list" data-moonwalk-run="brandoList">
-        <Tools
-          schema={@schema}
-          listing={@listing}
-          active_filter={@active_filter}
-          list_opts={@list_opts}
-          update_status="update_status"
-          update_filter="update_filter"
-          next_filter_key="next_filter_key"
-          delete_filter="delete_filter" />
-
-        <Entries
-          target={@myself}
-          listing_name={@listing.name}
-          entries={entry <- @entries.entries}>
-          <:empty>
-            <div class="empty-list">
-              <figure>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100" height="100"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/></svg>
-              </figure>
-              {gettext("No matching entries found")}
-            </div>
-          </:empty>
-          <Row
-            id={"list-row-#{@singular}-#{entry.id}"}
-            entry={entry}
-            sortable?={@sortable?}
-            status?={@status?}
-            creator?={@creator?}
-            listing={@listing}
-            schema={@schema}
-            selected_rows={@selected_rows}
-            target={@myself}
-            click="select_row" />
-        </Entries>
-
-        <Pagination
-          pagination_meta={@entries.pagination_meta}
-          change_page="change_page" />
-      </article>
-
-      <SelectedRows
-        selected_rows={@selected_rows}
-        selection_actions={@listing.selection_actions}
-        target={@myself} />
-
-    </div>
-    """
   end
 
   def push_query_params(%{assigns: %{uri: uri}} = socket, extra_params) do
@@ -156,7 +81,7 @@ defmodule BrandoAdmin.Components.Content.List do
         "update_status",
         %{"status" => status},
         %{assigns: %{list_opts: list_opts}} = socket
-      ) do
+    ) do
     status_atom = String.to_existing_atom(status)
     new_list_opts = update_status(list_opts, status_atom)
 
@@ -384,4 +309,254 @@ defmodule BrandoAdmin.Components.Content.List do
   defp update_status(list_opts, status) do
     Map.put(list_opts, :status, status)
   end
+
+  defp active_filters(assigns) do
+    ~H"""
+    <div class="active-filters">
+      Active filters &rarr;
+      <%= for {name, value} <- @active_filters do %>
+        <button
+          class="filter"
+          :on-click={@delete}
+          phx-value-filter={name}>
+          &times; <%= name %>: <%= value %>
+        </button>
+      <% end %>
+    </div>
+    """
+  end
+
+  defp pagination(%{pagination_meta: %{
+              page_size: page_size,
+              current_page: current_page,
+              total_entries: total_entries,
+              total_pages: total_pages
+            },
+            change_page: change_page
+          } = assigns) do
+
+    assigns =
+      assigns
+      |> assign(:change_page, change_page)
+      |> assign(:current_page, current_page)
+      |> assign(:total_entries, total_entries)
+      |> assign(:total_pages, total_pages)
+      |> assign(:showing_start, page_size * current_page - page_size + 1)
+      |> assign(:showing_end, min(page_size * current_page, total_entries))
+
+    ~H"""
+    <div class="pagination">
+      <div class="pagination-entries">
+        &rarr; <%= @total_entries %> <%= gettext("entries") %>
+        <%= if @total_entries > 0 do %>
+        | <%= gettext("showing") %> <%= @showing_start %>-<%= @showing_end %>
+        <% end %>
+      </div>
+      <%= for p <- 0..@total_pages - 1 do %>
+        <button
+          class={[active: p + 1 == @current_page]}
+          :on-click={@change_page}
+          phx-value-page={p}>
+          <%= p + 1 %>
+        </button>
+      <% end %>
+    </div>
+    """
+  end
+
+  defp tools(assigns) do
+    assigns =
+      assigns
+      |> assign(:active_filter, assigns.active_filter)
+      |> assign(:list_opts, assigns.list_opts)
+      |> assign_new(:has_status?, fn -> assigns.schema.has_trait(Brando.Trait.Status) end)
+      |> assign_new(:schema, fn -> assigns.schema end)
+      |> assign_new(:listing, fn -> assigns.listing end)
+      |> assign_new(:update_filter, fn -> assigns.update_filter end)
+      |> assign_new(:delete_filter, fn -> assigns.delete_filter end)
+      |> assign_new(:update_status, fn -> assigns.update_status end)
+      |> assign_new(:next_filter_key, fn -> assigns.next_filter_key end)
+      |> assign_new(:statuses, fn -> get_statuses(assigns.schema) end)
+      |> assign_new(:filters, fn -> assigns.listing.filters end)
+
+    ~H"""
+    <div class="list-tools-wrapper">
+      <div class="list-tools">
+        <%= if @has_status? do %>
+          <div class="statuses">
+            <%= for status <- @statuses do %>
+              <button
+                :on-click={@update_status}
+                phx-value-status={status}
+                class={[
+                  "status",
+                  active_status_class(@list_opts, status)
+                ]}
+                type="button"
+                phx-page-loading>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12">
+                  <circle
+                    class={status}
+                    r="6"
+                    cy="6"
+                    cx="6" />
+                </svg>
+                <span class="label"><%= render_status_label(status) %></span>
+              </button>
+            <% end %>
+          </div>
+        <% end %>
+
+        <div class="filters">
+          <%= for filter <- @filters do %>
+            <div class={[
+              "filter",
+              visible: filter[:filter] == @active_filter[:filter]
+            ]}>
+              <button
+                class="filter-key"
+                :on-click={@next_filter_key}>
+                <%= filter[:label] %>
+              </button>
+              <.form
+                for={:filter_form}
+                change={@update_filter}
+                onkeydown="return event.key != 'Enter';">
+                <input
+                  type="text"
+                  name="q"
+                  value=""
+                  placeholder={gettext("Filter")}
+                  autocomplete="off"
+                  phx-debounce="400"
+                />
+                <input
+                  type="hidden"
+                  name="filter"
+                  value={@active_filter[:filter]}
+                />
+              </.form>
+            </div>
+          <% end %>
+        </div>
+      </div>
+
+      <%= if @list_opts[:filter] do %>
+        <.active_filters
+          active_filters={@list_opts[:filter]}
+          filters={@filters}
+          delete={@delete_filter} />
+      <% end %>
+    </div>
+    """
+  end
+
+  def entries(assigns) do
+    assigns = assign_new(assigns, :empty, fn -> nil end)
+
+    ~H"""
+    <div
+      id={"sortable-#{@listing_name}"}
+      data-target={@target}
+      class="sort-container"
+      phx-hook="Brando.Sortable"
+      data-sortable-id={"content_listing_#{@listing_name}"}
+      data-sortable-handle=".sequence-handle"
+      data-sortable-selector=".list-row">
+      <%= if Enum.empty?(@entries) do %>
+        <%= render_slot(@empty) %>
+      <% end %>
+      <%= for entry <- @entries do %>
+        <%= for {_, index} <- Enum.with_index(@default) do %>
+          <%= render_slot(@inner_block, [entry: entry, index: index]) %>
+        <% end %>
+      <% end %>
+    </div>
+    """
+  end
+
+  def selected_rows(assigns) do
+    assigns =
+      assigns
+      |> assign(:encoded_selected_rows, Jason.encode!(assigns.selected_rows))
+      |> assign(:selected_rows_count, Enum.count(assigns.selected_rows))
+
+    ~H"""
+    <div class={[{:"selected-rows", true}, {:hidden, Enum.empty?(@selected_rows)}]}>
+      <div class="clear-selection">
+        <button
+          phx-click="clear_selection"
+          phx-target={@target}
+          type="button"
+          class="btn-outline-primary inverted">
+          <%= gettext("Clear selection") %>
+        </button>
+      </div>
+      <div class="selection-actions">
+        <%= gettext("With") %>
+        <div class="circle"><span><%= @selected_rows_count %></span></div>
+        <%= gettext("selected, perform action") %>: →
+        <div
+          id="selected_rows_dropdown"
+          class="circle-dropdown wrapper"
+          phx-hook="Brando.CircleDropdown">
+          <button
+            class="circle-dropdown-button"
+            data-testid="circle-dropdown-button"
+            type="button">
+            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="20" cy="20" r="19.5" fill="#0047FF" class="main-circle inverted"></circle>
+              <line x1="12" y1="12.5" x2="28" y2="12.5" stroke="white" class="inverted"></line>
+              <line x1="18" y1="26.5" x2="28" y2="26.5" stroke="white" class="inverted"></line>
+              <line x1="12" y1="19.5" x2="28" y2="19.5" stroke="white" class="inverted"></line>
+              <circle cx="13.5" cy="26.5" r="1.5" fill="white" class="inverted"></circle>
+            </svg>
+          </button>
+          <ul data-testid="circle-dropdown-content" class="dropdown-content">
+            <%= for %{event: event, label: label} <- @selection_actions do %>
+              <li>
+                <button
+                  phx-click={event}
+                  phx-value-ids={@encoded_selected_rows}>
+                  <%= label %>
+                </button>
+              </li>
+            <% end %>
+          </ul>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  defp get_statuses(schema) do
+    soft_delete? = schema.has_trait(SoftDelete)
+
+    if soft_delete? do
+      [:published, :disabled, :draft, :pending, :deleted]
+    else
+      [:published, :disabled, :draft, :pending]
+    end
+  end
+
+  defp active_status_class(list_opts, status) do
+    if active_status?(list_opts, status) do
+      " active"
+    else
+      ""
+    end
+  end
+
+  defp active_status?(%{status: current_status}, status) when current_status == status, do: true
+  defp active_status?(_, _), do: false
+
+  defp render_status_label(:disabled), do: gettext("Disabled")
+  defp render_status_label(:draft), do: gettext("Draft")
+  defp render_status_label(:pending), do: gettext("Pending")
+  defp render_status_label(:published), do: gettext("Published")
+  defp render_status_label(:deleted), do: gettext("Deleted")
 end
--- lib/brando_admin/components/content/list/row.ex
+++ lib/brando_admin/components/content/list/row.ex
@@ -1,23 +1,19 @@
 defmodule BrandoAdmin.Components.Content.List.Row do
-  use Surface.LiveComponent
+  use BrandoAdmin, :live_component
+  import Brando.Utils.Datetime
 
-  alias Brando.Trait.SoftDelete
+  alias Brando.Trait
   alias BrandoAdmin.Components.Content.List.Row
 
-  prop entry, :any
-  prop selected_rows, :list
-  prop listing, :any
-  prop schema, :any
-  prop sortable?, :boolean
-  prop status?, :boolean
-  prop creator?, :boolean
-  prop click, :event, required: true
-  prop target, :any, required: true
-
-  data soft_delete?, :boolean
-  data show_children, :boolean
-  data child_fields, :list
-  data selected?, :boolean
+  # prop entry, :any
+  # prop selected_rows, :list
+  # prop listing, :any
+  # prop schema, :any
+  # prop sortable?, :boolean
+  # prop status?, :boolean
+  # prop creator?, :boolean
+  # prop click, :event, required: true
+  # prop target, :any, required: true
 
   def mount(socket) do
     {:ok, assign(socket, show_children: false, child_fields: [])}
@@ -33,58 +29,344 @@ defmodule BrandoAdmin.Components.Content.List.Row do
      |> assign(assigns)
      |> assign(:selected?, assigns.entry.id in assigns.selected_rows)
      |> assign(:singular, assigns.schema.__naming__().singular)
-     |> assign(:soft_delete?, assigns.schema.has_trait(SoftDelete))}
+     |> assign(:soft_delete?, assigns.schema.has_trait(Trait.SoftDelete))}
   end
 
   def render(assigns) do
-    ~F"""
+    ~H"""
     <div
       id={"list-row-#{@entry.id}"}
-      class={"list-row", "draggable", selected: @selected?}
+      class={["list-row", "draggable", selected: @selected?]}
       data-id={@entry.id}
       :on-click={@click}
       phx-value-id={@entry.id}
       phx-page-loading>
       <div class="main-content">
-        {#if @sortable?}
-          <Row.Handle />
-        {/if}
+        <%= if @sortable? do %>
+          <.handle />
+        <% end %>
 
-        {#if @status?}
-          <Row.Status
+        <%= if @status? do %>
+          <.status
             entry={@entry}
             soft_delete?={@soft_delete?} />
-        {/if}
+        <% end %>
 
-        {#for field <- @listing.fields}
-          <Row.Field
+        <%= for field <- @listing.fields do %>
+          <.field
             field={field}
             entry={@entry}
             schema={@schema} />
-        {/for}
+        <% end %>
 
-        {#if @creator?}
-          <Row.Creator
+        <%= if @creator? do %>
+          <.creator
             entry={@entry}
             soft_delete?={@soft_delete?}/>
-        {/if}
+        <% end %>
 
-        <Row.EntryMenu
+        <.entry_menu
           entry={@entry}
           listing={@listing} />
       </div>
 
-      {#if @show_children}
-        {#for child_field <- @child_fields}
-          {#for child_entry <- Map.get(@entry, child_field)}
-            <Row.ChildRow
+      <%= if @show_children do %>
+        <%= for child_field <- @child_fields do %>
+          <%= for child_entry <- Map.get(@entry, child_field) do %>
+            <.child_row
               entry={child_entry}
               schema={@schema}
               child_listing={@listing.child_listing} />
-          {/for}
-        {/for}
-      {/if}
+          <% end %>
+        <% end %>
+      <% end %>
     </div>
     """
   end
+
+  def field(assigns) do
+    attr = assigns.schema.__attribute__(assigns.field.name)
+    entry_field = Map.get(assigns.entry, assigns.field.name)
+    class = Keyword.get(assigns.field.opts, :class)
+    columns = Keyword.get(assigns.field.opts, :columns)
+    offset = Keyword.get(assigns.field.opts, :offset)
+
+    assigns =
+      assigns
+      |> assign(:attr, attr)
+      |> assign(:entry_field, entry_field)
+      |> assign(:class, class)
+      |> assign(:columns, columns)
+      |> assign(:offset, offset)
+
+    assigns =
+      if assigns.field.__struct__ == Template do
+        assign_new(assigns, :rendered_tpl, fn ->
+          if tpl = Map.get(assigns.field, :template) do
+            {:ok, parsed_template} = Liquex.parse(tpl, Brando.Villain.LiquexParser)
+
+            context = Brando.Villain.get_base_context(assigns.entry)
+
+            Liquex.Render.render([], parsed_template, context)
+            |> elem(0)
+            |> Enum.join()
+            |> Phoenix.HTML.raw()
+          end
+        end)
+      end
+
+    ~H"""
+    <%= if @rendered_tpl do %>
+      <div class={[@class, "col-#{@columns}": @columns]}>
+        <%= @rendered_tpl %>
+      </div>
+    <% else %>
+      <%= case @field.type do %>
+        <% :image -> %>
+          <div
+            class={[@class, "col-#{@columns}": @columns, "offset-#{@offset}": @offset]}>
+            <%= if @entry_field do %>
+              <img src={"/media/#{Map.get(@entry_field.sizes, "thumb")}"}>
+            <% else %>
+              <div class="img-placeholder">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M4.828 21l-.02.02-.021-.02H2.992A.993.993 0 0 1 2 20.007V3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H4.828zM20 15V5H4v14L14 9l6 6zm0 2.828l-6-6L6.828 19H20v-1.172zM8 11a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>
+              </div>
+            <% end %>
+          </div>
+
+        <% :children_button -> %>
+          <div
+            class={[{@class, true}, "col-#{@columns}": @columns, "offset-#{@offset}": @offset]}>
+            <ChildrenButton.live_component
+              id={"#{@entry.id}-children-button"}
+              fields={@field.name}
+              entry={@entry} />
+          </div>
+
+        <% :language -> %>
+          <div
+            class={[{@class, true}, {:"col-#{@columns}", @columns}, {:"offset-#{@offset}", @offset}]}>
+            <Badge.language language={@entry_field} />
+          </div>
+      <% end %>
+    <% end %>
+    """
+  end
+
+  def entry_menu(assigns) do
+    language = Map.get(assigns.entry, :language)
+    assigns =
+      assigns
+      |> assign(:language, language)
+      |> assign(:no_actions, Enum.empty?(assigns.listing.actions))
+
+    ~H"""
+    <%= unless @no_actions do %>
+      <div
+        id={"entry_dropdown_#{@entry.id}"}
+        class="circle-dropdown wrapper"
+        phx-hook="Brando.CircleDropdown">
+        <button
+          class="circle-dropdown-button"
+          data-testid="circle-dropdown-button"
+          type="button">
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="20" cy="20" r="19.5" fill="#0047FF" class="main-circle"></circle>
+            <line x1="12" y1="12.5" x2="28" y2="12.5" stroke="white"></line>
+            <line x1="18" y1="26.5" x2="28" y2="26.5" stroke="white"></line>
+            <line x1="12" y1="19.5" x2="28" y2="19.5" stroke="white"></line>
+            <circle cx="13.5" cy="26.5" r="1.5" fill="white"></circle>
+          </svg>
+        </button>
+        <ul data-testid="circle-dropdown-content" class="dropdown-content">
+          <%= for %{event: event, label: label} = action <- @listing.actions do %>
+            <li>
+              <%= if action[:confirm] do %>
+                <button
+                  id={"action_#{event}_#{@entry.id}"}
+                  phx-hook="Brando.ConfirmClick"
+                  phx-confirm-click-message={action[:confirm]}
+                  phx-confirm-click={event}
+                  phx-value-language={@language}
+                  phx-value-id={@entry.id}>
+                  <%= label %>
+                </button>
+              <% else %>
+                <button
+                  id={"action_#{event}_#{@entry.id}"}
+                  phx-value-id={@entry.id}
+                  phx-value-language={@language}
+                  phx-click={event}>
+                  <%= label %>
+                </button>
+              <% end %>
+            </li>
+          <% end %>
+        </ul>
+      </div>
+    <% end %>
+    """
+  end
+
+  def handle(assigns) do
+    ~H"""
+    <div class="col-1">
+      <div class="center sequence-handle">
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="1.5" cy="1.5" r="1.5"></circle><circle cx="7.5" cy="1.5" r="1.5"></circle><circle cx="13.5" cy="1.5" r="1.5"></circle><circle cx="1.5" cy="7.5" r="1.5"></circle><circle cx="7.5" cy="7.5" r="1.5"></circle><circle cx="13.5" cy="7.5" r="1.5"></circle><circle cx="1.5" cy="13.5" r="1.5"></circle><circle cx="7.5" cy="13.5" r="1.5"></circle><circle cx="13.5" cy="13.5" r="1.5"></circle></svg>
+      </div>
+    </div>
+    """
+  end
+
+  def status(assigns) do
+    assigns =
+      assign_new(assigns, :json_statuses, fn -> json_statuses() end)
+
+    ~H"""
+    <%= cond do %>
+      <% @soft_delete? and @entry.deleted_at -> %>
+        <div class="status">
+          <div center="true">
+            <svg data-testid="status-deleted" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><circle r="7.5" cy="7.5" cx="7.5" class="deleted"></circle></svg>
+          </div>
+        </div>
+      <% @entry.status == :pending and @entry.publish_at -> %>
+        <div class="status">
+          <div
+            id={"entry_status_#{@entry.id}"}
+            data-statuses={@json_statuses}
+            phx-hook="Brando.StatusDropdown"
+            phx-value-id={@entry.id}>
+            <svg
+              data-testid="status-pending"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <circle class="pending" cx="7.5" cy="7.5" r="7.5" />
+              <line x1="7.5" y1="3" x2="7.5" y2="7" stroke="white" />
+              <line x1="3.5" y1="7.5" x2="8" y2="7.5" stroke="white" />
+            </svg>
+          </div>
+        </div>
+      <% true -> %>
+        <div class="status">
+          <div
+            id={"entry_status_#{@entry.id}"}
+            data-statuses={json_statuses()}
+            phx-hook="Brando.StatusDropdown"
+            phx-value-id={@entry.id}>
+            <svg
+              data-testid={"status-#{@entry.status}"}
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15">
+              <circle r="7.5" cy="7.5" cx="7.5" class={@entry.status} />
+            </svg>
+          </div>
+        </div>
+    <% end %>
+    """
+  end
+
+  def creator(%{entry: %{creator: %{avatar: avatar}}} = assigns) do
+    assigns =
+      assign(
+        assigns,
+        :avatar,
+        Brando.Utils.img_url(avatar, :thumb,
+          prefix: "/media",
+          default: "/images/admin/avatar.svg"
+        )
+      )
+
+    ~H"""
+    <div class="col-4">
+      <article class="item-meta">
+        <section class="avatar-wrapper">
+          <div class="avatar">
+            <img src={@avatar}>
+          </div>
+        </section>
+        <section class="content">
+          <div class="info">
+            <div class="name">
+              <%= @entry.creator.name %>
+            </div>
+
+            <div
+              class="time"
+              id={"entry_creator_time_icon_#{@entry.id}"}
+              data-popover={"The time the entry was #{@soft_delete? and @entry.deleted_at && "deleted" || "created"}"}>
+              <%= if @soft_delete? and @entry.deleted_at do %>
+                <%= format_datetime(@entry.deleted_at, "%d/%m/%y") %> <span>•</span> <%= format_datetime(@entry.deleted_at, "%H:%M") %>
+              <% else %>
+                <%= format_datetime(@entry.updated_at, "%d/%m/%y") %> <span>•</span> <%= format_datetime(@entry.updated_at, "%H:%M") %>
+              <% end %>
+            </div>
+          </div>
+        </section>
+      </article>
+    </div>
+    """
+  end
+
+  def child_row(%{schema: schema, entry: entry, child_listing: child_listing} = assigns) do
+    assigns =
+      assigns
+      |> assign(:sortable?, entry.__struct__.has_trait(Trait.Sequenced))
+      |> assign(:creator?, entry.__struct__.has_trait(Trait.Creator))
+      |> assign(:status?, entry.__struct__.has_trait(Trait.Status))
+      |> assign(:soft_delete?, entry.__struct__.has_trait(Trait.SoftDelete))
+      |> assign_new(:listing, fn ->
+        entry_schema = entry.__struct__
+
+        listing_for_schema = Keyword.fetch!(child_listing, entry_schema)
+        listing = Enum.find(schema.__listings__, &(&1.name == listing_for_schema))
+
+        if !listing do
+          raise "No listing `#{inspect(listing_for_schema)}` found for `#{inspect(entry_schema)}`"
+        end
+
+        listing
+      end)
+
+    ~H"""
+    <div class="child-content">
+      <%= if @status? do %>
+        <.status
+          entry={@entry}
+          soft_delete?={@soft_delete?} />
+      <% end %>
+      <%= for field <- @listing.fields do %>
+        <.field
+          field={field}
+          entry={@entry}
+          schema={@schema} />
+      <% end %>
+      <%= if @creator? do %>
+        <.creator
+          entry={@entry}
+          soft_delete?={@soft_delete?}/>
+      <% end %>
+      <.entry_menu
+        entry={@entry}
+        listing={@listing} />
+    </div>
+    """
+  end
+
+  defp json_statuses() do
+    [:published, :disabled, :draft, :pending]
+    |> Enum.map(&{&1, render_status_label(&1)})
+    |> Jason.encode!()
+  end
+
+  defp render_status_label(:disabled), do: gettext("Disabled")
+  defp render_status_label(:draft), do: gettext("Draft")
+  defp render_status_label(:pending), do: gettext("Pending")
+  defp render_status_label(:published), do: gettext("Published")
+  defp render_status_label(:deleted), do: gettext("Deleted")
 end
