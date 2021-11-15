2a78b4ef13b8c66ef0ac516f03f3865f720cff75
T. Mjoen <trond@twined.net>
2021-11-15 13:23:19 +0100

    Remove unused migration

--- priv/templates/brando.upgrade/migrations/brando_82_remove_sites_identities_image.exs
+++
@@ -1,9 +0,0 @@
-defmodule Brando.Repo.Migrations.RemoveSitesIdentitiesImage do
-  use Ecto.Migration
-
-  def change do
-    alter table(:sites_identities) do
-      remove :image
-    end
-  end
-end
