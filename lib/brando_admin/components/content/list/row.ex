50801e43fadffeae74726f7d93887f9267f20f0d
T. Mjoen <trond@twined.net>
2021-12-15 15:48:10 +0100

    Add :url listing field for absolute_url

--- lib/brando_admin/components/content/list/row.ex
+++ lib/brando_admin/components/content/list/row.ex
@@ -177,6 +177,14 @@ defmodule BrandoAdmin.Components.Content.List.Row do
           class={render_classes([@class, {:"col-#{@columns}", @columns}, {:"offset-#{@offset}", @offset}])}>
           <Badge.language language={@entry_field} />
         </div>
+
+      <% :url -> %>
+        <div
+          class={render_classes([@class, "col-1", {:"offset-#{@offset}", @offset}])}>
+          <a href={@schema.__absolute_url__(@entry)} target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M18.364 15.536L16.95 14.12l1.414-1.414a5 5 0 1 0-7.071-7.071L9.879 7.05 8.464 5.636 9.88 4.222a7 7 0 0 1 9.9 9.9l-1.415 1.414zm-2.828 2.828l-1.415 1.414a7 7 0 0 1-9.9-9.9l1.415-1.414L7.05 9.88l-1.414 1.414a5 5 0 1 0 7.071 7.071l1.414-1.414 1.415 1.414zm-.708-10.607l1.415 1.415-7.071 7.07-1.415-1.414 7.071-7.07z"/></svg>
+          </a>
+        </div>
     <% end %>
     """
   end
