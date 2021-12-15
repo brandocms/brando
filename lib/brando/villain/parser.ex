2eacb4f89ba16d6cd16ff515fc6bfa5e5aabdf40
T. Mjoen <trond@twined.net>
2021-12-15 15:49:09 +0100

    Allow container with empty palette, but show error

--- lib/brando/villain/parser.ex
+++ lib/brando/villain/parser.ex
@@ -772,21 +772,33 @@ defmodule Brando.Villain.Parser do
       def container(%{blocks: blocks, palette_id: palette_id}, opts) do
         palettes = opts.palettes
 
-        {:ok, palette} = Content.find_palette(palettes, palette_id)
+        with {:ok, palette} <- Content.find_palette(palettes, palette_id) do
+          blocks_html =
+            (blocks || [])
+            |> Enum.reduce([], fn d, acc ->
+              [apply(__MODULE__, String.to_atom(d.type), [d.data, opts]) | acc]
+            end)
+            |> Enum.reverse()
+            |> Enum.join("")
 
-        blocks_html =
-          (blocks || [])
-          |> Enum.reduce([], fn d, acc ->
-            [apply(__MODULE__, String.to_atom(d.type), [d.data, opts]) | acc]
-          end)
-          |> Enum.reverse()
-          |> Enum.join("")
-
-        """
-        <section b-section="#{palette.namespace}-#{palette.key}">
-          #{blocks_html}
-        </section>
-        """
+          """
+          <section b-section="#{palette.namespace}-#{palette.key}">
+            #{blocks_html}
+          </section>
+          """
+        else
+          {:error, {:palette, :not_found, nil}} ->
+            """
+            <div>
+              <code>>>> Error parsing container module <<<</code><br><br>
+              <code>
+                Could not find palette [#{palette_id}].<br>
+                Make sure that you have selected an existing and
+                valid palette in the container block.
+              </code>
+            </div>
+            """
+        end
       end
 
       defoverridable container: 2
