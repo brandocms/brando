47bd87103fbe31ed4ef819194376664f621d24c8
T. Mjoen <trond@twined.net>
2022-01-07 20:14:06 +0100

    Set zero index on empty table

--- lib/brando_admin/components/form/input/blocks/table_block.ex
+++ lib/brando_admin/components/form/input/blocks/table_block.ex
@@ -72,7 +72,7 @@ defmodule BrandoAdmin.Components.Form.Input.Blocks.TableBlock do
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M14 10h-4v4h4v-4zm2 0v4h3v-4h-3zm-2 9v-3h-4v3h4zm2 0h3v-3h-3v3zM14 5h-4v3h4V5zm2 0v3h3V5h-3zm-8 5H5v4h3v-4zm0 9v-3H5v3h3zM8 5H5v3h3V5zM4 3h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1z"/></svg>
             </figure>
             <div class="instructions">
-              <button type="button" class="tiny" phx-click={JS.push("add_row", target: @myself)}><%= gettext "Add row" %></button>
+              <button type="button" class="tiny" phx-click={JS.push("add_row", value: %{idx: 0}, target: @myself)}><%= gettext "Add row" %></button>
             </div>
           </div>
         <% else %>
