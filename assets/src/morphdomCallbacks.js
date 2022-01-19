1d7fee2b028a6a52032861fdefd87fccc41d8a8e
T. Mjoen <trond@twined.net>
2022-01-19 21:44:04 +0100

    Strip comments

--- assets/src/morphdomCallbacks.js
+++ assets/src/morphdomCallbacks.js
@@ -12,16 +12,6 @@ export default {
     if (fromEl.className === 'tiptap-menu') {
       return false
     }
-    
-    // console.log('fromEl.innerHTML === toEl.innerHTML', fromEl.innerHTML === toEl.innerHTML)
-    // console.log('doing something from =>', fromEl.innerHTML)
-    // console.log('doing something to =>', toEl.innerHTML)
-
-    // if (fromEl.innerHTML.includes('article-credits-rich-text-target-wrapper')) {
-    //   console.log('fromEl.innerHTML === toEl.innerHTML', fromEl.innerHTML === toEl.innerHTML)
-    //   console.log('doing something from =>', fromEl.innerHTML)
-    //   console.log('doing something to =>', toEl.innerHTML)
-    // }
 
     if (fromEl.style.cssText) {
       toEl.style.cssText = !toEl.style.cssText ? fromEl.style.cssText : toEl.style.cssText
