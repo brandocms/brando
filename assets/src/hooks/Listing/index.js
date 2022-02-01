bbd02e0ba38212776b9529a9ecaddd6f75e82439
T. Mjoen <trond@twined.net>
2022-02-01 14:52:04 +0100

    Undo listing test

---
+++ assets/src/hooks/Listing/index.js
@@ -0,0 +1,16 @@
+import { Dom } from '@brandocms/jupiter'
+
+export default (app) => ({
+  mounted () {
+    this.entryLinks = Dom.all(this.el, '.entry-link')
+    Array.from(this.entryLinks).forEach(l => l.addEventListener('click', this.anchorListener, false))
+  },
+
+  destroyed() { 
+    Array.from(this.entryLinks).forEach(l => l.removeEventListener('click', this.anchorListener, false))
+  },
+
+  anchorListener (ev) {
+    // ev.stopPropagation()
+  }
+})
