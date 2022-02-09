891205f24f28e0eb51149458916cd5ad3313f062
T. Mjoen <trond@twined.net>
2022-02-09 16:55:54 +0100

    Ensure table rows have uids

---
+++ lib/brando/traits/ensure_uid.ex
@@ -0,0 +1,14 @@
+defmodule Brando.Trait.EnsureUID do
+  @moduledoc """
+  Ensure UID field is set by generating if not
+  """
+  use Brando.Trait
+  import Ecto.Changeset
+
+  def changeset_mutator(_module, _config, changeset, _user, _opts) do
+    case get_field(changeset, :uid) do
+      nil -> put_change(changeset, :uid, Brando.Utils.generate_uid())
+      uid -> changeset
+    end
+  end
+end
