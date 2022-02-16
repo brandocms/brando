434728f090f3017db8fef0fdeb69c653efe11747
T. Mjoen <trond@twined.net>
2022-02-16 14:41:30 +0100

    Update preview colors on change

--- assets/src/hooks/ColorPicker/index.js
+++ assets/src/hooks/ColorPicker/index.js
@@ -6,25 +6,42 @@ export default (app) => ({
     const initialColor = this.el.dataset.color
     const inputTarget = Dom.find(this.el.dataset.input)
 
-    const circle = this.el.querySelector('.circle')
-    const colorHex = this.el.querySelector('.color-hex')
+    this.circle = this.el.querySelector('.circle')
+    this.colorHex = this.el.querySelector('.color-hex')
 
-    circle.style.background = initialColor
-    colorHex.innerHTML = initialColor
+    this.setColor(initialColor)
 
     const parent = this.el.querySelector('.picker-target')
-    const picker = new Picker({ parent: parent, popup: 'left', color: initialColor || '#000000', alpha: false })
+    this.picker = new Picker({ parent: parent, popup: 'top', color: initialColor || '#000000', alpha: false })
 
     // You can do what you want with the chosen color using two callbacks: onChange and onDone.
-    picker.onChange = function (color) {
+    this.picker.onChange = function (color) {
       let processedColor = color.printHex(false)
       if (processedColor === 9 && processedColor.slice(-2) === 'ff') {
         processedColor = processedColor.slice(0, -2)
       }
       inputTarget.value = processedColor
-      circle.style.background = processedColor
-      colorHex.innerHTML = processedColor
+      this.circle.style.background = processedColor
+      this.colorHex.innerHTML = processedColor
       inputTarget.dispatchEvent(new Event('input', { bubbles: true }))
     }
+
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'data-color') {
+          const currentColor = this.el.dataset.color
+          this.setColor(currentColor)
+        }
+      })
+    })
+
+    observer.observe(this.el, {
+      attributes: true //configure it to listen to attribute changes
+    })
+  },
+
+  setColor (color) {
+    this.circle.style.background = color
+    this.colorHex.innerHTML = color
   }
 })
