ffe53d7b231ddc03a4fd3a7d23a6c0457c5a58f3
T. Mjoen <trond@twined.net>
2022-02-18 16:05:27 +0100

    Improve liquid regex stripping

--- lib/brando_admin/components/form/input/blocks/module_block.ex
+++ lib/brando_admin/components/form/input/blocks/module_block.ex
@@ -474,7 +474,7 @@ defmodule BrandoAdmin.Components.Form.Input.Blocks.ModuleBlock do
     {:noreply, socket}
   end
 
-  @regex_strips ~r/((?:{%(?:-)? for (\w+) in [a-zA-Z0-9_.?|"-]+ (?:-)?%})(?:.*?)(?:{%(?:-)? endfor (?:-)?%}))|(<img.*?src="{{(?:-)? .*? (?:-)?}}".*?>)|({%(?:-)? assign .*? (?:-)?%})|(({%(?:-)? if .* (?:-)?%}(?:.*?){%(?:-)? endif (?:-)?%}))|(({%(?:-)? unless .* (?:-)?%}(?:.*?){%(?:-)? endunless (?:-)?%}))|(data-moonwalk-run(?:="\w+")|data-moonwalk-run|data-moonwalk-section(?:="\w+")|data-moonwalk-section|href(?:="[a-zA-Z0-9{}._\s]+"))/s
+  @regex_strips ~r/((?:{%(?:-)? for (\w+) in [a-zA-Z0-9_.?|"-]+ (?:-)?%})(?:.*?)(?:{%(?:-)? endfor (?:-)?%}))|(<img.*?src="{{(?:-)? .*? (?:-)?}}".*?>)|({%(?:-)? assign .*? (?:-)?%})|(((?:{%(?:-)? if .*? (?:-)?%})(?:.*?)(?:{%(?:-)? endif (?:-)?%})))|(((?:{%(?:-)? unless .*? (?:-)?%})(?:.*?)(?:{%(?:-)? endunless (?:-)?%})))|(data-moonwalk-run(?:="\w+")|data-moonwalk-run|data-moonwalk-section(?:="\w+")|data-moonwalk-section|href(?:="[a-zA-Z0-9{}._\s]+"))/s
   @regex_splits ~r/{% (?:ref|headless_ref) refs.(\w+) %}|<.*?>|\{\{\s?(.*?)\s?\}\}|{% picture ([a-zA-Z0-9_.?|"-]+) {.*} %}/
   @regex_chunks ~r/^{% (?:ref|headless_ref) refs.(?<ref>\w+) %}$|^{{ (?<content>[\w\s.|\"\']+) }}$|^{% picture (?<picture>[a-zA-Z0-9_.?|"-]+) {.*} %}$/
 
