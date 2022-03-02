7a3a362458a15b4dacbaacee33c48d59d46107d8
T. Mjoen <trond@twined.net>
2022-03-02 16:06:35 +0100

    More comprehensive logs in dev

--- priv/templates/brando.install/config/dev.exs
+++ priv/templates/brando.install/config/dev.exs
@@ -35,10 +35,12 @@ config :<%= application_name %>, <%= application_module %>Web.Endpoint,
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+config :logger, truncate: :infinity
+config :logger, :console, truncate: :infinity
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 30
+config :phoenix, :stacktrace_depth, 60
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
