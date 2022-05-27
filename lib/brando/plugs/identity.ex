6ed8e624bc0752d4ee8afea0e7a4383c97400194
T. Mjoen <trond@twined.net>
2022-05-27 11:53:48 +0200

    Get identity for current locale
    Closes #1685

--- lib/brando/plugs/identity.ex
+++ lib/brando/plugs/identity.ex
@@ -7,7 +7,8 @@ defmodule Brando.Plug.Identity do
 
   @doc false
   def call(conn, _) do
-    identity = Brando.Cache.get(:identity)
+    locale = Gettext.get_locale(Brando.web_module(Gettext))
+    identity = Brando.Cache.Identity.get(locale)
     Plug.Conn.assign(conn, :identity, identity)
   end
 end
