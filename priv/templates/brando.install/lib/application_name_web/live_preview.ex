748648a758e456d8e29fb266a0eb4ecb3d5e3e5a
T. Mjoen <trond@twined.net>
2022-11-13 21:43:45 +0100

    Use new syntax for live preview

--- priv/templates/brando.install/lib/application_name_web/live_preview.ex
+++ priv/templates/brando.install/lib/application_name_web/live_preview.ex
@@ -3,9 +3,8 @@ defmodule <%= application_module %>Web.LivePreview do
   alias Brando.Pages
 
   preview_target Pages.Page do
-    layout_module <%= application_module %>Web.LayoutView
-    view_module <%= application_module %>Web.PageView
-    view_template fn entry -> entry.template end
+    layout {<%= application_module %>Web.Layouts, "app"}
+    template fn e -> {<%= application_module %>Web.PageHTML, e.template} end
     template_section fn entry -> entry.uri end
     template_prop :page
   end
