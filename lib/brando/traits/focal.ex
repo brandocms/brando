c2c93dd8ef3ef1c89a249019defbaa0b0ac31d91
T. Mjoen <trond@twined.net>
2022-12-09 10:14:14 +0100

    Don’t queue processing for focal changes
    Fixes #1880

--- lib/brando/traits/focal.ex
+++ lib/brando/traits/focal.ex
@@ -1,18 +1,6 @@
 defmodule Brando.Trait.Focal do
   use Brando.Trait
 
-  def after_save(_entry, %{changes: %{path: _}}, _) do
-    :ok
-  end
-
-  def after_save(entry, %{changes: %{focal: _}}, user) do
-    Brando.Images.Processing.recreate_sizes_for_image(entry, user)
-  end
-
-  def after_save(_, _, _) do
-    :ok
-  end
-
   def changeset_mutator(_module, _config, %{changes: %{focal: _}} = changeset, _user, _opts) do
     Ecto.Changeset.put_change(changeset, :status, :unprocessed)
   end
