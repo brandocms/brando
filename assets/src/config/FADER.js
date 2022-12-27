b83e14b0aac10ea5c72125b29b6494c96ca147d2
T. Mjoen <trond@twined.net>
2022-12-27 15:00:18 +0100

    Speed up fade in

--- assets/src/config/FADER.js
+++ assets/src/config/FADER.js
@@ -4,8 +4,8 @@ export default () => ({
   fadeIn: callback => {
     gsap.to('.fader', {
       opacity: 0,
-      duration: 0.75,
-      delay: 0.25,
+      duration: 0.25,
+      delay: 0,
       ease: 'none',
       onComplete: () => {
         gsap.set('.fader', { display: 'none' })
