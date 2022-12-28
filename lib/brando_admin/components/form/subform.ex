f1c823db7f9b1b1ec3add5f44dcea9a2804f1e97
T. Mjoen <trond@twined.net>
2022-12-28 13:19:37 +0100

    Wrong incantation for assocs this time it seems :X

--- lib/brando_admin/components/form/subform.ex
+++ lib/brando_admin/components/form/subform.ex
@@ -75,47 +75,21 @@ defmodule BrandoAdmin.Components.Form.Subform do
         <div
           id={"#{@form.id}-#{@subform.field}-sortable"}
           data-embeds={@embeds?}
-          phx-hook={@sequenced? && "Brando.SubFormSortable"}>
-          <%= if Enum.empty?(inputs_for(@form, @subform.field)) do %>
-            <input type="hidden" name={"#{@form.name}[#{@subform.field}]"} value="" />
-            <div class="subform-empty">&rarr; <%= gettext "No associated entries" %></div>
-          <% end %>
+          phx-hook={@sequenced? && "Brando.SubFormSortable"}
+        >
+          <.empty_subform
+            :if={@indexed_sub_form_fields == []}
+            form={@form}
+            subform_field={@subform.field}
+          />
           <div
             :for={{sub_form, index} <- @indexed_sub_form_fields}
             class={render_classes(["subform-entry", "group", listing: index not in @open_entries])}
             data-id={index}>
             <div class="subform-tools">
-              <button
-                phx-click={JS.push("edit_subentry", value: %{index: index}, target: @myself)}
-                class="subform-edit"
-                type="button"
-                phx-page-loading>
-                <svg :if={index not in @open_entries} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                </svg>
-                <svg :if={index in @open_entries} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </button>
-              <button
-                :if={@sequenced?}
-                type="button"
-                class="subform-handle"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                </svg>
-              </button>
-              <button
-                phx-click={JS.push("remove_subentry", target: @myself)}
-                phx-value-index={index}
-                type="button"
-                class="subform-delete"
-                phx-page-loading>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="16" height="16">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg>
-              </button>
+              <.subentry_edit on_click={JS.push("edit_subentry", value: %{index: index}, target: @myself)} open={index in @open_entries} />
+              <.subentry_sequence :if={@sequenced?} />
+              <.subentry_remove on_click={JS.push("remove_subentry", target: @myself, value: %{index: index})} />
             </div>
             <.listing subform={sub_form} subform_config={@subform} />
             <div class="subform-fields">
@@ -140,29 +114,15 @@ defmodule BrandoAdmin.Components.Form.Subform do
           </div>
         </div>
         <div class="actions">
-          <button
-            id={"#{@form.id}-#{@subform.field}-add-entry"}
-            type="button"
-            class="add-entry-button"
-            phx-click={JS.push("add_subentry", target: @myself)}
-            phx-page-loading>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M18 15l-.001 3H21v2h-3.001L18 23h-2l-.001-3H13v-2h2.999L16 15h2zm-7 3v2H3v-2h8zm10-7v2H3v-2h18zm0-7v2H3V4h18z" fill="rgba(252,245,243,1)"/></svg>
-            <%= gettext("Add entry") %>
-          </button>
-          <label class="upload-button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
-            <%= gettext("Pick files") %>
-            <.live_file_input upload={@upload_field} />
-          </label>
+          <.subentry_add on_click={JS.push("add_subentry", target: @myself)} />
+          <.transformer_upload upload_field={@upload_field} />
         </div>
       </Form.field_base>
     </fieldset>
     """
   end
 
-  def render(assigns) do
+  def render(%{subform: %{style: :inline}} = assigns) do
     ~H"""
     <fieldset>
       <Form.field_base
@@ -171,10 +131,12 @@ defmodule BrandoAdmin.Components.Form.Subform do
         field={@subform.field}
         label={@label}
         instructions={@instructions}
-        class={"subform"}>
+        class={"subform"}
+      >
         <div
           :for={sub_form <- inputs_for(@form, @subform.field)}
-          class="subform-entry">
+          class="subform-entry"
+        >
           <Subform.Field.render
             :for={input <- @subform.sub_fields}
             cardinality={:one}
@@ -186,7 +148,8 @@ defmodule BrandoAdmin.Components.Form.Subform do
             input={input}
             path={@path}
             uploads={@uploads}
-            current_user={@current_user} />
+            current_user={@current_user}
+          />
         </div>
       </Form.field_base>
       <Form.field_base
@@ -195,55 +158,125 @@ defmodule BrandoAdmin.Components.Form.Subform do
         field={@subform.field}
         label={@label}
         instructions={@instructions}
-        class={"subform"}>
+        class={"subform"}
+      >
         <div
           id={"#{@form.id}-#{@subform.field}-sortable"}
           data-embeds={@embeds?}
-          phx-hook="Brando.SubFormSortable">
-          <%= if Enum.empty?(inputs_for(@form, @subform.field)) do %>
-            <input type="hidden" name={"#{@form.name}[#{@subform.field}]"} value="" />
-            <div class="subform-empty">&rarr; <%= gettext "No associated entries" %></div>
-          <% end %>
+          phx-hook="Brando.SubFormSortable"
+        >
+          <.empty_subform
+            :if={@indexed_sub_form_fields == []}
+            form={@form}
+            subform_field={@subform.field}
+          />
           <div
             :for={{sub_form, index} <- @indexed_sub_form_fields}
             class={render_classes(["subform-entry", inline: @subform.style == :inline])}
-            data-id={index}>
+            data-id={index}
+          >
             <div class="subform-tools">
-              <button type="button" class="subform-handle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path class="s" d="M12 2l4.243 4.243-1.415 1.414L12 4.828 9.172 7.657 7.757 6.243 12 2zM2 12l4.243-4.243 1.414 1.415L4.828 12l2.829 2.828-1.414 1.415L2 12zm20 0l-4.243 4.243-1.414-1.415L19.172 12l-2.829-2.828 1.414-1.415L22 12zm-10 2a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 8l-4.243-4.243 1.415-1.414L12 19.172l2.828-2.829 1.415 1.414L12 22z" fill="rgba(5,39,82,1)"/></svg>
-              </button>
-              <button
-                phx-click={JS.push("remove_subentry", target: @myself)}
-                phx-value-index={index}
-                type="button"
-                class="subform-delete"
-                phx-page-loading>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path class="s" d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-4.586 6l1.768 1.768-1.414 1.414L12 15.414l-1.768 1.768-1.414-1.414L10.586 14l-1.768-1.768 1.414-1.414L12 12.586l1.768-1.768 1.414 1.414L13.414 14zM9 4v2h6V4H9z" fill="rgba(5,39,82,1)"/></svg>
-              </button>
+              <.subentry_sequence :if={@sequenced?} />
+              <.subentry_remove on_click={JS.push("remove_subentry", target: @myself, value: %{index: index})} />
             </div>
 
-            <Subform.Field.render
-              :for={input <- @subform.sub_fields}
-              cardinality={:many}
-              form={@form}
-              sub_form={sub_form}
-              input={input}
-              path={@path ++ [index]}
-              uploads={@uploads}
-              current_user={@current_user} />
+            <div class="subform-fields">
+              <Input.hidden
+                :if={@sequenced?}
+                form={sub_form}
+                field={:sequence}
+                value={index}
+              />
+              <%= Phoenix.HTML.Form.hidden_inputs_for(sub_form) %>
+              <Subform.Field.render
+                :for={input <- @subform.sub_fields}
+                cardinality={:many}
+                form={@form}
+                sub_form={sub_form}
+                input={input}
+                path={@path ++ [index]}
+                uploads={@uploads}
+                current_user={@current_user} />
+              </div>
           </div>
         </div>
-        <button
-          id={"#{@form.id}-#{@subform.field}-add-entry"}
-          type="button"
-          class="add-entry-button"
-          phx-click={JS.push("add_subentry", target: @myself)}
-          phx-page-loading>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M18 15l-.001 3H21v2h-3.001L18 23h-2l-.001-3H13v-2h2.999L16 15h2zm-7 3v2H3v-2h8zm10-7v2H3v-2h18zm0-7v2H3V4h18z" fill="rgba(252,245,243,1)"/></svg>
-          <%= gettext("Add entry") %>
-        </button>
+        <.subentry_add on_click={JS.push("add_subentry", target: @myself)} />
       </Form.field_base>
     </fieldset>
+    """
+  end
+
+  def transformer_upload(assigns) do
+    ~H"""
+    <label class="upload-button">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+      <%= gettext("Pick files") %>
+      <.live_file_input upload={@upload_field} />
+    </label>
+    """
+  end
+
+  def subentry_add(assigns) do
+    ~H"""
+    <button
+      type="button"
+      class="add-entry-button"
+      phx-click={@on_click}
+      phx-page-loading>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M18 15l-.001 3H21v2h-3.001L18 23h-2l-.001-3H13v-2h2.999L16 15h2zm-7 3v2H3v-2h8zm10-7v2H3v-2h18zm0-7v2H3V4h18z" fill="rgba(252,245,243,1)"/></svg>
+      <%= gettext("Add entry") %>
+    </button>
+    """
+  end
+
+  def subentry_sequence(assigns) do
+    ~H"""
+    <button type="button" class="subform-handle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+      </svg>
+    </button>
+    """
+  end
+
+  def subentry_edit(assigns) do
+    ~H"""
+    <button
+      class="subform-edit"
+      type="button"
+      phx-click={@on_click}
+      phx-page-loading
+    >
+      <svg :if={!@open} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+      </svg>
+      <svg :if={@open} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+    </button>
+    """
+  end
+
+  def subentry_remove(assigns) do
+    ~H"""
+    <button
+      phx-click={@on_click}
+      type="button"
+      class="subform-delete"
+      phx-page-loading>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="16" height="16">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+      </svg>
+    </button>
+    """
+  end
+
+  def empty_subform(assigns) do
+    ~H"""
+    <input type="hidden" name={"#{@form.name}[#{@subform_field}]"} value="" />
+    <div class="subform-empty">&rarr; <%= gettext "No associated entries" %></div>
     """
   end
 
@@ -335,30 +368,14 @@ defmodule BrandoAdmin.Components.Form.Subform do
 
     related_entries =
       changeset
-      |> Map.get(:params)
-      |> Map.get(to_string(field_name))
-      |> Map.delete(to_string(index))
-
-    changeset =
-      put_in(
-        changeset,
-        [Access.key(:params), to_string(field_name)],
-        related_entries
-      )
+      |> Ecto.Changeset.get_field(field_name)
+      |> List.delete_at(index)
 
     updated_changeset =
-      cond do
-        socket.assigns.embeds? && Map.keys(related_entries) == [] ->
-          Ecto.Changeset.put_embed(changeset, field_name, [])
-
-        socket.assigns.embeds? ->
-          Ecto.Changeset.cast_embed(changeset, field_name)
-
-        Map.keys(related_entries) == [] ->
-          Ecto.Changeset.put_assoc(changeset, field_name, [])
-
-        true ->
-          Ecto.Changeset.cast_assoc(changeset, field_name)
+      if socket.assigns.embeds? do
+        Ecto.Changeset.put_embed(changeset, field_name, related_entries)
+      else
+        Ecto.Changeset.put_assoc(changeset, field_name, related_entries)
       end
 
     send_update(BrandoAdmin.Components.Form,
