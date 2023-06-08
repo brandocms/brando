6c1dffd2f9e25b7c7e07cf7972f1ed0759cae887
T. Mjoen <trond@twined.net>
2023-06-08 10:18:35 +0200

    Add icon for datasource in module list

--- lib/brando/content/module.ex
+++ lib/brando/content/module.ex
@@ -114,7 +114,9 @@ defmodule Brando.Content.Module do
           data-phx-link-state="push"
           href="/admin/config/content/modules/update/{{ entry.id }}"
           class="entry-link">
-          {{ entry.name }}
+          {% if entry.datasource %}<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" style="stroke: blue; display: inline-block" width="12" height="12">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+          </svg>{% endif %} {{ entry.name }}
         </a>
         <br>
         <small>{{ entry.help_text }}</small>
