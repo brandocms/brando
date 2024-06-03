456d0c4d5aeeae497f0de6d3482ed6c890c0cd12
T. Mjoen <trond@twined.net>
2024-06-03 11:07:58 +0200

    drop log

--- lib/brando/villain/tags/picture.ex
+++ lib/brando/villain/tags/picture.ex
@@ -35,15 +35,6 @@ defmodule Brando.Villain.Tags.Picture do
 
     assigns = %{src: evaled_source, opts: evaled_args}
 
-    require Logger
-
-    Logger.error("""
-
-    villain/tags/picture
-    #{inspect(assigns, pretty: true)}
-
-    """)
-
     comp = ~H"""
     <Brando.HTML.Images.picture src={@src} opts={@opts} />
     """
