4f45196fcb292f49a2330429c9c3a99f42006abd
T. Mjoen <trond@twined.net>
2024-06-22 15:09:53 +0200

    Fix table block del/sort

--- lib/brando_admin/components/form/block.ex
+++ lib/brando_admin/components/form/block.ex
@@ -2401,40 +2401,35 @@ defmodule BrandoAdmin.Components.Form.Block do
           <div
             id={"sortable-#{@uid}-table-rows"}
             class="table-rows"
-            phx-hook="Brando.Sortable"
+            phx-hook="Brando.SortableAssocs"
             data-target={@target}
             data-sortable-id={"sortable-#{@uid}-table-rows"}
             data-sortable-handle=".sort-handle"
             data-sortable-binary-keys="true"
             data-sortable-selector=".table-row"
+            data-sortable-dispatch-event="true"
           >
-            <.inputs_for :let={table_row} field={@block_data[:table_rows]}>
+            <.inputs_for :let={table_row} field={@block_data[:table_rows]} skip_hidden>
               <div class="table-row draggable" data-id={table_row.index}>
+                <input type="hidden" name={table_row[:id].name} value={table_row[:id].value} />
+                <input type="hidden" name={table_row[:_persistent_id].name} value={table_row.index} />
+                <input
+                  type="hidden"
+                  name={"#{@block_data.name}[sort_table_row_ids][]"}
+                  value={table_row.index}
+                />
                 <div class="subform-tools">
                   <button type="button" class="sort-handle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
-                      <path fill="none" d="M0 0h24v24H0z" /><path
-                        class="s"
-                        d="M12 2l4.243 4.243-1.415 1.414L12 4.828 9.172 7.657 7.757 6.243 12 2zM2 12l4.243-4.243 1.414 1.415L4.828 12l2.829 2.828-1.414 1.415L2 12zm20 0l-4.243 4.243-1.414-1.415L19.172 12l-2.829-2.828 1.414-1.415L22 12zm-10 2a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 8l-4.243-4.243 1.415-1.414L12 19.172l2.828-2.829 1.415 1.414L12 22z"
-                        fill="rgba(5,39,82,1)"
-                      />
-                    </svg>
+                    <.icon name="hero-arrows-up-down" />
                   </button>
                   <button
-                    phx-click="delete_row"
-                    phx-target={@target}
-                    phx-value-id={table_row.index}
                     type="button"
-                    class="subform-delete"
-                    phx-page-loading
+                    class="delete-image"
+                    name={"#{@block_data.name}[drop_table_row_ids][]"}
+                    value={table_row.index}
+                    phx-click={JS.dispatch("change")}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
-                      <path fill="none" d="M0 0h24v24H0z" /><path
-                        class="s"
-                        d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-4.586 6l1.768 1.768-1.414 1.414L12 15.414l-1.768 1.768-1.414-1.414L10.586 14l-1.768-1.768 1.414-1.414L12 12.586l1.768-1.768 1.414 1.414L13.414 14zM9 4v2h6V4H9z"
-                        fill="rgba(5,39,82,1)"
-                      />
-                    </svg>
+                    <.icon name="hero-x-mark" />
                   </button>
                 </div>
 
@@ -2460,6 +2455,7 @@ defmodule BrandoAdmin.Components.Form.Block do
                 </button>
               </div>
             </.inputs_for>
+            <input type="hidden" name={"#{@block_data.name}[drop_table_row_ids][]"} />
           </div>
         <% end %>
       </div>
