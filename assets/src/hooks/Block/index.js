bb7f6e1b45eeb7f64cdf77e2242f047344e7593b
T. Mjoen <trond@twined.net>
2024-08-12 16:36:58 +0200

    Drop fancy animation, clashes with bg style

--- assets/src/hooks/Block/index.js
+++ assets/src/hooks/Block/index.js
@@ -3,11 +3,6 @@ import { gsap } from '@brandocms/jupiter'
 
 export default app => ({
   mounted() {
-    const tl = gsap.timeline()
-    tl.set(this.el, { y: -8, opacity: 0 })
-      .to(this.el, { y: 0, ease: 'power2.out', duration: 0.4 })
-      .to(this.el, { opacity: 1, ease: 'none', duration: 0.2 }, '<')
-      .call(() => gsap.set(this.el, { clearProps: 'all' }))
     this.autosizeElements()
   },
 
