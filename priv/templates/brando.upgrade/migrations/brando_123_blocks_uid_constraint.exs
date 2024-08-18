77df908a623da822ee7686f01129dc77eebcec86
T. Mjoen <trond@twined.net>
2024-08-18 19:10:08 +0200

    Force change all block uids before enforcing uniq

--- priv/templates/brando.upgrade/migrations/brando_123_blocks_uid_constraint.exs
+++ priv/templates/brando.upgrade/migrations/brando_123_blocks_uid_constraint.exs
@@ -1,7 +1,22 @@
 defmodule Brando.Repo.Migrations.BlocksUniqueUID do
   use Ecto.Migration
+  import Ecto.Query
 
   def change do
+    # recreate all blocks uids, just to be safe against unique_violation from old stale data
+    Brando.repo().transaction(fn ->
+      Brando.repo().all(from cb in "content_blocks", select: %{id: cb.id})
+      |> Enum.each(fn content_block ->
+        new_uid = Brando.Utils.generate_uid()
+        Brando.repo().update_all(
+          from(cb in "content_blocks", where: cb.id == ^content_block.id),
+          set: [uid: new_uid]
+        )
+      end)
+    end)
+
+    flush()
+
     create unique_index(:content_blocks, [:uid])
   end
 end
