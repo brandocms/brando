b7118cef4c9abb93344d29de7ac29406f1203ec9
T. Mjoen <trond@twined.net>
2024-12-17 23:23:20 +0100

    Fix cachex 404 stream

--- lib/brando/sites/four_oh_four.ex
+++ lib/brando/sites/four_oh_four.ex
@@ -7,8 +7,13 @@ defmodule Brando.Sites.FourOhFour do
   def list() do
     :four_oh_four
     |> Cachex.stream!()
-    |> Enum.map(fn {:entry, url, timestamp, _, hits} ->
-      %{url: url, hits: hits, last_hit_at: timestamp}
+    |> Enum.map(fn {:entry, url, hits, timestamp, _} ->
+      last_hit_at =
+        timestamp
+        |> DateTime.from_unix!(:millisecond)
+        |> Brando.Utils.Datetime.format_datetime("%d/%m/%y, %H:%M")
+
+      %{url: url, hits: hits, last_hit_at: last_hit_at}
     end)
     |> Enum.sort(&(&1.hits >= &2.hits))
   end
