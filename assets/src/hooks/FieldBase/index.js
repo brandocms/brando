7ba81cc49fa0634b6f9f6adfbfc65c94cbc4c1f9
T. Mjoen <trond@twined.net>
2025-01-08 10:02:36 +0100

    Drop debug

--- assets/src/hooks/FieldBase/index.js
+++ assets/src/hooks/FieldBase/index.js
@@ -1,14 +1,12 @@
 export default (app) => ({
   mounted() {
     this.field = this.el.querySelector('[data-watch-focus]')
-    console.log('watchFocus', this.field)
     if (this.field) {
       this.field.addEventListener('focus', this.handleFocus.bind(this))
     }
   },
 
   handleFocus() {
-    console.log('==> Field focus')
     const fName = this.field.getAttribute('name')
     this.pushEventTo(this.el, 'focus', { field: fName })
   },
