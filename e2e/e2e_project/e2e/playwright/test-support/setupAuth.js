e4da9eb6d0fbf288636cd6f33b3b325705f0c0cb
T. Mjoen <trond@twined.net>
2025-01-12 21:30:38 +0100

    Clean up e2e

--- e2e/e2e_project/e2e/playwright/test-support/setupAuth.js
+++ e2e/e2e_project/e2e/playwright/test-support/setupAuth.js
@@ -7,28 +7,25 @@ export const test = baseTest.extend({
   // via `test.use({ scenario: 'scenario-name' })`
   scenario: '',
   page: async ({ browser, scenario }, use) => {
-    const authResponse = await fetch('http://localhost:4444/e2e/login/admin@brandocms.com', {
-      method: 'POST'
-    })
+    const authResponse = await fetch(
+      'http://localhost:4444/e2e/login/admin@brandocms.com',
+      {
+        method: 'POST',
+      }
+    )
 
     // This checks out the DB and gets the user agent string
     const resp = await fetch('http://localhost:4444/sandbox', {
-      method: 'POST'
+      method: 'POST',
     })
 
     const userAgentString = await resp.text()
-
-    console.log(`
-      
-      userAgentString: ${userAgentString} —— checking in.
-      
-      `)
 
     // We setup a new browser context with the user agent string
     // This allows the database to be sandboxed and provides isolation
     const context = await browser.newContext({
       baseURL: 'http://localhost:4444',
-      userAgent: userAgentString
+      userAgent: userAgentString,
     })
 
     // Extract and set cookies
@@ -53,27 +50,25 @@ export const test = baseTest.extend({
 
     await use(page)
 
-    console.log('==> after use.')
-
     await fetch('http://localhost:4444/sandbox', {
       method: 'DELETE',
       headers: {
-        'user-agent': userAgentString
-      }
+        'user-agent': userAgentString,
+      },
     })
-
-    console.log('==> after SANDBOX delete.')
-  }
+  },
 })
 
 // Helper function to parse 'Set-Cookie' header
 function parseSetCookieHeader(setCookieHeader) {
   const cookies = []
-  const cookieHeaders = Array.isArray(setCookieHeader) ? setCookieHeader : [setCookieHeader]
+  const cookieHeaders = Array.isArray(setCookieHeader)
+    ? setCookieHeader
+    : [setCookieHeader]
 
   for (const header of cookieHeaders) {
     const cookie = {}
-    const parts = header.split(';').map(part => part.trim())
+    const parts = header.split(';').map((part) => part.trim())
 
     // The first part is the name and value
     const [name, value] = parts[0].split('=')
--- e2e/e2e_project/test_e2e.sh
+++ e2e/e2e_project/test_e2e.sh
@@ -1,2 +1,13 @@
 #!/bin/zsh
-MIX_ENV=e2e mix do ecto.drop, ecto.create, ecto.migrate, run priv/repo/e2e_seeds.exs && cd e2e/playwright && yarn test:ui
+set -e
+
+# Default test command
+TEST_COMMAND="test"
+
+# Check if --ui was passed as the first argument
+if [ "$1" = "--ui" ]; then
+  TEST_COMMAND="test:ui"
+fi
+
+# Run the Mix tasks and then execute the Playwright tests
+MIX_ENV=e2e mix do ecto.drop, ecto.create, ecto.migrate, run priv/repo/e2e_seeds.exs && cd e2e/playwright && yarn $TEST_COMMAND
