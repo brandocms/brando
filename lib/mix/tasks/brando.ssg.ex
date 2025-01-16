43b6cc90cb0f1ebcdf49b872220a2befe39c18ec
T. Mjoen <trond@twined.net>
2025-01-16 09:06:38 +0100

    Add some more progress info to ssg

--- lib/mix/tasks/brando.ssg.ex
+++ lib/mix/tasks/brando.ssg.ex
@@ -14,15 +14,15 @@ defmodule Mix.Tasks.Brando.Ssg do
     Application.put_env(:phoenix, :serve_endpoints, true)
     Application.put_env(:logger, :level, :error)
 
-    Mix.Tasks.Run.run([])
-    :inets.start()
-
     Mix.shell().info("""
 
     ------------------------------
     % Brando Static Site Generator
     ------------------------------
     """)
+
+    Mix.Tasks.Run.run([])
+    :inets.start()
 
     ssg_path = Brando.SSG.get_root_path()
     File.mkdir_p!(ssg_path)
@@ -35,12 +35,39 @@ defmodule Mix.Tasks.Brando.Ssg do
     if Mix.shell().yes?("\nGenerate static files? (cleans priv/static first)") do
       # delete static
       static_path = Path.join([File.cwd!(), "priv", "static"])
+
+      IO.write([
+        IO.ANSI.blue(),
+        "* ",
+        IO.ANSI.reset(),
+        "Deleting static files... "
+      ])
+
       File.rm_rf!(static_path)
+      IO.write([IO.ANSI.green(), "done!\n", IO.ANSI.reset()])
       # generate static files
       assets_path = Path.join([File.cwd!(), "assets", "frontend"])
       vite_path = Path.join([File.cwd!(), "assets", "frontend", "node_modules", ".bin", "vite"])
+
+      IO.write([
+        IO.ANSI.blue(),
+        "* ",
+        IO.ANSI.reset(),
+        "Building static files... "
+      ])
+
       System.cmd(vite_path, ["build"], cd: assets_path)
+      IO.write([IO.ANSI.green(), "done!\n", IO.ANSI.reset()])
+
+      IO.write([
+        IO.ANSI.blue(),
+        "* ",
+        IO.ANSI.reset(),
+        "Copying static files... "
+      ])
+
       File.cp_r!(static_path, ssg_path)
+      IO.write([IO.ANSI.green(), "done!\n", IO.ANSI.reset()])
     end
 
     if Mix.shell().yes?("\nGenerate HTML?") do
