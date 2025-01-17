95b1c588fdb5f46ed627f4e8af6554539fbe12a7
T. Mjoen <trond@twined.net>
2025-01-17 12:49:42 +0100

    Format

--- config/test.exs
+++ config/test.exs
@@ -1,23 +1,8 @@
 import Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
-config :brando, BrandoIntegrationWeb.Endpoint,
-  http: [port: 80],
-  debug_errors: true,
-  server: false,
-  secret_key_base: "verysecret",
-  pubsub_server: BrandoIntegration.PubSub
+config :bcrypt_elixir, log_rounds: 1
 
-config :brando, BrandoIntegration.Repo,
-  url: "ecto://postgres:postgres@localhost/brando_test",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  ownership_pool: DBConnection.Poolboy,
-  pool_overflow: 0
-
-config :brando, Brando.Static, cdn: [enabled: false]
 config :brando, Brando.Files, cdn: [enabled: false]
-config :brando, scope_default_language_routes: true
 
 config :brando, Brando.Images,
   cdn: %{enabled: false},
@@ -54,23 +39,25 @@ config :brando, Brando.Images,
     ]
   }
 
-config :brando, :app_name, "MyApp"
-config :brando, :auth_sleep_duration, 0
-config :brando, :app_module, BrandoIntegration
-config :brando, :repo_module, BrandoIntegration.Repo
-config :brando, :admin_module, BrandoIntegrationAdmin
-config :brando, :default_language, "en"
-config :brando, :default_admin_language, "en"
-config :brando, :env, :test
-config :brando, :log_dir, Path.expand("./tmp/logs")
-config :brando, :logging, disable_logging: true
-config :brando, :login_url, "/login"
-config :brando, :media_path, Path.join([Mix.Project.app_path(), "tmp", "media"])
-config :brando, :media_url, "/media"
-config :brando, :otp_app, :brando
-config :brando, :warn_on_http_auth, true
-config :brando, :web_module, BrandoIntegrationWeb
-config :brando, :ecto_repos, [BrandoIntegration.Repo]
+config :brando, Brando.Static, cdn: [enabled: false]
+config :brando, Brando.Type.Role, roles: %{staff: 1, admin: 2, superuser: 4}
+config :brando, Brando.Villain, extra_blocks: []
+config :brando, Brando.Villain, parser: Brando.Villain.ParserTest.Parser
+
+config :brando, BrandoIntegration.Repo,
+  url: "ecto://postgres:postgres@localhost/brando_test",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  ownership_pool: DBConnection.Poolboy,
+  # We don't run a server during test. If one is required,
+  # you can enable the server option below.
+  pool_overflow: 0
+
+config :brando, BrandoIntegrationWeb.Endpoint,
+  http: [port: 80],
+  debug_errors: true,
+  server: false,
+  secret_key_base: "verysecret",
+  pubsub_server: BrandoIntegration.PubSub
 
 config :brando, Oban,
   crontab: false,
@@ -79,23 +66,37 @@ config :brando, Oban,
   repo: BrandoIntegration.Repo,
   testing: :inline
 
-config :brando, :languages, [
-  [value: "no", text: "Norsk"],
-  [value: "en", text: "English"]
-]
-
 config :brando, :admin_languages, [
   [value: "no", text: "Norsk"],
   [value: "en", text: "English"]
 ]
 
-config :brando, Brando.Villain, parser: Brando.Villain.ParserTest.Parser
-config :brando, Brando.Villain, extra_blocks: []
+config :brando, :admin_module, BrandoIntegrationAdmin
+config :brando, :app_module, BrandoIntegration
+config :brando, :app_name, "MyApp"
+config :brando, :auth_sleep_duration, 0
+config :brando, :default_admin_language, "en"
+config :brando, :default_language, "en"
+config :brando, :ecto_repos, [BrandoIntegration.Repo]
+config :brando, :env, :test
 
-config :brando, Brando.Type.Role, roles: %{staff: 1, admin: 2, superuser: 4}
+config :brando, :languages, [
+  [value: "no", text: "Norsk"],
+  [value: "en", text: "English"]
+]
 
-config :bcrypt_elixir, log_rounds: 1
+config :brando, :log_dir, Path.expand("./tmp/logs")
+config :brando, :logging, disable_logging: true
+config :brando, :login_url, "/login"
+config :brando, :media_path, Path.join([Mix.Project.app_path(), "tmp", "media"])
+config :brando, :media_url, "/media"
+config :brando, :otp_app, :brando
+config :brando, :repo_module, BrandoIntegration.Repo
+config :brando, :warn_on_http_auth, true
+config :brando, :web_module, BrandoIntegrationWeb
+config :brando, scope_default_language_routes: true
 
-config :phoenix, :stacktrace_depth, 30
 # Print only warnings and errors during test
 config :logger, level: :error
+
+config :phoenix, :stacktrace_depth, 30
