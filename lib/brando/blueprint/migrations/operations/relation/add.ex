f0e6529202ce360b2b762ff248c917fa68b07265
T. Mjoen <trond@twined.net>
2025-01-17 13:08:32 +0100

    Lint

--- lib/brando/blueprint/migrations/operations/relation/add.ex
+++ lib/brando/blueprint/migrations/operations/relation/add.ex
@@ -1,22 +1,18 @@
 defmodule Brando.Blueprint.Migrations.Operations.Relation.Add do
   # import Brando.Blueprint.Migrations.Types
-
+  @moduledoc false
   defstruct relation: nil,
             module: nil,
             opts: nil
 
-  def up(%{
-        relation: %{type: :has_many, name: name, opts: %{module: :blocks}}
-      }) do
+  def up(%{relation: %{type: :has_many, name: name, opts: %{module: :blocks}}}) do
     """
     add :rendered_#{name}, :text
     add :rendered_#{name}_at, :utc_datetime
     """
   end
 
-  def up(%{
-        relation: %{type: :belongs_to, name: name, opts: %{module: referenced_module} = opts}
-      }) do
+  def up(%{relation: %{type: :belongs_to, name: name, opts: %{module: referenced_module} = opts}}) do
     referenced_table = referenced_module.__schema__(:source)
     uuid? = referenced_module.__primary_key__() == {:id, :binary_id, autogenerate: true}
     on_delete = Map.get(opts, :on_delete, :nothing)
@@ -32,33 +28,25 @@ defmodule Brando.Blueprint.Migrations.Operations.Relation.Add do
     end
   end
 
-  def up(%{
-        relation: %{type: :image, name: name}
-      }) do
+  def up(%{relation: %{type: :image, name: name}}) do
     """
     add #{inspect(name)}, :jsonb
     """
   end
 
   # entries are handled as a m2m now
-  def up(%{
-        relation: %{type: :entries, name: _name}
-      }) do
+  def up(%{relation: %{type: :entries, name: _name}}) do
     """
     """
   end
 
-  def up(%{
-        relation: %{type: :embeds_many, name: name, opts: %{module: _}}
-      }) do
+  def up(%{relation: %{type: :embeds_many, name: name, opts: %{module: _}}}) do
     """
     add #{inspect(name)}, :jsonb
     """
   end
 
-  def up(%{
-        relation: %{type: :embeds_one, name: name, opts: %{module: _}}
-      }) do
+  def up(%{relation: %{type: :embeds_one, name: name, opts: %{module: _}}}) do
     """
     add #{inspect(name)}, :jsonb
     """
@@ -68,50 +56,38 @@ defmodule Brando.Blueprint.Migrations.Operations.Relation.Add do
     ""
   end
 
-  def down(%{
-        relation: %{type: :has_many, name: name, opts: %{module: :blocks}}
-      }) do
+  def down(%{relation: %{type: :has_many, name: name, opts: %{module: :blocks}}}) do
     """
     remove :rendered_#{name}
     remove :rendered_#{name}_at
     """
   end
 
-  def down(%{
-        relation: %{type: :belongs_to, name: name}
-      }) do
+  def down(%{relation: %{type: :belongs_to, name: name}}) do
     """
     remove #{inspect(name)}_id
     """
   end
 
-  def down(%{
-        relation: %{type: :image, name: name}
-      }) do
+  def down(%{relation: %{type: :image, name: name}}) do
     """
     remove #{inspect(name)}
     """
   end
 
   # entries are handled as an m2m now
-  def down(%{
-        relation: %{type: :entries, name: _}
-      }) do
+  def down(%{relation: %{type: :entries, name: _}}) do
     """
     """
   end
 
-  def down(%{
-        relation: %{type: :embeds_many, name: name, opts: %{module: _}}
-      }) do
+  def down(%{relation: %{type: :embeds_many, name: name, opts: %{module: _}}}) do
     """
     remove #{inspect(name)}
     """
   end
 
-  def down(%{
-        relation: %{type: :embeds_one, name: name, opts: %{module: _}}
-      }) do
+  def down(%{relation: %{type: :embeds_one, name: name, opts: %{module: _}}}) do
     """
     remove #{inspect(name)}
     """
@@ -121,10 +97,7 @@ defmodule Brando.Blueprint.Migrations.Operations.Relation.Add do
     ""
   end
 
-  def up_indexes(%{
-        relation: %{type: :belongs_to, name: name, opts: %{unique: [with: other_fields]}},
-        module: module
-      })
+  def up_indexes(%{relation: %{type: :belongs_to, name: name, opts: %{unique: [with: other_fields]}}, module: module})
       when is_list(other_fields) do
     table_name = module.__naming__().table_name
 
@@ -135,10 +108,7 @@ defmodule Brando.Blueprint.Migrations.Operations.Relation.Add do
     """
   end
 
-  def up_indexes(%{
-        relation: %{type: :belongs_to, name: name, opts: %{unique: [with: other_field]}},
-        module: module
-      })
+  def up_indexes(%{relation: %{type: :belongs_to, name: name, opts: %{unique: [with: other_field]}}, module: module})
       when is_atom(other_field) do
     table_name = module.__naming__().table_name
 
