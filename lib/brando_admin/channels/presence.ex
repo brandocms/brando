02eda7becf22b90e6c23c4205bfb6ba586e34127
T. Mjoen <trond@twined.net>
2025-04-06 08:04:08 +0200

    Simplify and extract presence quote block

--- lib/brando_admin/channels/presence.ex
+++ lib/brando_admin/channels/presence.ex
@@ -1,5 +1,128 @@
 defmodule BrandoAdmin.Presence do
   @moduledoc false
+
+  defmodule LobbyFetcher do
+    @moduledoc false
+
+    def fetch(presences) do
+      users =
+        presences
+        |> Map.keys()
+        |> Brando.Users.get_users_map()
+        |> Map.new()
+
+      for {id, %{metas: metas}} <- presences, into: %{} do
+        user = users[String.to_integer(id)]
+
+        {user.id,
+         %{
+           user: %{
+             id: user.id,
+             name: user.name,
+             avatar: user.avatar,
+             last_login: user.last_login
+           },
+           metas: metas
+         }}
+      end
+    end
+
+    def handle_metas(joins, leaves, presences, pubsub_server) do
+      # Process joins
+      for {user_id, presence} <- joins do
+        metas = Map.fetch!(presences, user_id)
+
+        user_data = %{
+          user: %{
+            id: presence.user.id,
+            name: presence.user.name,
+            avatar: presence.user.avatar,
+            last_login: presence.user.last_login
+          },
+          metas: metas
+        }
+
+        Phoenix.PubSub.local_broadcast(
+          pubsub_server,
+          "presence",
+          {BrandoAdmin.Presence, {:presence, %{user_joined: user_data}}}
+        )
+      end
+
+      # Process leaves
+      for {user_id, presence} <- leaves do
+        metas =
+          case Map.fetch(presences, user_id) do
+            {:ok, presence_metas} -> presence_metas
+            :error -> []
+          end
+
+        user_data = %{
+          user: %{
+            id: presence.user.id,
+            name: presence.user.name,
+            avatar: presence.user.avatar,
+            last_login: presence.user.last_login
+          },
+          metas: metas
+        }
+
+        Phoenix.PubSub.local_broadcast(
+          pubsub_server,
+          "presence",
+          {BrandoAdmin.Presence, {:presence, %{user_left: user_data}}}
+        )
+      end
+    end
+  end
+
+  defmodule UrlFetcher do
+    @moduledoc false
+
+    def fetch(presences) do
+      users =
+        presences
+        |> Map.keys()
+        |> Brando.Users.get_users_map()
+        |> Map.new()
+
+      for {key, %{metas: metas}} <- presences, into: %{} do
+        {key, %{metas: metas, user: users[String.to_integer(key)]}}
+      end
+    end
+
+    def handle_metas(topic, joins, leaves, presences, pubsub_server) do
+      # Process joins
+      for {user_id, presence} <- joins do
+        user_data = %{user: presence.user, metas: Map.fetch!(presences, user_id)}
+
+        Phoenix.PubSub.local_broadcast(
+          pubsub_server,
+          topic,
+          {BrandoAdmin.Presence, {:uri_presence, %{user_joined: user_data}}}
+        )
+      end
+
+      # Process leaves
+      for {user_id, presence} <- leaves do
+        metas =
+          case Map.fetch(presences, user_id) do
+            {:ok, presence_metas} -> presence_metas
+            :error -> []
+          end
+
+        user_data = %{user: presence.user, metas: metas}
+
+        Phoenix.PubSub.local_broadcast(
+          pubsub_server,
+          topic,
+          {BrandoAdmin.Presence, {:uri_presence, %{user_left: user_data}}}
+        )
+      end
+    end
+  end
+
+  @doc false
   defmacro __using__(opts) do
     otp_app = Keyword.fetch!(opts, :otp_app)
     pubsub_server = Keyword.fetch!(opts, :pubsub_server)
@@ -14,119 +137,21 @@ defmodule BrandoAdmin.Presence do
       def __brando_presence__, do: true
       def init(_opts), do: {:ok, %{}}
 
-      def fetch("lobby", presences) do
-        users =
-          presences
-          |> Map.keys()
-          |> Brando.Users.get_users_map()
-          |> Map.new()
+      # Implementations moved to external modules
+      def fetch("lobby", presences), do: BrandoAdmin.Presence.LobbyFetcher.fetch(presences)
+      def fetch("url:" <> _rest = _topic, presences), do: BrandoAdmin.Presence.UrlFetcher.fetch(presences)
 
-        for {id, %{metas: metas}} <- presences, into: %{} do
-          user = users[String.to_integer(id)]
-
-          {user.id,
-           %{
-             user: %{
-               id: user.id,
-               name: user.name,
-               avatar: user.avatar,
-               last_login: user.last_login
-             },
-             metas: metas
-           }}
-        end
-      end
-
-      def fetch("url:" <> _rest = topic, presences) do
-        users =
-          presences
-          |> Map.keys()
-          |> Brando.Users.get_users_map()
-          |> Map.new()
-
-        for {key, %{metas: metas}} <- presences, into: %{} do
-          {key, %{metas: metas, user: users[String.to_integer(key)]}}
-        end
+      def handle_metas("lobby", %{joins: joins, leaves: leaves}, presences, state) do
+        BrandoAdmin.Presence.LobbyFetcher.handle_metas(joins, leaves, presences, unquote(pubsub_server))
+        {:ok, state}
       end
 
       def handle_metas("url:" <> _rest = topic, %{joins: joins, leaves: leaves}, presences, state) do
-        for {user_id, presence} <- joins do
-          user_data = %{user: presence.user, metas: Map.fetch!(presences, user_id)}
-
-          Phoenix.PubSub.local_broadcast(
-            unquote(pubsub_server),
-            topic,
-            {__MODULE__, {:uri_presence, %{user_joined: user_data}}}
-          )
-        end
-
-        for {user_id, presence} <- leaves do
-          metas =
-            case Map.fetch(presences, user_id) do
-              {:ok, presence_metas} -> presence_metas
-              :error -> []
-            end
-
-          user_data = %{user: presence.user, metas: metas}
-
-          Phoenix.PubSub.local_broadcast(
-            unquote(pubsub_server),
-            topic,
-            {__MODULE__, {:uri_presence, %{user_left: user_data}}}
-          )
-        end
-
+        BrandoAdmin.Presence.UrlFetcher.handle_metas(topic, joins, leaves, presences, unquote(pubsub_server))
         {:ok, state}
       end
 
-      def handle_metas("lobby", %{joins: joins, leaves: leaves}, presences, state) do
-        for {user_id, presence} <- joins do
-          metas = Map.fetch!(presences, user_id)
-
-          user_data = %{
-            user: %{
-              id: presence.user.id,
-              name: presence.user.name,
-              avatar: presence.user.avatar,
-              last_login: presence.user.last_login
-            },
-            metas: metas
-          }
-
-          Phoenix.PubSub.local_broadcast(
-            unquote(pubsub_server),
-            "presence",
-            {__MODULE__, {:presence, %{user_joined: user_data}}}
-          )
-        end
-
-        for {user_id, presence} <- leaves do
-          metas =
-            case Map.fetch(presences, user_id) do
-              {:ok, presence_metas} -> presence_metas
-              :error -> []
-            end
-
-          user_data = %{
-            user: %{
-              id: presence.user.id,
-              name: presence.user.name,
-              avatar: presence.user.avatar,
-              last_login: presence.user.last_login
-            },
-            metas: metas
-          }
-
-          Phoenix.PubSub.local_broadcast(
-            unquote(pubsub_server),
-            "presence",
-            {__MODULE__, {:presence, %{user_left: user_data}}}
-          )
-        end
-
-        {:ok, state}
-      end
-
+      # URL tracking functions
       def track_url(url, current_user_id) do
         timestamp =
           DateTime.utc_now()
