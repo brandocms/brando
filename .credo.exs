6a4915b2db1049c7fad3d84a576ef8b68a9c06d6
T. Mjoen <trond@twined.net>
2025-04-06 07:41:30 +0200

    Leave TODO and FIXME alone

--- .credo.exs
+++ .credo.exs
@@ -35,8 +35,9 @@
         # set the `excluded_macros` parameter to `[:schema, :setup, :test]`.
         {Credo.Check.Design.DuplicatedCode, excluded_macros: []},
 
-        # {Credo.Check.Design.TagTODO, false},
-        # {Credo.Check.Design.TagFIXME, false},
+        {Credo.Check.Design.TagTODO, false},
+        {Credo.Check.Design.TagFIXME, false},
+
         {Credo.Check.Readability.FunctionNames},
         {Credo.Check.Readability.LargeNumbers},
         {Credo.Check.Readability.MaxLineLength, false},
