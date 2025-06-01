0cdad70cde88bd03ddf9a715d068db6bb96b7b51
T. Mjoen <trond@twined.net>
2025-06-01 10:15:39 +0200

    Run single worker, no parallel for E2E :(

--- e2e/e2e_project/e2e/playwright/playwright.config.js
+++ e2e/e2e_project/e2e/playwright/playwright.config.js
@@ -2,37 +2,25 @@
 const { defineConfig, devices } = require('@playwright/test')
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
-    // Move up test project
     cwd: '../../',
     command: 'env MIX_ENV=e2e PORT=4444 mix phx.server',
-    // Used by playwright to check if the server is running
     url: 'http://localhost:4444/',
     stdout: 'pipe',
     stderr: 'pipe',
     reuseExistingServer: !process.env.CI,
   },
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? [['github'], ['html'], ['dot']] : [['list']],
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     baseURL: 'http://localhost:4444/',
@@ -40,7 +28,6 @@ module.exports = defineConfig({
   },
   globalTeardown: require.resolve('./teardown'),
 
-  /* Configure projects for major browsers */
   projects: [
     // {
     //   name: 'chromium',
