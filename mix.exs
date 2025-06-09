77b89c42efb466b92dabc36094c62e6a6b328915
T. Mjoen <trond@twined.net>
2025-06-09 21:55:59 +0200

    Bump deps

--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule Brando.Mixfile do
       {:ecto_sql, "~> 3.12.0"},
 
       # liveview
-      {:phoenix_live_view, "1.0.14"},
+      {:phoenix_live_view, "1.0.17"},
       {:phoenix_html, "~> 4.0"},
 
       # hashing/passwords
