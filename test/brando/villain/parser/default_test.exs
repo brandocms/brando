32dc0b5c1a238a7efbeee37a72127e35d90bc5f7
T. Mjoen <trond@twined.net>
2025-06-14 16:25:37 +0200

    Fix tests

--- test/brando/villain/parser/default_test.exs
+++ test/brando/villain/parser/default_test.exs
@@ -34,12 +34,12 @@ defmodule Brando.Villain.ParserTest do
   end
 
   test "video/2 youtube" do
-    assert video(%{remote_id: "asdf1234", source: :youtube, autoplay: false}, []) ==
+    assert video(%{remote_id: "asdf1234", type: :youtube, autoplay: false}, []) ==
              "<div class=\"video-wrapper video-embed\" data-orientation=\"landscape\" style=\"--aspect-ratio: 0.75\">\n         <iframe width=\"420\"\n                 height=\"315\"\n                 src=\"//www.youtube.com/embed/asdf1234?autoplay=0&controls=0&showinfo=0&rel=0\"\n                 frameborder=\"0\"\n                 allowfullscreen>\n         </iframe>\n       </div>"
   end
 
   test "video/2 vimeo" do
-    assert video(%{remote_id: "asdf1234", source: :vimeo}, []) ==
+    assert video(%{remote_id: "asdf1234", type: :vimeo}, []) ==
              "<div class=\"video-wrapper video-embed\" data-orientation=\"landscape\" style=\"--aspect-ratio: 0.562\">\n         <iframe src=\"//player.vimeo.com/video/asdf1234?dnt=1\"\n                 width=\"500\"\n                 height=\"281\"\n                 frameborder=\"0\"\n                 webkitallowfullscreen\n                 mozallowfullscreen\n                 allowfullscreen>\n         </iframe>\n       </div>"
   end
 
@@ -48,7 +48,7 @@ defmodule Brando.Villain.ParserTest do
              %{
                play_button: false,
                remote_id: "my_video.mp4",
-               source: :file,
+               type: :upload,
                width: 300,
                height: 300,
                autoplay: true,
@@ -66,7 +66,7 @@ defmodule Brando.Villain.ParserTest do
              %{
                play_button: false,
                remote_id: "my_video.mp4",
-               source: :file,
+               type: :upload,
                width: 300,
                height: 300,
                autoplay: true,
